20ef4d7a80fe0592099b26850135653546561c54
David Gardner <dagardner@nvidia.com>
2022-05-03 14:52:41 -0700

    wip

--- ci/scripts/jenkins_build.sh
+++ ci/scripts/jenkins_build.sh
@@ -43,7 +43,8 @@ conda config --show-sources
 conda list --show-channel-urls
 
 gpuci_logger "Building cuDF"
-${MORPHEUS_ROOT}/docker/build_conda_packages.sh libcudf cudf
+#${MORPHEUS_ROOT}/docker/build_conda_packages.sh libcudf cudf
+CONDA_BLD_DIR=/opt/conda/conda-bld CONDA_ARGS="--no-test" ${MORPHEUS_ROOT}/ci/conda/recipes/run_conda_build.sh libcudf cudf
 
 gpuci_logger "Installing cuDF"
 mamba install -q -c file:///${MORPHEUS_ROOT}/.conda-bld -c nvidia -c rapidsai -c conda-forge libcudf cudf
