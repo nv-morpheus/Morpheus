0a22c2d0110b9a90d07b77ae2f80ecf47decbcdd
David Gardner <dagardner@nvidia.com>
2022-05-06 11:33:32 -0700

    First pass attempt at using sccache

--- ci/scripts/jenkins_build.sh
+++ ci/scripts/jenkins_build.sh
@@ -34,7 +34,15 @@ conda config --env --set channel_alias ${CONDA_CHANNEL_ALIAS:-"https://conda.ana
 conda install -q -y -n base -c conda-forge "mamba >=0.22" "boa >=0.10" python=${PYTHON_VER}
 conda create -q -y -n morpheus python=${PYTHON_VER}
 conda activate morpheus
-mamba install -q -y -c conda-forge -c gpuci gpuci-tools "pkg-config=0.29.2"
+mamba install -q -y -c conda-forge -c gpuci gpuci-tools "pkg-config=0.29.2" "sccache=0.3.0"
+
+# Set sccache env vars
+export SCCACHE_S3_KEY_PREFIX=morpheus-linux64
+export SCCACHE_BUCKET=rapids-sccache
+export SCCACHE_REGION=us-west-2
+export SCCACHE_IDLE_TIMEOUT=32768
+export SCCACHE_ERROR_LOG=/tmp/sccache_log.txt
+export SCCACHE_LOG=debug
 
 gpuci_logger "Check versions"
 python3 --version
@@ -49,7 +57,11 @@ conda list --show-channel-urls
 gpuci_logger "Building cuDF"
 CONDA_BLD_DIR=/opt/conda/conda-bld
 mkdir -p ${CONDA_BLD_DIR}
+sccache --zero-stats
 CONDA_ARGS="--output-folder ${CONDA_BLD_DIR} --skip-existing --no-test" ${MORPHEUS_ROOT}/ci/conda/recipes/run_conda_build.sh libcudf cudf
+
+gpuci_logger "sccache usage for cudf build:"
+sccache --show-stats
 
 gpuci_logger "Installing cuDF"
 mamba install -q -y -c file://${CONDA_BLD_DIR} -c nvidia -c rapidsai -c conda-forge libcudf cudf
@@ -62,13 +74,31 @@ cmake --version
 ninja --version
 
 gpuci_logger "Configuring cmake for Morpheus"
-cmake -B build -G Ninja -DCMAKE_MESSAGE_CONTEXT_SHOW=ON -DMORPHEUS_BUILD_BENCHMARKS=ON -DMORPHEUS_BUILD_EXAMPLES=ON -DMORPHEUS_BUILD_TESTS=ON -DMORPHEUS_USE_CONDA=ON -DMORPHEUS_PYTHON_INPLACE_BUILD=ON .
+sccache --zero-stats
+cmake -B build -G Ninja \
+      -DCMAKE_MESSAGE_CONTEXT_SHOW=ON \
+      -DMORPHEUS_BUILD_BENCHMARKS=ON \
+      -DMORPHEUS_BUILD_EXAMPLES=ON \
+      -DMORPHEUS_BUILD_TESTS=ON \
+      -DMORPHEUS_USE_CONDA=ON \
+      -DMORPHEUS_PYTHON_INPLACE_BUILD=ON \
+      -DMORPHEUS_USE_CCACHE=OFF \
+      -DCMAKE_C_COMPILER_LAUNCHER=sccache \
+      -DCMAKE_CXX_COMPILER_LAUNCHER=sccache \
+      -DCMAKE_CUDA_COMPILER_LAUNCHER=sccache \
+      .
 
 gpuci_logger "Building Morpheus"
 cmake --build build -j
 
+gpuci_logger "sccache usage for morpheus build:"
+sccache --show-stats
+
 gpuci_logger "Installing Morpheus"
 pip install -e ${MORPHEUS_ROOT}
+
+gpuci_logger "sccache log"
+cat /tmp/sccache_log.txt
 
 gpuci_logger "Success!"
 
