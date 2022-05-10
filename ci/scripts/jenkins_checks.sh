ff56256df62b68b488010edd23d8409b3a7c03db
David Gardner <dagardner@nvidia.com>
2022-05-10 15:10:46 -0700

    Add check stage

---
+++ ci/scripts/jenkins_checks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+source ci/scripts/jenkins_common.sh
+
+gpuci_logger "Installing CI dependencies"
+mamba install -q -y -c conda-forge "yapf=0.32.0"
+
+gpuci_logger "Runing Python style checks"
+ci/scripts/python_checks.sh
+
+gpuci_logger "Checking copyright headers"
+python ci/scripts/copyright.py --verify-apache-v2 --git-diff-commits ${CHANGE_TARGET} ${GIT_COMMIT}
