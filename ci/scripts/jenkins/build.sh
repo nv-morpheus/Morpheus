a0619ba90fdfe94d62466eba3d4a441352efadb6
David Gardner <dagardner@nvidia.com>
2022-05-20 11:25:20 -0700

    Attempt to build morpheus conda package

--- ci/scripts/jenkins/build.sh
+++ ci/scripts/jenkins/build.sh
@@ -22,7 +22,6 @@ gpuci_logger "Creating conda env"
 conda config --add pkgs_dirs /opt/conda/pkgs
 conda config --env --add channels conda-forge
 conda config --env --set channel_alias ${CONDA_CHANNEL_ALIAS:-"https://conda.anaconda.org"}
-mamba install -q -y -n base -c conda-forge "boa >=0.10"
 mamba create -q -y -n morpheus python=${PYTHON_VER}
 conda activate morpheus
 
@@ -110,19 +109,20 @@ mamba pack --quiet --force --ignore-missing-files --n-threads ${PARALLEL_LEVEL} 
 gpuci_logger "Pushing results to ${DISPLAY_ARTIFACT_URL}"
 aws s3 cp --no-progress "${WORKSPACE_TMP}/conda_env.tar.gz" "${ARTIFACT_URL}/conda_env.tar.gz"
 
-# gpuci_logger "Running conda build for morpheus"
-# ZS=$(sccache --zero-stats)
-# CONDA_ARGS="--no-build-id --output-folder ${CONDA_BLD_DIR} --no-test" ${MORPHEUS_ROOT}/ci/conda/recipes/run_conda_build.sh morpheus
+gpuci_logger "Running conda build for morpheus"
+ZS=$(sccache --zero-stats)
+# git config --global --add safe.directory ${MORPHEUS_ROOT}
+CONDA_ARGS="--no-build-id --output-folder ${CONDA_BLD_DIR} --no-test" ${MORPHEUS_ROOT}/ci/conda/recipes/run_conda_build.sh morpheus
 
-# gpuci_logger "sccache usage for morpheus conda build:"
-# sccache --show-stats
+gpuci_logger "sccache usage for morpheus conda build:"
+sccache --show-stats
 
-# gpuci_logger "Archiving conda builds"
-# tar cfj ${WORKSPACE_TMP}/conda_build.tar.bz ${CONDA_BLD_DIR}
-# ls -lh ${WORKSPACE_TMP}/
+gpuci_logger "Archiving conda builds"
+tar cfj ${WORKSPACE_TMP}/conda_build.tar.bz ${CONDA_BLD_DIR}
+ls -lh ${WORKSPACE_TMP}/
 
-# gpuci_logger "Pushing conda builds to ${DISPLAY_ARTIFACT_URL}"
-# aws s3 cp --no-progress "${WORKSPACE_TMP}/conda_build.tar.bz" "${ARTIFACT_URL}/conda_build.tar.bz"
+gpuci_logger "Pushing conda builds to ${DISPLAY_ARTIFACT_URL}"
+aws s3 cp --no-progress "${WORKSPACE_TMP}/conda_build.tar.bz" "${ARTIFACT_URL}/conda_build.tar.bz"
 
 gpuci_logger "Success"
 exit 0
