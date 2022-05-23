c52cb248d13a3d54adee9e1b39ba8a21b9866d90
David Gardner <96306125+dagardner-nv@users.noreply.github.com>
2022-05-23 13:16:31 -0700

    Jenkins integration (#80)
    Updates conda scripts to uses sccache (ccache work-alike with an S3 backend)
    Adds 4 Jenkins stages:
    1. check
    1. build
    1. test
    1. docs build
    
    Results are publised into S3 with a 30 day retention police and are available via:
    `https://downloads.rapids.ai/ci/morpheus/pull-request/<pull-req id>/<git commit>/<arch>/`
    ex:
    https://downloads.rapids.ai/ci/morpheus/pull-request/80/5161d30673f4d5fb34452df5a8ac6f68b8462685/x86_64/
    
    Intermediate Artifacts pushed to S3:
    * conda_env.tar.gz - Morpheus conda environment used by test & documentation stages
    * workspace.tar.bz - Morpheus workspace used by test & documentation stages
    
    Results pushed to S3:
    * docs.tar.bz - Sphinx HTML documentation
    * report_pytest.xml - Pytest junit report
    * report_pytest_coverage.xml - Pytest coverage report
    * conda_build.tar.bz - Morpheus conda build (not currently working)
    
    Conda builds for cudf are cached seperately (even with sscache this step was still took 17 minutes).
    S3 cache location at:
    `https://downloads.rapids.ai/ci/morpheus/cudf/<cuda ver>/<python ver>/<rapids ver>/<latest git commit to ci/conda>/<arch>cudf_conda.tar.bz`
    ex:
    https://downloads.rapids.ai/ci/morpheus/cudf/11.4/3.8/21.10/b7f728e6db4fc6124970935f461a6791818bfb1d/x86_64/cudf_conda.tar.bz
    
    TODO List:
    
    - [x] Need a test stage
    - [x] Need to run style checks
    - [x] Need to add sccache
    - [x] Need to cache cudf conda packages
    - [x] Need to be able to build without using a gpu
    - [x] Output junit style xml
    - [x] Output docs build
    - [ ] Add conda build step
    - [ ] Need to figure out a way to cache lfs
    
    Closes #102
    Closes #104
    
    Authors:
      - David Gardner (https://github.com/dagardner-nv)
    
    Approvers:
      - Michael Demoret (https://github.com/mdemoret-nv)
    
    URL: https://github.com/nv-morpheus/Morpheus/pull/80

---
+++ ci/scripts/jenkins/common.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+gpuci_logger "Env Setup"
+source /opt/conda/etc/profile.d/conda.sh
+export MORPHEUS_ROOT=${MORPHEUS_ROOT:-$(git rev-parse --show-toplevel)}
+gpuci_logger "Procs: $(nproc)"
+gpuci_logger "Memory"
+
+/usr/bin/free -g
+
+gpuci_logger "user info"
+id
+
+# Change target is the branch name we are merging into but due to the weird way jenkins does
+# the checkout it isn't recognized by git without the origin/ prefix
+export CHANGE_TARGET="origin/${CHANGE_TARGET}"
+
+# S3 vars
+export S3_URL="s3://rapids-downloads/ci/morpheus"
+export DISPLAY_URL="https://downloads.rapids.ai/ci/morpheus"
+export ARTIFACT_URL="${S3_URL}/pull-request/${CHANGE_ID}/${GIT_COMMIT}/${NVARCH}"
+export DISPLAY_ARTIFACT_URL="${DISPLAY_URL}/pull-request/${CHANGE_ID}/${GIT_COMMIT}/${NVARCH}/"
+
+# Set sccache env vars
+export SCCACHE_S3_KEY_PREFIX=morpheus-${NVARCH}
+export SCCACHE_BUCKET=rapids-sccache
+export SCCACHE_REGION=us-west-2
+export SCCACHE_IDLE_TIMEOUT=32768
+#export SCCACHE_LOG=debug
+
+gpuci_logger "Environ:"
+env | sort
