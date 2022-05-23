46d7b801e9551b29b2c1fc0b1b3bc3dc08e11420
David Gardner <dagardner@nvidia.com>
2022-05-23 09:47:13 -0700

    Rename CONDA_BLD_DIR to CUDF_CONDA_BLD_DIR to eliminate confusion with the similarly named CONDA_BLD_PATH var

--- ci/scripts/jenkins/build.sh
+++ ci/scripts/jenkins/build.sh
@@ -37,7 +37,7 @@ g++ --version
 show_conda_info
 
 gpuci_logger "Checking S3 cuDF cache"
-CONDA_BLD_DIR=/opt/conda/conda-bld
+CUDF_CONDA_BLD_DIR=/opt/conda/conda-bld
 CUDF_CONDA_COMMIT=$(git log -n 1 --pretty=format:%H -- ci/conda)
 CUDF_CONDA_CACHE_PATH="/cudf/${CUDA_VER}/${PYTHON_VER}/${RAPIDS_VER}/${CUDF_CONDA_COMMIT}/${NVARCH}/cudf_conda.tar.bz"
 CUDF_CONDA_CACHE_URL="${S3_URL}${CUDF_CONDA_CACHE_PATH}"
@@ -50,22 +50,22 @@ set -e
 
 if [[ "${FETCH_STATUS}" != "0" ]]; then
       gpuci_logger "Cache miss, Building cuDF"
-      mkdir -p ${CONDA_BLD_DIR}
+      mkdir -p ${CUDF_CONDA_BLD_DIR}
       # The --no-build-id bit is needed for sccache
-      CONDA_ARGS="--no-build-id --output-folder ${CONDA_BLD_DIR} --skip-existing --no-test" ${MORPHEUS_ROOT}/ci/conda/recipes/run_conda_build.sh libcudf cudf
+      CONDA_ARGS="--no-build-id --output-folder ${CUDF_CONDA_BLD_DIR} --skip-existing --no-test" ${MORPHEUS_ROOT}/ci/conda/recipes/run_conda_build.sh libcudf cudf
 
       gpuci_logger "sccache usage for cudf build:"
       sccache --show-stats
       sccache --zero-stats 2>&1 > /dev/null
 
       gpuci_logger "Archiving cuDF build"
-      cd $(dirname ${CONDA_BLD_DIR})
-      tar cfj ${CUDF_CONDA_TAR} $(basename ${CONDA_BLD_DIR})
+      cd $(dirname ${CUDF_CONDA_BLD_DIR})
+      tar cfj ${CUDF_CONDA_TAR} $(basename ${CUDF_CONDA_BLD_DIR})
       cd -
       aws s3 cp --no-progress ${CUDF_CONDA_TAR} ${CUDF_CONDA_CACHE_URL}
 else
       gpuci_logger "Cache hit, using cached cuDF"
-      cd $(dirname ${CONDA_BLD_DIR})
+      cd $(dirname ${CUDF_CONDA_BLD_DIR})
       tar xf ${CUDF_CONDA_TAR}
       cd -
 fi
