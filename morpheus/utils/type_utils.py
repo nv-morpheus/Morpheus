930a81678f13d0db83352341dde7d5fe4e014cad
David Gardner <dagardner@nvidia.com>
2022-06-02 12:15:57 -0700

    Return an empty string when t is None, this works-around an issue where the pipeline visualization is getting None values for input/output types

--- morpheus/utils/type_utils.py
+++ morpheus/utils/type_utils.py
@@ -116,6 +116,9 @@ def pretty_print_type_name(t: typing.Type) -> str:
     Determines a good label to use for a type. Keeps the strings shorter.
     """
 
+    if t is None:
+        return ""
+
     if (t.__module__ == "typing"):
         return str(t).replace("typing.", "")
 
