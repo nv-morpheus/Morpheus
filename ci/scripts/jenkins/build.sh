d6fd3321331868fd53b212753ff209f2b00bc5bf
David Gardner <dagardner@nvidia.com>
2022-06-06 10:33:39 -0700

    Manually ensure that the build is clean

--- ci/scripts/jenkins/build.sh
+++ ci/scripts/jenkins/build.sh
@@ -19,6 +19,7 @@ set -e
 source ${WORKSPACE}/ci/scripts/jenkins/common.sh
 
 gpuci_logger "Creating conda env"
+rm -rf ${MORPHEUS_ROOT}/.cache/ ${MORPHEUS_ROOT}/build/
 conda config --add pkgs_dirs /opt/conda/pkgs
 conda config --env --add channels conda-forge
 conda config --env --set channel_alias ${CONDA_CHANNEL_ALIAS:-"https://conda.anaconda.org"}
