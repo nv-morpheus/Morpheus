d91c2ee0ae1c894bd456d2aeb2beb8aea159c06f
Christopher Harris <xixonia@gmail.com>
2022-06-14 17:58:41 -0500

    .

--- ci/conda/recipes/run_conda_build.sh
+++ ci/conda/recipes/run_conda_build.sh
@@ -43,7 +43,7 @@ export USE_SCCACHE=${USE_SCCACHE:-""}
 
 export CUDA="$(conda list | grep cudatoolkit | egrep -o "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+")"
 export PYTHON_VER="$(python -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")"
-export CUDA=11.4.1
+export CUDA=11.5
 echo "CUDA        : ${CUDA}"
 echo "PYTHON_VER  : ${PYTHON_VER}"
 echo "NEO_GIT_TAG : ${NEO_GIT_TAG}"
@@ -91,6 +91,7 @@ CONDA_ARGS_ARRAY+=("--variants" "{python: 3.8}")
 # And default channels (with optional channel alias)
 CONDA_ARGS_ARRAY+=("-c" "${CONDA_CHANNEL_ALIAS:+"${CONDA_CHANNEL_ALIAS%/}/"}rapidsai")
 CONDA_ARGS_ARRAY+=("-c" "${CONDA_CHANNEL_ALIAS:+"${CONDA_CHANNEL_ALIAS%/}/"}nvidia")
+CONDA_ARGS_ARRAY+=("-c" "${CONDA_CHANNEL_ALIAS:+"${CONDA_CHANNEL_ALIAS%/}/"}nvidia/label/cuda-11.5.2")
 CONDA_ARGS_ARRAY+=("-c" "${CONDA_CHANNEL_ALIAS:+"${CONDA_CHANNEL_ALIAS%/}/"}nvidia/label/dev")
 CONDA_ARGS_ARRAY+=("-c" "conda-forge")
 
