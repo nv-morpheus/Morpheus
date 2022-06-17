59c6d4c09ee9686665780d71cc18942ea30fc757
Michael Demoret <mdemoret@nvidia.com>
2022-06-16 18:22:46 -0700

    Exporting the cmake variable

--- ci/conda/recipes/run_conda_build.sh
+++ ci/conda/recipes/run_conda_build.sh
@@ -35,7 +35,7 @@ export CONDA_COMMAND=${CONDA_COMMAND:-"mambabuild"}
 export MORPHEUS_ROOT=${MORPHEUS_ROOT:-$(git rev-parse --show-toplevel)}
 
 # Export whether or not to build with python stubs
-# export MORPHEUS_BUILD_PYTHON_STUBS=${MORPHEUS_BUILD_PYTHON_STUBS:-"ON"}
+export MORPHEUS_BUILD_PYTHON_STUBS=${MORPHEUS_BUILD_PYTHON_STUBS:-"ON"}
 
 # Set the tag for the neo commit to use
 export NEO_GIT_TAG=${NEO_GIT_TAG:-"5b55e37c6320c1a5747311a1e29e7ebb049d12bc"}
