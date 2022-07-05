61b96c1573c95459f59770109b825ea1388d223d
Eli Fajardo <efajardo@nvidia.com>
2022-07-05 21:42:27 +0000

    update configs for e2e benchmarks

--- tests/benchmarks/test_bench_e2e_pipelines.py
+++ tests/benchmarks/test_bench_e2e_pipelines.py
@@ -43,11 +43,11 @@ from utils import TEST_DIRS
 TEST_SOURCES = {
     "test_sid_nlp_e2e": {
         "file_path": os.path.join(TEST_DIRS.validation_data_dir, 'sid-validation-data.csv'),
-        "repeat": 1
+        "repeat": 10
     },
      "test_abp_fil_e2e": {
         "file_path": os.path.join(TEST_DIRS.validation_data_dir, 'abp-validation-data.jsonlines'),
-        "repeat": 1
+        "repeat": 100
     },
      "test_phishing_nlp_e2e": {
         "file_path": os.path.join(TEST_DIRS.validation_data_dir, 'phishing-email-validation-data.jsonlines'),
@@ -55,7 +55,7 @@ TEST_SOURCES = {
     },
      "test_cloudtrail_ae_e2e": {
         "glob_path": os.path.join(TEST_DIRS.validation_data_dir, 'hammah-*.csv'),
-        "repeat": 1000
+        "repeat": 1
     }
 }
 
@@ -135,16 +135,17 @@ def ae_pipeline(config: Config,
     pipeline.add_stage(SerializeStage(config))
     pipeline.add_stage(WriteToFileStage(config, filename=output_file, overwrite=True))
 
-
+    pipeline.build()
+    pipeline.run()
 
 def test_sid_nlp_e2e(benchmark, tmp_path):
 
     config = Config()
     config.mode = PipelineModes.NLP
-    config.num_threads = 1
-    config.pipeline_batch_size = 64
+    config.num_threads = 8
+    config.pipeline_batch_size = 1024
     config.model_max_batch_size = 64
-    config.feature_length = 32
+    config.feature_length = 256
     config.edge_buffer_size = 4
     config.class_labels = [
         "address",
@@ -172,7 +173,7 @@ def test_abp_fil_e2e(benchmark, tmp_path):
 
     config = Config()
     config.mode = PipelineModes.FIL
-    config.num_threads = 4
+    config.num_threads = 8
     config.pipeline_batch_size = 1024
     config.model_max_batch_size = 1024
     config.feature_length = 29
@@ -196,10 +197,10 @@ def test_phishing_nlp_e2e(benchmark, tmp_path):
 
     config = Config()
     config.mode = PipelineModes.NLP
-    config.num_threads = 1
-    config.pipeline_batch_size = 64
+    config.num_threads = 8
+    config.pipeline_batch_size = 1024
     config.model_max_batch_size = 64
-    config.feature_length = 32
+    config.feature_length = 128
     config.edge_buffer_size = 4
     config.class_labels = ["score", "pred"]
     CppConfig.set_should_use_cpp(True)
@@ -218,8 +219,8 @@ def test_cloudtrail_ae_e2e(benchmark, tmp_path):
     config = Config()
     config.mode = PipelineModes.AE
     config.num_threads = 1
-    config.pipeline_batch_size = 64
-    config.model_max_batch_size = 64
+    config.pipeline_batch_size = 1024
+    config.model_max_batch_size = 1024
     config.feature_length = 32
     config.edge_buffer_size = 4
     config.class_labels = ["ae_anomaly_score"]
@@ -227,11 +228,16 @@ def test_cloudtrail_ae_e2e(benchmark, tmp_path):
     config.ae = ConfigAutoEncoder()
     config.ae.userid_column_name = "userIdentityaccountId"
     config.ae.userid_filter = "Account-123456789"
+    ae_cols_filepath = os.path.join(TEST_DIRS.data_dir, 'columns_ae.txt')
+    with open(ae_cols_filepath, "r") as lf:
+        config.ae.feature_columns = [x.strip() for x in lf.readlines()]
     CppConfig.set_should_use_cpp(False)
 
     input_glob = TEST_SOURCES["test_cloudtrail_ae_e2e"]["glob_path"]
+    print(input_glob)
     repeat = TEST_SOURCES["test_cloudtrail_ae_e2e"]["repeat"]
     train_data_glob = os.path.join(TEST_DIRS.training_data_dir, "hammah-*.csv")
+    print(train_data_glob)
     output_filepath = os.path.join(tmp_path, "cloudtrail_ae_e2e_output.csv")
 
     benchmark(ae_pipeline, config, input_glob, repeat, train_data_glob, output_filepath)
