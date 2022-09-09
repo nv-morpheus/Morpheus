438ea7c10057f4bb83e1d86484e59d2592627593
David Gardner <dagardner@nvidia.com>
2022-09-09 11:07:16 -0700

    Mark benchmark tests as slow

--- tests/benchmarks/test_bench_e2e_pipelines.py
+++ tests/benchmarks/test_bench_e2e_pipelines.py
@@ -17,6 +17,8 @@ import json
 import logging
 import os
 
+import pytest
+
 from morpheus.config import Config
 from morpheus.config import ConfigAutoEncoder
 from morpheus.config import ConfigFIL
@@ -120,6 +122,7 @@ def ae_pipeline(config: Config, input_glob, repeat, train_data_glob, output_file
     pipeline.run()
 
 
+@pytest.mark.slow
 def test_sid_nlp_e2e(benchmark, tmp_path):
 
     config = Config()
@@ -152,6 +155,7 @@ def test_sid_nlp_e2e(benchmark, tmp_path):
     benchmark(nlp_pipeline, config, input_filepath, repeat, vocab_filepath, output_filepath, model_name)
 
 
+@pytest.mark.slow
 def test_abp_fil_e2e(benchmark, tmp_path):
 
     config = Config()
@@ -176,6 +180,7 @@ def test_abp_fil_e2e(benchmark, tmp_path):
     benchmark(fil_pipeline, config, input_filepath, repeat, output_filepath, model_name)
 
 
+@pytest.mark.slow
 def test_phishing_nlp_e2e(benchmark, tmp_path):
 
     config = Config()
@@ -197,6 +202,7 @@ def test_phishing_nlp_e2e(benchmark, tmp_path):
     benchmark(nlp_pipeline, config, input_filepath, repeat, vocab_filepath, output_filepath, model_name)
 
 
+@pytest.mark.slow
 def test_cloudtrail_ae_e2e(benchmark, tmp_path):
 
     config = Config()
