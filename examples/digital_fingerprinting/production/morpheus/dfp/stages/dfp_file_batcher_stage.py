767430e6c0ac8c779440edd017062176cdf7d146
David Gardner <dagardner@nvidia.com>
2022-09-27 12:30:39 -0700

    Apply time window filtering if defined. Avoid redundant calls to `self._date_conversion_func`. Avoid multiple iterations of the file_objects.

--- examples/digital_fingerprinting/production/morpheus/dfp/stages/dfp_file_batcher_stage.py
+++ examples/digital_fingerprinting/production/morpheus/dfp/stages/dfp_file_batcher_stage.py
@@ -14,6 +14,8 @@
 
 import logging
 import typing
+from collections import namedtuple
+from datetime import datetime
 
 import fsspec
 import pandas as pd
@@ -26,15 +28,25 @@ from morpheus.pipeline.stream_pair import StreamPair
 
 logger = logging.getLogger("morpheus.{}".format(__name__))
 
+TimestampFileObj = namedtuple("TimestampFileObj", ["timestamp", "file_object"])
+
 
 class DFPFileBatcherStage(SinglePortStage):
 
-    def __init__(self, c: Config, date_conversion_func, period="D", sampling_rate_s=0, start_time=None, end_time=None):
+    def __init__(self,
+                 c: Config,
+                 date_conversion_func,
+                 period="D",
+                 sampling_rate_s=0,
+                 start_time: datetime = None,
+                 end_time: datetime = None):
         super().__init__(c)
 
         self._date_conversion_func = date_conversion_func
         self._sampling_rate_s = sampling_rate_s
         self._period = period
+        self._start_time = start_time
+        self._end_time = end_time
 
     @property
     def name(self) -> str:
@@ -48,33 +60,52 @@ class DFPFileBatcherStage(SinglePortStage):
 
     def on_data(self, file_objects: fsspec.core.OpenFiles):
 
-        file_object_list = file_objects
+        # Determine the date of the file, and apply the window filter if we have one
+        ts_and_files = []
+        for file_object in file_objects:
+            ts = self._date_conversion_func(file_object)
+
+            # Exclude any files outside the time window
+            if ((self._start_time is not None and ts < self._start_time)
+                    or (self._end_time is not None and ts > self._end_time)):
+                continue
+
+            ts_and_files.append(TimestampFileObj(ts, file_object))
+
+        # sort the incoming data by date
+        ts_and_files.sort()
 
         # Create a dataframe with the incoming metadata
-        if ((len(file_object_list) > 1) and (self._sampling_rate_s > 0)):
+        if ((len(ts_and_files) > 1) and (self._sampling_rate_s > 0)):
             file_sampled_list = []
 
-            file_object_list.sort(key=lambda file_object: self._date_conversion_func(file_object))
+            ts_last = ts_and_files[0].timestamp
 
-            ts_last = self._date_conversion_func(file_object_list[0])
+            file_sampled_list.append(ts_and_files[0])
 
-            file_sampled_list.append(file_object_list[0])
-
-            for idx in range(1, len(file_object_list)):
-                ts = self._date_conversion_func(file_object_list[idx])
+            for idx in range(1, len(ts_and_files)):
+                ts = ts_and_files[idx].timestamp
 
                 if ((ts - ts_last).seconds >= self._sampling_rate_s):
 
-                    file_sampled_list.append(file_object_list[idx])
+                    ts_and_files.append(ts_and_files[idx])
                     ts_last = ts
             else:
-                file_object_list = file_sampled_list
+                ts_and_files = file_sampled_list
 
         df = pd.DataFrame()
 
-        df["dfp_timestamp"] = [self._date_conversion_func(file_object) for file_object in file_object_list]
-        df["key"] = [file_object.full_name for file_object in file_object_list]
-        df["objects"] = file_object_list
+        timestamps = []
+        full_names = []
+        file_objs = []
+        for (ts, file_object) in ts_and_files:
+            timestamps.append(ts)
+            full_names.append(file_object.full_name)
+            file_objs.append(file_object)
+
+        df["dfp_timestamp"] = timestamps
+        df["key"] = full_names
+        df["objects"] = file_objs
 
         # Now split by the batching settings
         df_period = df["dfp_timestamp"].dt.to_period(self._period)
