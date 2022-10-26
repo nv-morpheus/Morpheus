65c5184cf72065646941c2f3aaa792300c60a34e
David Gardner <dagardner@nvidia.com>
2022-10-26 09:18:14 -0700

    WIP

---
+++ morpheus/_lib/include/morpheus/messages/multi_tensor.hpp
@@ -0,0 +1,86 @@
+/**
+ * SPDX-FileCopyrightText: Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "morpheus/messages/memory/tensor_memory.hpp"
+#include "morpheus/messages/meta.hpp"
+#include "morpheus/messages/multi.hpp"
+#include "morpheus/objects/tensor_object.hpp"
+
+#include <cstddef>
+#include <memory>
+#include <string>
+#include <utility>  // for pair
+#include <vector>
+
+namespace morpheus {
+/****** MultiTensorMessage*******************************/
+/**
+ * Base class for MultiInferenceMessage & MultiResponseMessage
+ * Contains a pointer to an instance of TensorMemory along with an
+ * offset & count to those tensors.
+ *
+ * mess_offset & mess_count refer to the range of records in meta.
+ * offset & count refer to the range of records in TensorMemory.
+ *
+ * While TensorMemory can contain multiple tensors, it is a requirement that
+ * they are all of the same length and that element N in each tensor refers
+ * to the same record.
+ */
+#pragma GCC visibility push(default)
+class MultiTensorMessage : public DerivedMultiMessage<MultiTensorMessage, MultiMessage>
+{
+  public:
+    MultiTensorMessage(const MultiTensorMessage &other) = default;
+    MultiTensorMessage(std::shared_ptr<morpheus::MessageMeta> meta,
+                       std::size_t mess_offset,
+                       std::size_t mess_count,
+                       std::shared_ptr<morpheus::TensorMemory> memory,
+                       std::size_t offset,
+                       std::size_t count);
+
+    std::shared_ptr<morpheus::TensorMemory> memory;
+    std::size_t offset{0};
+    std::size_t count{0};
+
+    /**
+     * Returns a tensor with the given name. Will halt on a fatal error if the tensor does not exist.
+     */
+    const TensorObject get_tensor(const std::string &name) const;
+
+    /**
+     * Update the value of a given tensor. The tensor must already exist, otherwise this will halt on a fatal error.
+     */
+    const void set_tensor(const std::string &name, const TensorObject &value);
+
+  protected:
+    /**
+     * TODO(Documentation)
+     */
+    void get_slice_impl(std::shared_ptr<MultiMessage> new_message, std::size_t start, std::size_t stop) const override;
+
+    void copy_ranges_impl(std::shared_ptr<MultiMessage> new_message,
+                          const std::vector<std::pair<std::size_t, std::size_t>> &ranges,
+                          size_t num_selected_rows) const override;
+
+    std::shared_ptr<morpheus::TensorMemory> copy_input_ranges(
+        const std::vector<std::pair<std::size_t, std::size_t>> &ranges, std::size_t num_selected_rows) const;
+};
+
+#pragma GCC visibility pop
+}  // namespace morpheus
---
+++ morpheus/_lib/src/messages/multi_tensor.cpp
@@ -0,0 +1,111 @@
+/**
+ * SPDX-FileCopyrightText: Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "morpheus/messages/multi_tensor.hpp"
+
+#include "morpheus/utilities/cupy_util.hpp"
+
+#include <cudf/types.hpp>  // for cudf::size_type>
+#include <glog/logging.h>
+
+#include <cstdint>  // for int32_t
+#include <ostream>  // needed for logging
+
+namespace morpheus {
+/****** Component public implementations *******************/
+/****** <MultiTensorMessage>****************************************/
+MultiTensorMessage::MultiTensorMessage(std::shared_ptr<morpheus::MessageMeta> meta,
+                                       std::size_t mess_offset,
+                                       std::size_t mess_count,
+                                       std::shared_ptr<morpheus::TensorMemory> memory,
+                                       std::size_t offset,
+                                       std::size_t count) :
+  DerivedMultiMessage(meta, mess_offset, mess_count),
+  memory(std::move(memory)),
+  offset(offset),
+  count(count)
+{}
+
+const TensorObject MultiTensorMessage::get_tensor(const std::string &name) const
+{
+    CHECK(this->memory->has_tensor(name)) << "Cound not find tensor: " << name;
+
+    // check if we are getting the entire input
+    if (this->offset == 0 && this->count == this->memory->count)
+    {
+        return this->memory->tensors[name];
+    }
+
+    // TODO(MDD): This really needs to return the slice of the tensor
+    return this->memory->tensors[name].slice({static_cast<cudf::size_type>(this->offset), 0},
+                                             {static_cast<cudf::size_type>(this->offset + this->count), -1});
+}
+
+const void MultiTensorMessage::set_tensor(const std::string &name, const TensorObject &value)
+{
+    // Get the input slice first
+    auto slice = this->get_tensor(name);
+
+    // Set the value to use assignment
+    slice = value;
+}
+
+void MultiTensorMessage::get_slice_impl(std::shared_ptr<MultiMessage> new_message,
+                                        std::size_t start,
+                                        std::size_t stop) const
+{
+    auto sliced_message = DCHECK_NOTNULL(std::dynamic_pointer_cast<MultiTensorMessage>(new_message));
+
+    sliced_message->offset = start;
+    sliced_message->count  = stop - start;
+
+    // If we have more inference rows than message rows, we need to use the seq_ids to figure out the slicing. This
+    // will be slow and should be avoided at all costs
+    if (this->count != this->mess_count && this->memory->has_input("seq_ids"))
+    {
+        auto seq_ids = this->get_input("seq_ids");
+
+        // Determine the new start and stop before passing onto the base
+        start = seq_ids.read_element<int32_t>({(TensorIndex)start, 0});
+        stop  = seq_ids.read_element<int32_t>({(TensorIndex)stop - 1, 0}) + 1;
+    }
+
+    // Pass onto the base
+    DerivedMultiMessage::get_slice_impl(new_message, start, stop);
+}
+
+void MultiTensorMessage::copy_ranges_impl(std::shared_ptr<MultiMessage> new_message,
+                                          const std::vector<std::pair<size_t, size_t>> &ranges,
+                                          size_t num_selected_rows) const
+{
+    auto copied_message = DCHECK_NOTNULL(std::dynamic_pointer_cast<MultiTensorMessage>(new_message));
+    DerivedMultiMessage::copy_ranges_impl(copied_message, ranges, num_selected_rows);
+
+    copied_message->offset = 0;
+    copied_message->count  = num_selected_rows;
+    copied_message->memory = copy_input_ranges(ranges, num_selected_rows);
+}
+
+std::shared_ptr<TensorMemory> MultiTensorMessage::copy_input_ranges(
+    const std::vector<std::pair<size_t, size_t>> &ranges, size_t num_selected_rows) const
+{
+    auto offset_ranges = apply_offset_to_ranges(offset, ranges);
+    auto tensors       = memory->copy_tensor_ranges(offset_ranges, num_selected_rows);
+    return std::make_shared<TensorMemory>(num_selected_rows, std::move(tensors));
+}
+
+}  // namespace morpheus
