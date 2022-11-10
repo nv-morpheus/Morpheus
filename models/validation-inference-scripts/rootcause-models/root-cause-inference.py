bcfa577358b7ab4d9accd8bdea820d71f26c8415
gbatmaz <50459436+gbatmaz@users.noreply.github.com>
2022-11-10 21:38:03 +0000

    change model filenames

--- models/validation-inference-scripts/rootcause-models/root-cause-inference.py
+++ models/validation-inference-scripts/rootcause-models/root-cause-inference.py
@@ -18,8 +18,8 @@ Example Usage:
 python \
 Root-Cause-Predictive-Maintenance-Sequence-Classifier-FineTuning.py \
     --validationdata ../../datasets/validation-data\
-    /Rootcause-validation-data.jsonlines \
-    --model ../../rootcause-models/Rootcause.onnx \
+    /rootcause-validation-data.jsonlines \
+    --model ../../rootcause-models/rootcause.onnx \
     --vocab ../../training-tuning-scripts/rootcause-models\
     /resources/bert-base-uncased-hash.txt \
     --output rootcause-validation-output.jsonlines
