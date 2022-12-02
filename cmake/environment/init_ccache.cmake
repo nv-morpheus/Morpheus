da9cf59357b9c02715245f01ba0f84bcd1eaba8f
Devin Robison <drobison@nvidia.com>
2022-12-02 11:55:26 -0700

    Checkpoint

--- cmake/environment/init_ccache.cmake
+++ cmake/environment/init_ccache.cmake
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2021-2022,NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,11 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
+include_guard(GLOBAL)
 
-set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+list(APPEND CMAKE_MESSAGE_CONTEXT "cache")
 
-# Set the RPATH to allow finding other libraries in the same folder
-set(VCPKG_LINKER_FLAGS "-Wl,-rpath,'$ORIGIN':'$ORIGIN/../lib'")
+morpheus_utils_check_cache_path(MORPHEUS_CACHE_DIR)
+
+# Configure CCache if requested
+if(MORPHEUS_USE_CCACHE)
+  morpheus_utils_initialize_ccache(MORPHEUS_CACHE_DIR)
+endif()
+
+morpheus_utils_initialize_cpm(MORPHEUS_CACHE_DIR)
+
+list(POP_BACK CMAKE_MESSAGE_CONTEXT)
