6cea7d3c2d36eb47482cc3c69d3c280c87132365
Christopher Harris <xixonia@gmail.com>
2022-12-09 01:39:16 +0000

    .

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-find_in_conda_env(){
+conda_env_find(){
     conda env list | grep "${@}" >/dev/null 2>/dev/null
 }
 
-env_name=morpheus
+MORPHEUS_DEFAULT_CONDA_ENV=${MORPHEUS_DEFAULT_CONDA_ENV:-morpheus}
 
-if find_in_conda_env "${env_name}" ; then
-    conda activate ${env_name}
+conda_env_update(){onda
+    mamba env update -n ${MORPHEUS_DEFAULT_CONDA_ENV} -f ${MORPHEUS_ROOT}/docker/conda/environments/cuda11.5_dev.yml
+}
+
+if conda_env_find "${MORPHEUS_DEFAULT_CONDA_ENV}" ; then
+    conda activate ${MORPHEUS_DEFAULT_CONDA_ENV}
 else
     read -p "Morpheus conda environment not found. Would you like to create it? [yn] " yn
     case $yn in
-        [Yy]* ) mamba env create --name ${env_name} -f ${CODESPACE_VSCODE_FOLDER}/docker/conda/environments/cuda11.5_dev.yml && conda activate ${env_name};;
+        [Yy]* ) mamba env create --name ${MORPHUES_DEFAULT_CONDA_ENV} -f ${MORPHEUS_ROOT}/docker/conda/environments/cuda11.5_dev.yml && conda activate ${env_name};;
     esac
 fi
