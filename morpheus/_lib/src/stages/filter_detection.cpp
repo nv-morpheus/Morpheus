796c2b0b54f3dae2f0bf4371e0a578d40361eb8f
David Gardner <dagardner@nvidia.com>
2022-12-21 14:50:25 -0800

    Use field_name var

--- morpheus/_lib/src/stages/filter_detection.cpp
+++ morpheus/_lib/src/stages/filter_detection.cpp
@@ -58,101 +58,100 @@ FilterDetectionsStage::FilterDetectionsStage(float threshold,
 
 FilterDetectionsStage::subscribe_fn_t FilterDetectionsStage::build_operator()
 {
-    return
-        [this](rxcpp::observable<sink_type_t> input, rxcpp::subscriber<source_type_t> output) {
-            return input.subscribe(rxcpp::make_observer<sink_type_t>(
-                [this, &output](sink_type_t x) {
-                    const auto& probs  = x->get_probs();
-                    const auto& shape  = probs.get_shape();
-                    const auto& stride = probs.get_stride();
+    return [this](rxcpp::observable<sink_type_t> input, rxcpp::subscriber<source_type_t> output) {
+        return input.subscribe(rxcpp::make_observer<sink_type_t>(
+            [this, &output](sink_type_t x) {
+                const auto& filter_source = x->get_output(m_field_name);
+                const auto& shape         = filter_source.get_shape();
+                const auto& stride        = filter_source.get_stride();
 
-                    CHECK(probs.rank() == 2)
-                        << "C++ impl of the FilterDetectionsStage currently only supports two dimensional arrays";
+                CHECK(filter_source.rank() == 2)
+                    << "C++ impl of the FilterDetectionsStage currently only supports two dimensional arrays";
 
-                    const std::size_t num_rows    = shape[0];
-                    const std::size_t num_columns = shape[1];
+                const std::size_t num_rows    = shape[0];
+                const std::size_t num_columns = shape[1];
 
-                    // A bit ugly, but we cant get access to the rmm::device_buffer here. So make a copy
-                    auto tmp_buffer = std::make_shared<rmm::device_buffer>(probs.count() * probs.dtype_size(),
-                                                                           rmm::cuda_stream_per_thread);
+                // A bit ugly, but we cant get access to the rmm::device_buffer here. So make a copy
+                auto tmp_buffer = std::make_shared<rmm::device_buffer>(
+                    filter_source.count() * filter_source.dtype_size(), rmm::cuda_stream_per_thread);
 
-                    MRC_CHECK_CUDA(
-                        cudaMemcpy(tmp_buffer->data(), probs.data(), tmp_buffer->size(), cudaMemcpyDeviceToDevice));
+                MRC_CHECK_CUDA(
+                    cudaMemcpy(tmp_buffer->data(), filter_source.data(), tmp_buffer->size(), cudaMemcpyDeviceToDevice));
 
-                    // Depending on the input the stride is given in bytes or elements, convert to elements
-                    auto tensor_stride = TensorUtils::get_element_stride<TensorIndex, std::size_t>(stride);
+                // Depending on the input the stride is given in bytes or elements, convert to elements
+                auto tensor_stride = TensorUtils::get_element_stride<TensorIndex, std::size_t>(stride);
 
-                    // Now call the threshold function
-                    auto thresh_bool_buffer =
-                        MatxUtil::threshold(DevMemInfo{probs.count(), probs.dtype().type_id(), tmp_buffer, 0},
-                                            num_rows,
-                                            num_columns,
-                                            tensor_stride,
-                                            m_threshold,
-                                            true);
-                    std::vector<uint8_t> host_bool_values(num_rows);
+                // Now call the threshold function
+                auto thresh_bool_buffer = MatxUtil::threshold(
+                    DevMemInfo{filter_source.count(), filter_source.dtype().type_id(), tmp_buffer, 0},
+                    num_rows,
+                    num_columns,
+                    tensor_stride,
+                    m_threshold,
+                    true);
+                std::vector<uint8_t> host_bool_values(num_rows);
 
-                    // Copy bools back to host
-                    MRC_CHECK_CUDA(cudaMemcpy(host_bool_values.data(),
-                                              thresh_bool_buffer->data(),
-                                              thresh_bool_buffer->size(),
-                                              cudaMemcpyDeviceToHost));
+                // Copy bools back to host
+                MRC_CHECK_CUDA(cudaMemcpy(host_bool_values.data(),
+                                          thresh_bool_buffer->data(),
+                                          thresh_bool_buffer->size(),
+                                          cudaMemcpyDeviceToHost));
 
-                    // Only used when m_copy is true
-                    std::vector<std::pair<std::size_t, std::size_t>> selected_ranges;
-                    std::size_t num_selected_rows = 0;
+                // Only used when m_copy is true
+                std::vector<std::pair<std::size_t, std::size_t>> selected_ranges;
+                std::size_t num_selected_rows = 0;
 
-                    // We are slicing by rows, using num_rows as our marker for undefined
-                    std::size_t slice_start = num_rows;
-                    for (std::size_t row = 0; row < num_rows; ++row)
+                // We are slicing by rows, using num_rows as our marker for undefined
+                std::size_t slice_start = num_rows;
+                for (std::size_t row = 0; row < num_rows; ++row)
+                {
+                    bool above_threshold = host_bool_values[row];
+
+                    if (above_threshold && slice_start == num_rows)
                     {
-                        bool above_threshold = host_bool_values[row];
-
-                        if (above_threshold && slice_start == num_rows)
-                        {
-                            slice_start = row;
-                        }
-                        else if (!above_threshold && slice_start != num_rows)
-                        {
-                            if (m_copy)
-                            {
-                                selected_ranges.emplace_back(std::pair{slice_start, row});
-                                num_selected_rows += (row - slice_start);
-                            }
-                            else
-                            {
-                                output.on_next(x->get_slice(slice_start, row));
-                            }
-
-                            slice_start = num_rows;
-                        }
+                        slice_start = row;
                     }
-
-                    if (slice_start != num_rows)
+                    else if (!above_threshold && slice_start != num_rows)
                     {
-                        // Last row was above the threshold
                         if (m_copy)
                         {
-                            selected_ranges.emplace_back(std::pair{slice_start, num_rows});
-                            num_selected_rows += (num_rows - slice_start);
+                            selected_ranges.emplace_back(std::pair{slice_start, row});
+                            num_selected_rows += (row - slice_start);
                         }
                         else
                         {
-                            output.on_next(x->get_slice(slice_start, num_rows));
+                            output.on_next(x->get_slice(slice_start, row));
                         }
-                    }
 
-                    // num_selected_rows will always be 0 when m_copy is false,
-                    // or when m_copy is true, but none of the rows matched the output
-                    if (num_selected_rows > 0)
-                    {
-                        DCHECK(m_copy);
-                        output.on_next(x->copy_ranges(selected_ranges, num_selected_rows));
+                        slice_start = num_rows;
                     }
-                },
-                [&](std::exception_ptr error_ptr) { output.on_error(error_ptr); },
-                [&]() { output.on_completed(); }));
-        };
+                }
+
+                if (slice_start != num_rows)
+                {
+                    // Last row was above the threshold
+                    if (m_copy)
+                    {
+                        selected_ranges.emplace_back(std::pair{slice_start, num_rows});
+                        num_selected_rows += (num_rows - slice_start);
+                    }
+                    else
+                    {
+                        output.on_next(x->get_slice(slice_start, num_rows));
+                    }
+                }
+
+                // num_selected_rows will always be 0 when m_copy is false,
+                // or when m_copy is true, but none of the rows matched the output
+                if (num_selected_rows > 0)
+                {
+                    DCHECK(m_copy);
+                    output.on_next(x->copy_ranges(selected_ranges, num_selected_rows));
+                }
+            },
+            [&](std::exception_ptr error_ptr) { output.on_error(error_ptr); },
+            [&]() { output.on_completed(); }));
+    };
 }
 
 // ************ FilterDetectionStageInterfaceProxy ************* //
