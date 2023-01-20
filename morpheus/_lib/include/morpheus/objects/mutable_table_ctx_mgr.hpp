a16fa52c29082de12bf5eb166cea183f078e5b36
David Gardner <dagardner@nvidia.com>
2023-01-20 15:20:38 -0800

    Move MutableTableCtxMgr to its own compilation unit

---
+++ morpheus/_lib/include/morpheus/objects/mutable_table_ctx_mgr.hpp
@@ -0,0 +1,53 @@
+/**
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "morpheus/messages/meta.hpp"
+
+#include <pybind11/pytypes.h>  // for object
+
+#include <memory>   // for unique_ptr
+#include <utility>  // for move
+
+namespace morpheus {
+/**
+ * @addtogroup objects
+ * @{
+ * @file
+ */
+
+#pragma GCC visibility push(default)
+class MutableTableCtxMgr
+{
+  public:
+    MutableTableCtxMgr(MessageMeta& meta_msg);
+    pybind11::object enter();
+    void exit(const pybind11::object& type, const pybind11::object& value, const pybind11::object& traceback);
+
+    // Throws a useful exception when a user attempts to use this object as if it were the dataframe itself
+    void throw_usage_error(pybind11::args args, const pybind11::kwargs& kwargs);
+
+  private:
+    MessageMeta& m_meta_msg;
+    std::unique_ptr<MutableTableInfo> m_table;
+    std::unique_ptr<pybind11::object> m_py_table;
+};
+
+#pragma GCC visibility pop
+/** @} */  // end of group
+}  // namespace morpheus
---
+++ morpheus/_lib/src/objects/mutable_table_ctx_mgr.cpp
@@ -0,0 +1,39 @@
+#include "morpheus/objects/mutable_table_ctx_mgr.hpp"
+
+#include <pybind11/gil.h>
+
+namespace morpheus {
+
+namespace py = pybind11;
+
+/********** MutableTableCtxMgr **********/
+MutableTableCtxMgr::MutableTableCtxMgr(MessageMeta& meta_msg) :
+  m_meta_msg{meta_msg},
+  m_table{nullptr},
+  m_py_table{nullptr} {};
+
+py::object MutableTableCtxMgr::enter()
+{
+    // Release the GIL
+    py::gil_scoped_release no_gil;
+    m_table    = std::make_unique<MutableTableInfo>(std::move(m_meta_msg.get_mutable_info()));
+    m_py_table = std::make_unique<py::object>(std::move(m_table->checkout_obj()));
+    return *m_py_table;
+}
+
+void MutableTableCtxMgr::exit(const py::object& type, const py::object& value, const py::object& traceback)
+{
+    m_table->return_obj(std::move(*m_py_table.release()));
+    m_table.reset(nullptr);
+}
+
+void MutableTableCtxMgr::throw_usage_error(pybind11::args args, const pybind11::kwargs& kwargs)
+{
+    std::ostringstream err_msg;
+    err_msg << "Error attempting to use mutable_dataframe outside of context manager. Intended usage :\n";
+    err_msg << "with message_meta.mutable_dataframe() as df:\n";
+    err_msg << "    df['col'] = 5";
+    throw std::runtime_error(err_msg.str());
+}
+
+}  // namespace morpheus
