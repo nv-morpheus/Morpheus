a94783ceb4996fc71aba11f813aa1d449899dca9
David Gardner <dagardner@nvidia.com>
2023-01-23 15:00:43 -0800

    Add docstrings

--- morpheus/cli/default_command_hooks.py
+++ morpheus/cli/default_command_hooks.py
@@ -23,17 +23,24 @@ from morpheus.config import PipelineModes
 
 
 class DefaultCommandHooks:
+    """
+    Hooks for registering and collecting stages to execute as part of a morhpeus pipeline
+    """
 
     @hookimpl
     def morpheus_cli_collect_stage_names(self, mode: PipelineModes) -> typing.List[str]:
-
-        # Loop over the existing stage registry and return the names
+        """
+        Loop over the existing stage registry and return the names
+        """
         command_names = StageRegistry.get_registered_names(mode=mode)
 
         return command_names
 
     @hookimpl
     def morpheus_cli_make_stage_command(self, mode: PipelineModes, stage_name: str) -> click.Command:
+        """
+        Add a stage command to a pipeline mode based on info in the stage registry
+        """
 
         stage_info = StageRegistry.get_stage_info(stage_name=stage_name, mode=mode, raise_missing=False)
 
