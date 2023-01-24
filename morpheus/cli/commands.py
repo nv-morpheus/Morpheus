51a33d724793ddcabbd25ad4c87a5d20ee96e340
David Gardner <dagardner@nvidia.com>
2023-01-24 09:31:24 -0800

    Move click_completion_tools to cli

--- morpheus/cli/commands.py
+++ morpheus/cli/commands.py
@@ -19,6 +19,7 @@ import typing
 
 import click
 
+from morpheus.cli import click_completion_tools
 from morpheus.cli.plugin_manager import PluginManager
 from morpheus.cli.stage_registry import GlobalStageRegistry
 from morpheus.cli.stage_registry import LazyStageInfo
@@ -227,8 +228,6 @@ def autocomplete(**kwargs):
 def show(shell):
     """Show the click-completion-command completion code"""
 
-    from morpheus.utils import click_completion_tools
-
     shell, path, code = click_completion_tools.get_code(shell=shell)
 
     click.secho("To add %s completion, write the following code to '%s':\n" % (shell, path), fg="blue")
@@ -246,9 +245,6 @@ def show(shell):
               help="Location to install complete to. Leave empty to choose the default for the specified shell")
 def install(**kwargs):
     """Install the click-completion-command completion"""
-
-    from morpheus.utils import click_completion_tools
-
     shell, path = click_completion_tools.install_code(**kwargs)
 
     click.echo('%s completion installed in %s' % (shell, path))
