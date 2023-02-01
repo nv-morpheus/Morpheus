a29523533754bfc52645f5cc48c642b6cff83951
David Gardner <96306125+dagardner-nv@users.noreply.github.com>
2023-02-01 09:32:23 -0800

    Checking out a specific commit, requires a non-shallow checkout (#662)
    Authors:
      - David Gardner (https://github.com/dagardner-nv)
    
    Approvers:
      - Michael Demoret (https://github.com/mdemoret-nv)
    
    URL: https://github.com/nv-morpheus/Morpheus/pull/662

--- examples/developer_guide/4_rabbitmq_cpp_stage/cmake/Configure_rabbitmq.cmake
+++ examples/developer_guide/4_rabbitmq_cpp_stage/cmake/Configure_rabbitmq.cmake
@@ -32,7 +32,6 @@ function(find_and_configure_rabbitmq version)
     CPM_ARGS
       GIT_REPOSITORY  https://github.com/alanxz/rabbitmq-c
       GIT_TAG         7fa7b0b
-      GIT_SHALLOW     TRUE
       OPTIONS         "BUILD_EXAMPLES OFF"
                       "BUILD_TESTING OFF"
                       "BUILD_TOOLS OFF"
