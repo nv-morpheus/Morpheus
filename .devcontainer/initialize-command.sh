af6d829dfbf01cc85b1a47791928b51445254bd6
Christopher Harris <xixonia@gmail.com>
2023-02-17 18:26:11 +0000

    move .conda folder to parent dir

--- .devcontainer/initialize-command.sh
+++ .devcontainer/initialize-command.sh
@@ -18,7 +18,7 @@
 docker network inspect morpheus >/dev/null 2>&1 || docker network create morpheus
 
 # create the parent conda folder so it's found when mounting
-mkdir -p .cache/conda
+mkdir -p ../.conda
 
 # create a config directory if it does not exist so it's found when mounting
 mkdir -p ../.config
