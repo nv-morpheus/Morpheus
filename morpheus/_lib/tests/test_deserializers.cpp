25268173532f6787716a229bdf7c38ec76c9f1e2
David Gardner <dagardner@nvidia.com>
2023-02-23 12:44:36 -0800

    More tests

--- morpheus/_lib/tests/test_deserializers.cpp
+++ morpheus/_lib/tests/test_deserializers.cpp
@@ -19,6 +19,7 @@
 
 #include "morpheus/io/deserializers.hpp"
 
+#include <cudf/io/csv.hpp>
 #include <cudf/io/types.hpp>
 #include <gtest/gtest.h>
 
@@ -28,9 +29,14 @@
 
 using namespace morpheus;
 
+namespace {
+const std::string UserCols{",time,eventID,eventSource,eventName,z_loss\n"};
+const std::string DataRow{"0,1,2,test,test,5\n"};
+}  // namespace
+
 TEST_CLASS(Deserializers);
 
-TEST_F(TestDeserializers, GetIndexColCountNoIdx)
+TEST_F(TestDeserializers, GetIndexColCountNoIdxFromFile)
 {
     auto test_data_dir = test::get_morpheus_root() / "tests/tests_data";
 
@@ -44,10 +50,56 @@ TEST_F(TestDeserializers, GetIndexColCountNoIdx)
     }
 }
 
-TEST_F(TestDeserializers, GetIndexColCountWithIdx)
+TEST_F(TestDeserializers, GetIndexColCountWithIdxFromFile)
 {
     // now test a file with an index
     auto input_file = test::get_morpheus_root() / "tests/tests_data/filter_probs_w_id_col.csv";
     auto table      = load_table_from_file(input_file);
     EXPECT_EQ(get_index_col_count(table), 1);
+}
+
+TEST_F(TestDeserializers, GetIndexColCountNoIdxSimilarName)
+{
+    std::vector<std::string> not_id_cols{"identity", "Unnamed: 01", "test id", "email"};
+
+    for (const auto& col : not_id_cols)
+    {
+        // Build a list of column names with `col` as our first element followed by the columns in `UserCols`
+        std::string csv_data{col + UserCols + DataRow};
+
+        auto options = cudf::io::csv_reader_options::builder(cudf::io::source_info{csv_data.c_str(), csv_data.size()});
+        auto table   = cudf::io::read_csv(options.build());
+
+        EXPECT_EQ(get_index_col_count(table), 0);
+    }
+}
+
+TEST_F(TestDeserializers, GetIndexColCountIdx)
+{
+    // These should all match regardless of whitespace and case
+    std::vector<std::string> id_cols{
+        "Unnamed: 0", "UNNamed: 0", "Unnamed: 0\t", "\tUnnamed: 0\t", " Unnamed: 0", "ID", "id", "id ", " id", " iD "};
+
+    for (const auto& col : id_cols)
+    {
+        // Build a list of column names with `col` as our first element followed by the columns in `UserCols`
+        std::string csv_data{col + UserCols + DataRow};
+
+        auto options = cudf::io::csv_reader_options::builder(cudf::io::source_info{csv_data.c_str(), csv_data.size()});
+        auto table   = cudf::io::read_csv(options.build());
+
+        EXPECT_EQ(get_index_col_count(table), 1);
+    }
+}
+
+TEST_F(TestDeserializers, GetIndexColCountValidNameInvalidType)
+{
+    // Construct a csv with two rows, first row will contain the expected int id value second row will be a string
+    std::string data_rows{DataRow + "s,6,7,frog,toad,8\n"};
+    std::string csv_data{"id" + UserCols + data_rows};
+
+    auto options = cudf::io::csv_reader_options::builder(cudf::io::source_info{csv_data.c_str(), csv_data.size()});
+    auto table   = cudf::io::read_csv(options.build());
+
+    EXPECT_EQ(get_index_col_count(table), 0);
 }
