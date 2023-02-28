a39de93d3897c5988dbf158aef392dbf56a6d3dc
Devin Robison <drobison@nvidia.com>
2023-02-28 15:56:57 -0700

    Cleanup file_batcher module

--- morpheus/modules/file_batcher.py
+++ morpheus/modules/file_batcher.py
@@ -99,18 +99,50 @@ def file_batcher(builder: mrc.Builder):
         df["ts"] = timestamps
         df["key"] = full_names
 
-        return ts_and_files
+        return df
+
+    def generate_cms_for_batch_periods(control_message: MessageControl, period_gb, n_groups):
+        data_type = control_message.get_metadata("data_type")
+
+        control_messages = []
+        for group in period_gb.groups:
+            period_df = period_gb.get_group(group)
+            filenames = period_df["key"].to_list()
+
+            load_task = {
+                "loader_id": FILE_TO_DF_LOADER,
+                "strategy": "aggregate",
+                "files": filenames,
+                "n_groups": n_groups,
+                "batcher_config": {  # TODO(Devin): remove this
+                    "timestamp_column_name": config.get("timestamp_column_name"),
+                    "schema": config.get("schema"),
+                    "file_type": config.get("file_type"),
+                    "filter_null": config.get("filter_null"),
+                    "parser_kwargs": config.get("parser_kwargs"),
+                    "cache_dir": config.get("cache_dir")
+                }
+            }
+
+            if (data_type == "payload"):
+                control_message.add_task("load", load_task)
+            elif (data_type == "streaming"):
+                batch_control_message = control_message.copy()
+                batch_control_message.add_task("load", load_task)
+                control_messages.append(batch_control_message)
+            else:
+                raise Exception("Unknown data type")
+
+        if (data_type == "payload"):
+            control_messages.append(control_message)
+
+        return control_messages
 
     def on_data(control_message: MessageControl):
-        data_type = "streaming"
-        if (control_message.has_metadata("data_type")):
-            data_type = control_message.get_metadata("data_type")
-
         df = control_message.payload().df
         files = df.files.to_arrow().to_pylist()
         ts_filenames_df = build_fs_filename_df(files)
 
-        # TODO(Devin): Clean this up
         control_messages = []
         if len(ts_filenames_df) > 0:
             # Now split by the batching settings
@@ -119,36 +151,9 @@ def file_batcher(builder: mrc.Builder):
             n_groups = len(period_gb)
 
             logger.debug("Batching %d files => %d groups", len(ts_filenames_df), n_groups)
-            for group in period_gb.groups:
-                period_df = period_gb.get_group(group)
-                filenames = period_df["key"].to_list()
 
-                load_task = {
-                    "loader_id": FILE_TO_DF_LOADER,
-                    "strategy": "aggregate",
-                    "files": filenames,
-                    "n_groups": n_groups,
-                    "batcher_config": {  # TODO(Devin): remove this
-                        "timestamp_column_name": config.get("timestamp_column_name"),
-                        "schema": config.get("schema"),
-                        "file_type": config.get("file_type"),
-                        "filter_null": config.get("filter_null"),
-                        "parser_kwargs": config.get("parser_kwargs"),
-                        "cache_dir": config.get("cache_dir")
-                    }
-                }
-
-                if (data_type == "payload"):
-                    control_message.add_task("load", load_task)
-                elif (data_type == "streaming"):
-                    batch_control_message = control_message.copy()
-                    batch_control_message.add_task("load", load_task)
-                    control_messages.append(batch_control_message)
-                else:
-                    raise Exception("Unknown data type")
-
-        if (data_type == "payload"):
-            control_messages.append(control_message)
+            control_messages = generate_cms_for_batch_periods(control_message,
+                                                              period_gb, n_groups)
 
         return control_messages
 
