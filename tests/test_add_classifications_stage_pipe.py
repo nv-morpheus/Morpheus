3be102ae6b0d3c103ad060680b2f61b9adaf0479
David Gardner <dagardner@nvidia.com>
2023-03-09 11:21:58 -0800

    Use comp_stage

--- tests/test_add_classifications_stage_pipe.py
+++ tests/test_add_classifications_stage_pipe.py
@@ -16,34 +16,35 @@
 
 import os
 
-import numpy as np
-import pandas as pd
 import pytest
 
+from morpheus._lib.common import FileTypes
+from morpheus.io.deserializers import read_file_to_df
 from morpheus.messages import MessageMeta
 from morpheus.messages import MultiMessage
 from morpheus.messages import MultiResponseProbsMessage
 from morpheus.pipeline import LinearPipeline
 from morpheus.stages.input.file_source_stage import FileSourceStage
-from morpheus.stages.output.write_to_file_stage import WriteToFileStage
 from morpheus.stages.postprocess.add_classifications_stage import AddClassificationsStage
 from morpheus.stages.postprocess.serialize_stage import SerializeStage
 from morpheus.stages.preprocess.deserialize_stage import DeserializeStage
+from stages import CompareDataframeStage
 from stages import ConvMsg
 from utils import TEST_DIRS
-from utils import assert_path_exists
 
 
 @pytest.mark.slow
-def test_add_classifications_stage_pipe(config, tmp_path):
+def test_add_classifications_stage_pipe(config):
     config.class_labels = ['frogs', 'lizards', 'toads', 'turtles']
     config.num_threads = 1
 
-    # Silly data with all false values
-    input_file = os.path.join(TEST_DIRS.tests_data_dir, "filter_probs.csv")
-    out_file = os.path.join(tmp_path, 'results.csv')
-
     threshold = 0.75
+
+    input_file = os.path.join(TEST_DIRS.tests_data_dir, "filter_probs.csv")
+    input_df = read_file_to_df(input_file, df_type='pandas', file_type=FileTypes.Auto)
+    expected_df = (input_df > threshold)
+
+    comp_stage = CompareDataframeStage(config, expected_df)
 
     pipe = LinearPipeline(config)
     pipe.set_source(FileSourceStage(config, filename=input_file, iterative=False))
@@ -51,34 +52,28 @@ def test_add_classifications_stage_pipe(config, tmp_path):
     pipe.add_stage(ConvMsg(config, input_file))
     pipe.add_stage(AddClassificationsStage(config, threshold=threshold))
     pipe.add_stage(SerializeStage(config, include=["^{}$".format(c) for c in config.class_labels]))
-    pipe.add_stage(WriteToFileStage(config, filename=out_file, overwrite=False))
+    pipe.add_stage(comp_stage)
     pipe.run()
 
-    assert_path_exists(out_file)
-
-    input_data = np.loadtxt(input_file, delimiter=",", skiprows=1)
-    expected = (input_data > threshold)
-
-    # The output data will contain an additional id column that we will need to slice off
-    output_data = pd.read_csv(out_file)
-    idx = output_data.columns.intersection(config.class_labels)
+    results_df = comp_stage.concat_dataframes()
+    idx = results_df.columns.intersection(config.class_labels)
     assert idx.to_list() == config.class_labels
 
-    output_np = output_data[idx].to_numpy()
-
-    assert output_np.tolist() == expected.tolist()
+    comp_stage.get_results()["diff_rows"] == 0
 
 
 @pytest.mark.slow
-def test_add_classifications_stage_multi_segment_pipe(config, tmp_path):
+def test_add_classifications_stage_multi_segment_pipe(config):
     config.class_labels = ['frogs', 'lizards', 'toads', 'turtles']
     config.num_threads = 1
 
-    # Silly data with all false values
-    input_file = os.path.join(TEST_DIRS.tests_data_dir, "filter_probs.csv")
-    out_file = os.path.join(tmp_path, 'results.csv')
-
     threshold = 0.75
+
+    input_file = os.path.join(TEST_DIRS.tests_data_dir, "filter_probs.csv")
+    input_df = read_file_to_df(input_file, df_type='pandas', file_type=FileTypes.Auto)
+    expected_df = (input_df > threshold)
+
+    comp_stage = CompareDataframeStage(config, expected_df)
 
     pipe = LinearPipeline(config)
     pipe.set_source(FileSourceStage(config, filename=input_file, iterative=False))
@@ -91,19 +86,11 @@ def test_add_classifications_stage_multi_segment_pipe(config, tmp_path):
     pipe.add_segment_boundary(MultiResponseProbsMessage)
     pipe.add_stage(SerializeStage(config, include=["^{}$".format(c) for c in config.class_labels]))
     pipe.add_segment_boundary(MessageMeta)
-    pipe.add_stage(WriteToFileStage(config, filename=out_file, overwrite=False))
+    pipe.add_stage(comp_stage)
     pipe.run()
 
-    assert_path_exists(out_file)
-
-    input_data = np.loadtxt(input_file, delimiter=",", skiprows=1)
-    expected = (input_data > threshold)
-
-    # The output data will contain an additional id column that we will need to slice off
-    output_data = pd.read_csv(out_file)
-    idx = output_data.columns.intersection(config.class_labels)
+    results_df = comp_stage.concat_dataframes()
+    idx = results_df.columns.intersection(config.class_labels)
     assert idx.to_list() == config.class_labels
 
-    output_np = output_data[idx].to_numpy()
-
-    assert output_np.tolist() == expected.tolist()
+    comp_stage.get_results()["diff_rows"] == 0
