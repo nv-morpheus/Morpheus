2c0f2e56ee6d8d8b02aeb2ca3af0c7f0318d79c0
David Gardner <dagardner@nvidia.com>
2023-03-22 08:17:16 -0700

    Sort imports

--- morpheus/models/dfencoder/__init__.py
+++ morpheus/models/dfencoder/__init__.py
@@ -48,7 +48,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from .dataframe import *
 from .autoencoder import *
+from .dataframe import *
 from .logging import *
 from .scalers import *
