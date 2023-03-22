1c9da0adc813bb3bdc5f650e829e942d639f2486
David Gardner <dagardner@nvidia.com>
2023-03-22 16:00:37 -0700

    Use new memory stages

--- tests/test_filter_detections_stage_pipe.py
+++ tests/test_filter_detections_stage_pipe.py
@@ -17,69 +17,68 @@
 import os
 
 import numpy as np
+import pandas as pd
 import pytest
 
+import cudf
+
+from morpheus.io.deserializers import read_file_to_df
 from morpheus.messages import MessageMeta
 from morpheus.messages import MultiMessage
 from morpheus.messages import MultiResponseMessage
 from morpheus.pipeline import LinearPipeline
-from morpheus.stages.input.file_source_stage import FileSourceStage
+from morpheus.stages.input.in_memory_source_stage import InMemorySourceStage
+from morpheus.stages.output.compare_dataframe_stage import CompareDataframeStage
 from morpheus.stages.output.write_to_file_stage import WriteToFileStage
 from morpheus.stages.postprocess.filter_detections_stage import FilterDetectionsStage
 from morpheus.stages.postprocess.serialize_stage import SerializeStage
 from morpheus.stages.preprocess.deserialize_stage import DeserializeStage
 from stages.conv_msg import ConvMsg
 from utils import TEST_DIRS
-from utils import assert_path_exists
-from utils import extend_data
-from utils import get_column_names_from_file
+from utils import extend_df
+
+
+def build_expected(df: pd.DataFrame, threshold: float):
+    """
+    Takes a copy of `df` and apply the threshold
+    """
+    expected_df = df.copy(deep=True)
+    return expected_df[expected_df.max(axis=1) >= threshold]
 
 
 def _test_filter_detections_stage_pipe(config, tmp_path, copy=True, order='K', pipeline_batch_size=256, repeat=1):
     config.pipeline_batch_size = pipeline_batch_size
 
     src_file = os.path.join(TEST_DIRS.tests_data_dir, "filter_probs.csv")
-    out_file = os.path.join(tmp_path, 'results.csv')
+    input_df = read_file_to_df(src_file, df_type='pandas')
 
-    input_cols = get_column_names_from_file(src_file)
     if repeat > 1:
-        input_file = os.path.join(tmp_path, 'input.csv')
-        extend_data(src_file, input_file, repeat)
-    else:
-        input_file = src_file
+        input_df = extend_df(input_df, repeat)
 
     threshold = 0.75
 
     pipe = LinearPipeline(config)
-    pipe.set_source(FileSourceStage(config, filename=input_file, iterative=False))
+    pipe.set_source(InMemorySourceStage(config, [cudf.DataFrame(input_df)]))
     pipe.add_stage(DeserializeStage(config))
-    pipe.add_stage(ConvMsg(config, order=order, columns=input_cols))
+    pipe.add_stage(ConvMsg(config, order=order, columns=list(input_df.columns)))
     pipe.add_stage(FilterDetectionsStage(config, threshold=threshold, copy=copy))
     pipe.add_stage(SerializeStage(config))
-    pipe.add_stage(WriteToFileStage(config, filename=out_file, overwrite=False))
+    comp_stage = pipe.add_stage(CompareDataframeStage(config, build_expected(input_df, threshold)))
     pipe.run()
 
-    assert_path_exists(out_file)
-
-    input_data = np.loadtxt(input_file, delimiter=",", skiprows=1)
-    output_data = np.loadtxt(out_file, delimiter=",", skiprows=1)
-
-    # The output data will contain an additional id column that we will need to slice off
-    # also somehow 0.7 ends up being 0.7000000000000001
-    output_data = np.around(output_data[:, 1:], 2)
-
-    expected = input_data[np.any(input_data >= threshold, axis=1), :]
-    assert output_data.tolist() == expected.tolist()
+    results = comp_stage.get_results()
+    assert results["diff_cols"] == 0, f"Expected diff_cols=0 : {results}"
+    assert results["diff_rows"] == 0, f"Expected diff_rows=0 : {results}"
 
 
 def _test_filter_detections_stage_multi_segment_pipe(config, tmp_path, copy=True):
-    input_file = os.path.join(TEST_DIRS.tests_data_dir, "filter_probs.csv")
-    out_file = os.path.join(tmp_path, 'results.csv')
+    src_file = os.path.join(TEST_DIRS.tests_data_dir, "filter_probs.csv")
+    input_df = read_file_to_df(src_file, df_type='pandas')
 
     threshold = 0.75
 
     pipe = LinearPipeline(config)
-    pipe.set_source(FileSourceStage(config, filename=input_file, iterative=False))
+    pipe.set_source(InMemorySourceStage(config, [cudf.DataFrame(input_df)]))
     pipe.add_segment_boundary(MessageMeta)
     pipe.add_stage(DeserializeStage(config))
     pipe.add_segment_boundary(MultiMessage)
@@ -89,20 +88,12 @@ def _test_filter_detections_stage_multi_segment_pipe(config, tmp_path, copy=True
     pipe.add_segment_boundary(MultiResponseMessage)
     pipe.add_stage(SerializeStage(config))
     pipe.add_segment_boundary(MessageMeta)
-    pipe.add_stage(WriteToFileStage(config, filename=out_file, overwrite=False))
+    comp_stage = pipe.add_stage(CompareDataframeStage(config, build_expected(input_df, threshold)))
     pipe.run()
 
-    assert_path_exists(out_file)
-
-    input_data = np.loadtxt(input_file, delimiter=",", skiprows=1)
-    output_data = np.loadtxt(out_file, delimiter=",", skiprows=1)
-
-    # The output data will contain an additional id column that we will need to slice off
-    # also somehow 0.7 ends up being 0.7000000000000001
-    output_data = np.around(output_data[:, 1:], 2)
-
-    expected = input_data[np.any(input_data >= threshold, axis=1), :]
-    assert output_data.tolist() == expected.tolist()
+    results = comp_stage.get_results()
+    assert results["diff_cols"] == 0, f"Expected diff_cols=0 : {results}"
+    assert results["diff_rows"] == 0, f"Expected diff_rows=0 : {results}"
 
 
 @pytest.mark.slow
