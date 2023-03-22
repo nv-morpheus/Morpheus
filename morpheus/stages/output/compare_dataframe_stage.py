9d1897151dcf2cd3398a0bf67705d36ca58487d3
David Gardner <dagardner@nvidia.com>
2023-03-22 12:21:09 -0700

    Expose optional arguments from compare_df

--- morpheus/stages/output/compare_dataframe_stage.py
+++ morpheus/stages/output/compare_dataframe_stage.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import copy
 import typing
 
 import pandas as pd
@@ -31,6 +32,8 @@ class CompareDataframeStage(InMemorySinkStage):
     Collects incoming messages, comparing the concatinated dataframe of all messages against an expected dataframe
     `compare_df`.
 
+    If a column name is matched by both `include` and `exclude`, it will be excluded.
+
     Parameters
     ----------
     c : `morpheus.config.Config`
@@ -38,15 +41,40 @@ class CompareDataframeStage(InMemorySinkStage):
     compare_df : typing.Union[pd.DataFrame, str]
         Dataframe to compare against the aggregate Dataframe composed from the received messages. When `compare_df` is
         a string it is assumed to be a file path.
+    include : typing.List[str], optional
+        List of regex patterns to match columns to include. By default all columns are included.
+    exclude : typing.List[str], optional
+        List of regex patters to match columns to exclude. By default no columns are excluded.
+    index_col : str, optional
+        Whether to convert any column in the dataset to the index. Useful when the pipeline will change the index,
+        by default None.
+    abs_tol : float, default = 0.001
+        Absolute tolerance to use when comparing float columns.
+    rel_tol : float, default = 0.05
+        Relative tolerance to use when comparing float columns.
     """
 
-    def __init__(self, c: Config, compare_df: typing.Union[pd.DataFrame, str]):
+    def __init__(self,
+                 c: Config,
+                 compare_df: typing.Union[pd.DataFrame, str],
+                 include: typing.List[str] = None,
+                 exclude: typing.List[str] = None,
+                 index_col: str = None,
+                 abs_tol: float = 0.001,
+                 rel_tol: float = 0.005):
         super().__init__(c)
 
         if isinstance(compare_df, str):
             compare_df = read_file_to_df(compare_df, file_type=FileTypes.Auto, df_type='pandas')
 
         self._compare_df = compare_df
+
+        # Make copies of the arrays to prevent changes after the Regex is compiled
+        self._include_columns = copy.copy(include)
+        self._exclude_columns = copy.copy(exclude)
+        self._index_col = index_col
+        self._abs_tol = abs_tol
+        self._rel_tol = rel_tol
 
     @property
     def name(self) -> str:
@@ -81,4 +109,10 @@ class CompareDataframeStage(InMemorySinkStage):
 
         combined_df = self.concat_dataframes(clear=clear)
 
-        return compare_df.compare_df(self._compare_df, combined_df)
+        return compare_df.compare_df(self._compare_df,
+                                     combined_df,
+                                     include_columns=self._include_columns,
+                                     exclude_columns=self._exclude_columns,
+                                     replace_idx=self._index_col,
+                                     abs_tol=self._abs_tol,
+                                     rel_tol=self._rel_tol)
