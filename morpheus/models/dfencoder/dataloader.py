fd9bfc078ec40c59d9caea2cced3448c09ae72e0
Hsin Chen <hsinc@nvidia.com>
2023-03-25 07:41:22 +0000

    Add a dataset class that supports loading from a dataframe

--- morpheus/models/dfencoder/dataloader.py
+++ morpheus/models/dfencoder/dataloader.py
@@ -38,7 +38,7 @@ class DFEncoderDataLoader(DataLoader):
             yield data_d
 
 
-class DFEncoderDataset(Dataset):
+class DatasetFromPath(Dataset):
     def __init__(
         self,
         data_folder,
@@ -108,7 +108,7 @@ class DFEncoderDataset(Dataset):
     def __len__(self):
         """Returns the number of batches in the dataset.
         Under normal circumstances, `Dataset` loads/returns one sample at a time. However, to optimize the loading of
-        high-volume csv files, this class loads a batch of csv rows at a time. So this built-in len funciton needs to 
+        high-volume csv files, this class loads a batch of csv rows at a time. So this built-in len function needs to 
         return the batch count instead of sample count.
         """
         return int(np.ceil(self.len / self.batch_size))
@@ -195,15 +195,133 @@ class DFEncoderDataset(Dataset):
         return pd.concat(pdf for pdf in self.preloaded_data.values())
 
 
-def get_distributed_training_dataloader(
+class DatasetFromDataframe(Dataset):
+    def __init__(
+        self,
+        df,
+        batch_size,
+        preprocess_fn,
+        shuffle_rows_in_batch=True,
+    ):
+        """
+        A dataset class that slice a given dataframe into batches and applies preprocessing to each batch.
+        * This class is developed to match the interface of the DatasetFromPath class. 
+          As a result, unlike other common implementations of PyTorch datasets that return one row at a time and
+          let the higher level DataLoader batch the data, this class returns one batch at a time when `__getitem__`
+          is called. (Even if this limits the ability to fully shuffle the whole dataset.)
+
+        Args:
+            df (pandas dataframe): input dataframe used for the dataset
+            batch_size (int): the size of the batches to read the data in
+            preprocess_fn (function): a function to preprocess the data, which should take a pandas dataframe and
+                a boolean indicating whether to shuffle rows in batch or not, and return a dictionary containing
+                the preprocessed data
+            shuffle_rows_in_batch (bool): whether to shuffle the rows within each batch
+        """
+        self.df = df
+        self.preprocess_fn = preprocess_fn
+
+        self.len = len(self.df)
+        self.batch_size = batch_size
+        self.shuffle_rows_in_batch = shuffle_rows_in_batch
+
+    @property
+    def num_samples(self):
+        """Returns the number of samples in the dataset."""
+        return len(self.df)
+
+    def __len__(self):
+        """Returns the number of batches in the dataset.
+        Under normal circumstances, `Dataset` loads/returns one sample at a time. However, to oatch the behavior of the
+        DatasetFromPath class, this class returns a batch of data when queried. So this built-in len function needs to 
+        return the batch count instead of sample count.
+        """
+        return int(np.ceil(self.len / self.batch_size))
+
+    def __iter__(self):
+        """Iterates through the whole dataset by batch in order, without any shuffling."""
+        for i in range(len(self)):
+            yield self[i]
+
+    def __getitem__(self, idx):
+        """
+        Gets the item (batch) at the given index in the dataset.
+
+        Args:
+            idx (int): the index of the item to get
+
+        Returns:
+            dict: a dictionary containing the preprocessed data for the current batch
+        """
+        start = idx * self.batch_size
+        end = (idx + 1) * self.batch_size
+
+        data = self.df[start:end]
+        return self._preprocess(data, batch_index=idx)
+
+    def _get_data_from_filename(self, filename):
+        """
+        Returns the data from the given file as a pandas dataframe.
+
+        Args:
+            filename (str): The filename of the file to load
+
+        Returns:
+            pandas dataframe
+        """
+        if self.preloaded_data:
+            return self.preloaded_data[filename]
+        return self.load_data_fn(f"{self.data_folder}/{filename}")
+
+    def _preprocess(self, df, batch_index):
+        """
+        Preprocesses the given dataframe and returns a dictionary containing the preprocessed data.
+
+        Args:
+            df (pandas dataframe): the dataframe to preprocess.
+            batch_index (int): the index of the current batch.
+
+        Returns:
+            dict: a dictionary containing the preprocessed data for the current batch.
+        """
+        data = self.preprocess_fn(
+            df,
+            shuffle_rows_in_batch=self.shuffle_rows_in_batch,
+        )
+        return {"batch_index": batch_index, "data": data}
+
+
+def get_distributed_training_dataloader_from_path(
     model, data_folder, load_data_fn, rank, world_size, pin_memory=False, num_workers=0
 ):
-    dataset = DFEncoderDataset(
+    dataset = DatasetFromPath(
         data_folder,
         model.batch_size,
         model.preprocess_train_data,
         load_data_fn=load_data_fn,
     )
+    dataloader = get_distributed_training_dataloader_from_dataset(
+        dataset=dataset, rank=rank, world_size=world_size, pin_memory=pin_memory, num_workers=num_workers,
+    )
+    return dataloader
+
+def get_distributed_training_dataloader_from_df(
+    model, df, rank, world_size, pin_memory=False, num_workers=0
+):
+    dataset = DatasetFromDataframe(
+        df=df,
+        batch_size=model.batch_size,
+        preprocess_fn=model.preprocess_train_data,
+        shuffle_rows_in_batch=True,
+    )
+    dataloader = get_distributed_training_dataloader_from_dataset(
+        dataset=dataset, rank=rank, world_size=world_size, pin_memory=pin_memory, num_workers=num_workers,
+    )
+    return dataloader
+    
+def get_distributed_training_dataloader_from_dataset(
+    dataset, rank, world_size, pin_memory=False, num_workers=0
+):
     sampler = DistributedSampler(
         dataset, num_replicas=world_size, rank=rank, shuffle=True, drop_last=False
     )
@@ -218,13 +336,22 @@ def get_distributed_training_dataloader(
     )
     return dataloader
 
-def get_validation_dataset(model, data_folder, load_data_fn, preload_data_into_memory=True):
-    dataset = DFEncoderDataset(
+def get_validation_dataset_from_path(model, data_folder, load_data_fn, preload_data_into_memory=True):
+    dataset = DatasetFromPath(
         data_folder, 
         model.eval_batch_size, 
         model.preprocess_validation_data, 
         load_data_fn=load_data_fn,
         shuffle_rows_in_batch=False, 
         preload_data_into_memory=preload_data_into_memory,
+    )
+    return dataset
+
+def get_validation_dataset_from_df(model, df):
+    dataset = DatasetFromDataframe(
+        df=df,
+        batch_size=model.eval_batch_size,
+        preprocess_fn=model.preprocess_validation_data,
+        shuffle_rows_in_batch=False, 
     )
     return dataset
