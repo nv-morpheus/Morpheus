99a178d686db5492e0eaafd5cd834b605e1a061b
Michael Demoret <mdemoret@nvidia.com>
2023-03-28 23:39:32 -0600

    Cleaning up unnecessary changes

--- ci/scripts/github/test.sh
+++ ci/scripts/github/test.sh
@@ -69,12 +69,11 @@ done
 rapids-logger "Running Python tests"
 set +e
 
-python -I -m pytest -s -v --run_slow --run_kafka \
+python -I -m pytest --run_slow --run_kafka \
        --junit-xml=${REPORTS_DIR}/report_pytest.xml \
        --cov=morpheus \
        --cov-report term-missing \
-       --cov-report=xml:${REPORTS_DIR}/report_pytest_coverage.xml \
-       dfencoder
+       --cov-report=xml:${REPORTS_DIR}/report_pytest_coverage.xml
 
 PYTEST_RESULTS=$?
 TEST_RESULTS=$(($TEST_RESULTS+$PYTEST_RESULTS))
