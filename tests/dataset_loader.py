a8f5b84cdda974f624d897af99c6ffe8808cf278
David Gardner <dagardner@nvidia.com>
2023-04-04 15:38:58 -0700

    WIP: First pass, untested

---
+++ tests/dataset_loader.py
@@ -0,0 +1,102 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import typing
+
+import pandas as pd
+
+import cudf
+
+from morpheus.io.deserializers import read_file_to_df
+from utils import TEST_DIRS
+
+
+class DatasetLoader:
+    """
+    Helper class for loading and caching test datasets as DataFrames.
+
+    Parameters
+    ----------
+    df_type : typing.Literal['cudf', 'pandas']
+        Type of DataFrame to return unless otherwise explicitly specified.
+    """
+
+    __df_cache = {}  # {(df_type, path): DF}
+
+    def __init__(self, df_type: typing.Literal['cudf', 'pandas']) -> None:
+        self._default_df_type = df_type
+
+    def get_alt_df_type(self, df_type: typing.Literal['cudf', 'pandas']) -> typing.Literal['cudf', 'pandas']:
+        """Returns the other possible df type."""
+        return 'cudf' if df_type == 'pandas' else 'pandas'
+
+    def clear(self):
+        self.__df_cache.clear()
+
+    def get_df(self,
+               file_path: str,
+               df_type: typing.Literal['cudf', 'pandas'] = None,
+               relative_path: str = TEST_DIRS.tests_data_dir) -> typing.Union[cudf.DataFrame, pd.DataFrame]:
+        """
+        Fetch a DataFrame specified from `file_path` which is a file path relative to `relative_path` or is absolute if
+        None. If a DataFrame matching both the path and `df_type` has already been fetched, then a cached copy will be
+        returned.
+        """
+
+        if relative_path is not None:
+            full_path = os.path.join(relative_path, file_path)
+        else:
+            full_path = file_path
+
+        if df_type is None:
+            df_type = self._default_df_type
+
+        df = self.__df_cache.get((df_type, full_path))
+        if df is None:
+            # If it isn't in the cache, but we have a cached copy in another DF format use it instead of going to disk
+            alt_df_type = self.get_alt_df_type(df_type=df_type)
+            alt_df = self.__df_cache.get((alt_df_type, full_path))
+            if alt_df is not None:
+                if alt_df_type == 'cudf':
+                    df = alt_df.to_pandas()
+                else:
+                    df = cudf.DataFrame.from_pandas(alt_df)
+            else:
+                df = read_file_to_df(full_path, df_type=df_type)
+
+            self.__df_cache[(df_type, full_path)] = df
+
+        return df.copy(deep=True)
+
+    def __getitem__(self, item):
+        if not isinstance(item, tuple):
+            item = (item, )
+
+        return self.get_df(*item)
+
+    def _get_loader(self, df_type: typing.Literal['cudf', 'pandas']):
+        if self._default_df_type == df_type:
+            return self
+        else:
+            return DatasetLoader(df_type=df_type)
+
+    @property
+    def cudf(self):
+        return self._get_loader(df_type='cudf')
+
+    @property
+    def pandas(self):
+        return self._get_loader(df_type='pandas')
