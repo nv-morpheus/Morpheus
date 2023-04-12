f895ce3f234423cddf6235dbc3b731145b0b03eb
David Gardner <dagardner@nvidia.com>
2023-04-12 10:34:59 -0700

    wip

--- tests/examples/log_parsing/test_inference.py
+++ tests/examples/log_parsing/test_inference.py
@@ -46,6 +46,31 @@ def build_response_mem(messages_mod, log_test_data_dir: str):
     return messages_mod.ResponseMemoryLogParsing(count=count, **tensors)
 
 
+def build_inf_message(df: typing.Union[pd.DataFrame, cudf.DataFrame],
+                      mess_offset: int,
+                      mess_count: int,
+                      offset: int,
+                      count: int,
+                      num_cols: int = 2) -> MultiInferenceMessage:
+    tensor_length = offset + count
+    seq_ids = cp.zeros((tensor_length, 3), dtype=cp.uint32)
+    seq_ids[offset:offset + count, 0] = cp.arange(mess_offset, mess_offset + count, dtype=cp.uint32)
+    seq_ids[:, 2] = 42
+
+    meta = MessageMeta(df)
+    mem = InferenceMemoryNLP(count=tensor_length,
+                             input_ids=cp.zeros((tensor_length, num_cols), dtype=cp.float32),
+                             input_mask=cp.zeros((tensor_length, num_cols), dtype=cp.float32),
+                             seq_ids=seq_ids)
+
+    return MultiInferenceMessage(meta=meta,
+                                 mess_offset=mess_offset,
+                                 mess_count=mess_count,
+                                 memory=mem,
+                                 offset=offset,
+                                 count=count)
+
+
 def _check_worker(inference_mod: typing.Any, worker: _TritonInferenceWorker):
     assert isinstance(worker, _TritonInferenceWorker)
     assert isinstance(worker, inference_mod.TritonInferenceLogParsing)
@@ -81,28 +106,16 @@ def test_log_parsing_triton_inference_log_parsing_build_output_message(config: C
                                                                        filter_probs_df: typing.Union[pd.DataFrame,
                                                                                                      cudf.DataFrame],
                                                                        import_mod: typing.List[typing.Any],
-                                                                       mess_offset,
-                                                                       mess_count,
-                                                                       offset,
-                                                                       count):
+                                                                       mess_offset: int,
+                                                                       mess_count: int,
+                                                                       offset: int,
+                                                                       count: int):
     inference_mod = import_mod[0]
-    tensor_length = offset + count
-    seq_ids = cp.zeros((tensor_length, 3), dtype=cp.uint32)
-    seq_ids[offset:offset + count, 0] = cp.arange(mess_offset, mess_offset + count, dtype=cp.uint32)
-    seq_ids[:, 2] = 42
-
-    meta = MessageMeta(filter_probs_df)
-    input_mem = InferenceMemoryNLP(count=tensor_length,
-                                   input_ids=cp.zeros((tensor_length, 2), dtype=cp.float32),
-                                   input_mask=cp.ones((tensor_length, 2), dtype=cp.float32),
-                                   seq_ids=seq_ids)
-
-    input_msg = MultiInferenceMessage(meta=meta,
-                                      mess_offset=mess_offset,
-                                      mess_count=mess_count,
-                                      memory=input_mem,
-                                      offset=offset,
-                                      count=count)
+    input_msg = build_inf_message(filter_probs_df,
+                                  mess_offset=mess_offset,
+                                  mess_count=mess_count,
+                                  offset=offset,
+                                  count=count)
 
     worker = inference_mod.TritonInferenceLogParsing(inf_queue=ProducerConsumerQueue(),
                                                      c=config,
@@ -116,7 +129,7 @@ def test_log_parsing_triton_inference_log_parsing_build_output_message(config: C
     worker._inputs['test'] = mock_inout
 
     msg = worker.build_output_message(input_msg)
-    assert msg.meta is meta
+    assert msg.meta is input_msg.meta
     assert msg.mess_offset == mess_offset
     assert msg.mess_count == count
     assert msg.offset == 0
@@ -177,25 +190,53 @@ def test_log_parsing_inference_stage_get_inference_worker(config: Config, import
     os.path.join(TEST_DIRS.examples_dir, 'log_parsing', 'inference.py'),
     os.path.join(TEST_DIRS.examples_dir, 'log_parsing', 'messages.py')
 ])
-@pytest.mark.parametrize("mess_offset,mess_count,offset,count", [(0, 20, 0, 20), (5, 10, 5, 10)])
+@pytest.mark.parametrize("mess_offset,mess_count,offset,count", [(0, 5, 0, 5), (5, 5, 5, 5)])
 def test_log_parsing_inference_stage_convert_one_response(config: Config,
                                                           import_mod: typing.List[typing.Any],
+                                                          filter_probs_df: typing.Union[pd.DataFrame, cudf.DataFrame],
                                                           mess_offset,
                                                           mess_count,
                                                           offset,
                                                           count):
     inference_mod, messages_mod = import_mod
-    stage = inference_mod.LogParsingInferenceStage(
-        config,
-        model_name='test_model',
-        server_url='test_server',
-        force_convert_inputs=False,
-        use_shared_memory=False,
-    )
 
-    input_mem = InferenceMemoryNLP(count=count,
-                                   input_ids=cp.zeros((count, 2), dtype=cp.float32),
-                                   input_mask=cp.zeros((count, 2), dtype=cp.float32),
-                                   seq_ids=cp.zeros((count, 3), dtype=cp.uint32))
+    ttl_count = len(filter_probs_df)
 
     input_res = build_response_mem(messages_mod, os.path.join(TEST_DIRS.tests_data_dir, 'log_parsing'))
+
+    # confidences, labels & input_ids all have the same shape
+    num_cols = input_res.confidences.shape[1]
+    input_mem = messages_mod.PostprocMemoryLogParsing(
+        count=ttl_count,
+        confidences=cp.zeros((ttl_count, num_cols), dtype=cp.float32),
+        input_ids=cp.zeros((ttl_count, num_cols), dtype=cp.float32),
+        labels=cp.zeros((ttl_count, num_cols), dtype=cp.float32),
+        seq_ids=cp.zeros((ttl_count, 3), dtype=cp.uint32),
+    )
+
+    input_inf = build_inf_message(filter_probs_df,
+                                  mess_offset=mess_offset,
+                                  mess_count=mess_count,
+                                  offset=offset,
+                                  count=count,
+                                  num_cols=num_cols)
+
+    output_msg = inference_mod.LogParsingInferenceStage._convert_one_response(input_mem, input_inf, input_res)
+
+    assert isinstance(output_msg, messages_mod.MultiPostprocLogParsingMessage)
+    assert output_msg.meta is input_inf.meta
+    assert output_msg.memory is input_mem
+    assert output_msg.mess_offset == mess_offset
+    assert output_msg.mess_count == mess_count
+    assert output_msg.offset == offset
+    assert output_msg.count == count
+
+    assert (output_msg.seq_ids == input_inf.seq_ids).all()
+    assert (output_msg.confidences == input_res.confidences).all()
+    assert (output_msg.labels == input_res.labels).all()
+
+    # Ensure we didn't write to the memory outside of the [offset:offset+count] bounds
+    tensors = input_mem.get_tensors()
+    for (tensor_name, tensor) in tensors.items():
+        assert (tensor[0:offset] == 0).all(), f"Out of bounds values for {tensor_name}"
+        assert (tensor[offset + count:] == 0).all(), f"Out of bounds values for {tensor_name}"
