cd6ecfdc83372d4b18f98afb3eac8ae0d1eab1af
David Gardner <dagardner@nvidia.com>
2023-04-20 14:44:45 -0700

    wip

--- tests/examples/ransomware_detection/test_create_features.py
+++ tests/examples/ransomware_detection/test_create_features.py
@@ -26,19 +26,38 @@ import cudf
 from morpheus.config import Config
 from morpheus.messages import MessageMeta
 from morpheus.messages import MultiMessage
+from morpheus.pipeline.multi_message_stage import MultiMessageStage
 from utils import TEST_DIRS
 
 
 def test_constructor(config: Config, dask_distributed: types.ModuleType, rwd_conf: dict):
+    from common.data_models import FeatureConfig
+    from common.feature_extractor import FeatureExtractor
     from stages.create_features import CreateFeaturesRWStage
 
     interested_plugins = ['ldrmodules', 'threadlist', 'envars', 'vadinfo', 'handles']
 
+    n_workers = 12
+    threads_per_worker = 8
     stage = CreateFeaturesRWStage(config,
                                   interested_plugins=interested_plugins,
                                   feature_columns=rwd_conf['model_features'],
                                   file_extns=rwd_conf['file_extensions'],
-                                  n_workers=12,
-                                  threads_per_worker=8)
+                                  n_workers=n_workers,
+                                  threads_per_worker=threads_per_worker)
 
+    assert isinstance(stage, MultiMessageStage)
     assert isinstance(stage._client, dask_distributed.Client)
+    scheduler_info = stage._client.scheduler_info()
+    len(scheduler_info['workers']) == n_workers
+    for worker in scheduler_info['workers'].values():
+        assert worker['nthreads'] == threads_per_worker
+
+    assert isinstance(stage._feature_config, FeatureConfig)
+    assert stage._feature_config.file_extns == rwd_conf['file_extensions']
+    assert stage._feature_config.interested_plugins == interested_plugins
+
+    assert stage._feas_all_zeros == {c: 0 for c in rwd_conf['model_features']}
+
+    assert isinstance(stage._fe, FeatureExtractor)
+    assert stage._fe._config is stage._feature_config
