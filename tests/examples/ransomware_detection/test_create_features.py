efd91faf344545222cee6e322e758419256a7ef9
David Gardner <dagardner@nvidia.com>
2023-04-21 13:48:11 -0700

    Tests for create_features.py

--- tests/examples/ransomware_detection/test_create_features.py
+++ tests/examples/ransomware_detection/test_create_features.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import glob
 import os
 import types
 import typing
@@ -24,9 +25,11 @@ import pytest
 
 from morpheus.config import Config
 from morpheus.messages import MultiMessage
+from morpheus.messages.message_meta import AppShieldMessageMeta
 from morpheus.pipeline.multi_message_stage import MultiMessageStage
-from morpheus.stages.input.appshield_source_stage import AppShieldMessageMeta
+from morpheus.stages.input.appshield_source_stage import AppShieldSourceStage
 from utils import TEST_DIRS
+from utils.dataset_manager import DatasetManager
 
 
 @pytest.mark.use_python
@@ -65,6 +68,60 @@ class TestCreateFeaturesRWStage:
 
         assert isinstance(stage._fe, FeatureExtractor)
         assert stage._fe._config is stage._feature_config
+
+    @mock.patch('stages.create_features.Client')
+    def test_on_next(self,
+                     mock_dask_client,
+                     config: Config,
+                     rwd_conf: dict,
+                     interested_plugins: typing.List[str],
+                     dataset_pandas: DatasetManager):
+        from stages.create_features import CreateFeaturesRWStage
+
+        test_data_dir = os.path.join(TEST_DIRS.tests_data_dir, 'examples/ransomware_detection')
+
+        mock_dask_client.return_value = mock_dask_client
+        mock_dask_client.map.return_value = mock.MagicMock()
+
+        dask_results = dataset_pandas[os.path.join(test_data_dir, 'dask_results.csv')]
+
+        mock_dask_future = mock.MagicMock()
+        mock_dask_future.result.return_value = dask_results
+        mock_dask_client.submit.return_value = mock_dask_future
+
+        input_glob = os.path.join(TEST_DIRS.tests_data_dir, 'appshield', 'snapshot-1', '*.json')
+        input_data = AppShieldSourceStage.files_to_dfs(glob.glob(input_glob),
+                                                       cols_include=rwd_conf['raw_columns'],
+                                                       cols_exclude=["SHA256"],
+                                                       plugins_include=interested_plugins,
+                                                       encoding='latin1')
+
+        input_metas = AppShieldSourceStage._build_metadata(input_data)
+
+        # Make sure the input test date looks the way we expect it
+        assert len(input_metas) == 1
+        input_meta = input_metas[0]
+        assert input_meta.source == 'appshield'
+
+        stage = CreateFeaturesRWStage(config,
+                                      interested_plugins=interested_plugins,
+                                      feature_columns=rwd_conf['model_features'],
+                                      file_extns=rwd_conf['file_extensions'],
+                                      n_workers=5,
+                                      threads_per_worker=6)
+
+        # make sure we have a mocked dask client
+        assert stage._client is mock_dask_client
+
+        meta = stage.on_next(input_meta)
+        assert isinstance(meta, AppShieldMessageMeta)
+        assert meta.source == input_meta.source
+
+        expected_df = dataset_pandas[os.path.join(test_data_dir, 'dask_results.csv')]
+        expected_df['source_pid_process'] = 'appshield_' + expected_df.pid_process
+        expected_df.sort_values(by=["pid_process", "snapshot_id"], inplace=True)
+        expected_df.reset_index(drop=True, inplace=True)
+        dataset_pandas.assert_compare_df(meta.copy_dataframe(), expected_df)
 
     @mock.patch('stages.create_features.Client')
     def test_create_multi_messages(self,
