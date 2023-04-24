e05717a11eb189f4c6c52de113632692352647e2
David Gardner <dagardner@nvidia.com>
2023-04-24 13:16:23 -0700

    Use new StellarGraph.from_networkx instead of deprecated constructor usage

--- examples/gnn_fraud_detection_pipeline/stages/graph_construction_stage.py
+++ examples/gnn_fraud_detection_pipeline/stages/graph_construction_stage.py
@@ -90,8 +90,7 @@ class FraudGraphConstructionStage(SinglePortStage):
         for edge in edges:
             g_nx.add_edges_from(edge)
 
-        # TODO: StellarGraph.from_networkx
-        return StellarGraph(g_nx, node_type_name="ntype", node_features=node_features)
+        return StellarGraph.from_networkx(g_nx, node_type_attr='ntype', node_features=node_features)
 
     @staticmethod
     def _build_graph_features(dataset: pd.DataFrame) -> "stellargraph.StellarGraph":
