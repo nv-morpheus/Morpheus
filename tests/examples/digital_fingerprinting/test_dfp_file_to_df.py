1e0bd81d166bffb4d1968bc936b006311d995975
David Gardner <dagardner@nvidia.com>
2023-04-28 13:26:04 -0700

    wip

--- tests/examples/digital_fingerprinting/test_dfp_file_to_df.py
+++ tests/examples/digital_fingerprinting/test_dfp_file_to_df.py
@@ -13,13 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import functools
+import hashlib
 import json
 import os
-import re
-import types
-from datetime import datetime
-from datetime import timezone
+from functools import partial
 from unittest import mock
 
 import fsspec
@@ -161,10 +158,12 @@ def test_close_dask_cluster_noop(mock_dask_cluster: mock.MagicMock, config: Conf
 @pytest.mark.parametrize('dl_type', ["single_thread", "multiprocess", "dask", "dask_thread"])
 @mock.patch('multiprocessing.get_context')
 @mock.patch('dask.config')
+@mock.patch('dfp.stages.dfp_file_to_df.Client')
 @mock.patch('dfp.stages.dfp_file_to_df.LocalCluster')
 @mock.patch('dfp.stages.dfp_file_to_df._single_object_to_dataframe')
 def test_get_or_create_dataframe_from_s3_batch_cache_miss(mock_obf_to_df: mock.MagicMock,
                                                           mock_dask_cluster: mock.MagicMock,
+                                                          mock_dask_client: mock.MagicMock,
                                                           mock_dask_config: mock.MagicMock,
                                                           mock_mp_gc: mock.MagicMock,
                                                           config: Config,
@@ -173,37 +172,81 @@ def test_get_or_create_dataframe_from_s3_batch_cache_miss(mock_obf_to_df: mock.M
                                                           single_file_obj: fsspec.core.OpenFile,
                                                           dataset_pandas: DatasetManager):
     from dfp.stages.dfp_file_to_df import DFPFileToDataFrameStage
-
-    # We're going to feed the function a file object pointing to a different file than the one we are going to return
-    # from out mocked fetch function. This way we will be able to easily tell if our mocks are working
-    mock_obf_to_df.return_value = dataset_pandas['filter_probs.csv']
+    config.ae.timestamp_column_name = 'v1'
     mock_dask_cluster.return_value = mock_dask_cluster
+    mock_dask_client.return_value = mock_dask_client
+    mock_dask_client.__enter__.return_value = mock_dask_client
+    mock_dask_client.__exit__.return_value = False
 
     mock_mp_gc.return_value = mock_mp_gc
-    mock_pool = mock.MagicMock()
-    mock_mp_gc.Pool.return_value = mock_pool
+    mock_mp_pool = mock.MagicMock()
+    mock_mp_gc.Pool.return_value = mock_mp_pool
+    mock_mp_pool.return_value = mock_mp_pool
+    mock_mp_pool.__enter__.return_value = mock_mp_pool
+    mock_mp_pool.__exit__.return_value = False
+
+    expected_hash = hashlib.md5(json.dumps([{
+        'ukey': single_file_obj.fs.ukey(single_file_obj.path)
+    }]).encode()).hexdigest()
+
+    expected_df = dataset_pandas['filter_probs.csv']
+    expected_df.sort_values(by=['v1'], inplace=True)
+    expected_df.reset_index(drop=True, inplace=True)
+    expected_df['batch_count'] = 1
+    expected_df["origin_hash"] = expected_hash
+
+    # We're going to feed the function a file object pointing to a different file than the one we are going to return
+    # from out mocked fetch function. This way we will be able to easily tell if our mocks are working. Mostly we just
+    # want to make sure that we aren't accidentally spinning up dask clusters or process pools in CI
+    returnd_df = dataset_pandas['filter_probs.csv']
+    if dl_type.startswith('dask'):
+        mock_dask_client.gather.return_value = [returnd_df]
+    elif dl_type == "multiprocess":
+        mock_mp_pool.map.return_value = [returnd_df]
+    else:
+        mock_obf_to_df.return_value = returnd_df
 
     os.environ['MORPHEUS_FILE_DOWNLOAD_TYPE'] = dl_type
     stage = DFPFileToDataFrameStage(config, DataFrameInputSchema(), cache_dir=tmp_path)
 
-    results = stage._get_or_create_dataframe_from_s3_batch((single_file_obj, 1))
+    batch = fsspec.core.OpenFiles([single_file_obj], fs=single_file_obj.fs)
+    (output_df, cache_hit) = stage._get_or_create_dataframe_from_s3_batch((batch, 1))
 
     if dl_type == "multiprocess":
         mock_mp_gc.assert_called_once()
-        mock_pool.map.assert_called_once()
+        mock_mp_pool.map.assert_called_once()
     else:
         mock_mp_gc.assert_not_called()
-        mock_pool.map.assert_not_called()
+        mock_mp_pool.map.assert_not_called()
+
+    if dl_type == "single_thread":
+        mock_obf_to_df.assert_called_once()
+    else:
+        mock_obf_to_df.assert_not_called()
+
+    if dl_type.startswith('dask'):
+        mock_dask_client.assert_called_once_with(mock_dask_cluster)
+        mock_dask_client.map.assert_called_once()
+        mock_dask_client.gather.assert_called_once()
+    else:
+        mock_dask_cluster.assert_not_called()
+        mock_dask_client.assert_not_called()
+        mock_dask_config.assert_not_called()
+
+    assert not cache_hit
+    dataset_pandas.assert_df_equal(output_df, expected_df)
 
 
 @pytest.mark.restore_environ
 @pytest.mark.parametrize('dl_type', ["single_thread", "multiprocess", "dask", "dask_thread"])
 @mock.patch('multiprocessing.get_context')
 @mock.patch('dask.config')
+@mock.patch('dfp.stages.dfp_file_to_df.Client')
 @mock.patch('dfp.stages.dfp_file_to_df.LocalCluster')
 @mock.patch('dfp.stages.dfp_file_to_df._single_object_to_dataframe')
 def test_get_or_create_dataframe_from_s3_batch_none_noop(mock_obf_to_df: mock.MagicMock,
                                                          mock_dask_cluster: mock.MagicMock,
+                                                         mock_dask_client: mock.MagicMock,
                                                          mock_dask_config: mock.MagicMock,
                                                          mock_mp_gc: mock.MagicMock,
                                                          config: Config,
@@ -211,10 +254,11 @@ def test_get_or_create_dataframe_from_s3_batch_none_noop(mock_obf_to_df: mock.Ma
                                                          tmp_path: str):
     from dfp.stages.dfp_file_to_df import DFPFileToDataFrameStage
     mock_dask_cluster.return_value = mock_dask_cluster
+    mock_dask_client.return_value = mock_dask_client
 
     mock_mp_gc.return_value = mock_mp_gc
-    mock_pool = mock.MagicMock()
-    mock_mp_gc.Pool.return_value = mock_pool
+    mock_mp_pool = mock.MagicMock()
+    mock_mp_gc.Pool.return_value = mock_mp_pool
 
     os.environ['MORPHEUS_FILE_DOWNLOAD_TYPE'] = dl_type
     stage = DFPFileToDataFrameStage(config, DataFrameInputSchema(), cache_dir=tmp_path)
@@ -222,8 +266,9 @@ def test_get_or_create_dataframe_from_s3_batch_none_noop(mock_obf_to_df: mock.Ma
 
     mock_obf_to_df.assert_not_called()
     mock_dask_cluster.assert_not_called()
+    mock_dask_client.assert_not_called()
     mock_dask_config.assert_not_called()
     mock_mp_gc.assert_not_called()
-    mock_pool.map.assert_not_called()
+    mock_mp_pool.map.assert_not_called()
 
     assert os.listdir(tmp_path) == []
