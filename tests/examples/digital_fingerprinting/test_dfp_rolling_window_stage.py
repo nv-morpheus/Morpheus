a5b4229c4457cc1941c15d5677969a57f130a1c7
David Gardner <dagardner@nvidia.com>
2023-05-02 12:30:05 -0700

    wip

--- tests/examples/digital_fingerprinting/test_dfp_rolling_window_stage.py
+++ tests/examples/digital_fingerprinting/test_dfp_rolling_window_stage.py
@@ -26,6 +26,17 @@ from utils import TEST_DIRS
 from utils.dataset_manager import DatasetManager
 
 
+@pytest.fixture
+def dfp_message_meta(config: Config, dataset_pandas: DatasetManager):
+    from dfp.messages.multi_dfp_message import DFPMessageMeta
+
+    user_id = 'test_user'
+    df = dataset_pandas['filter_probs.csv']
+    df[config.ae.timestamp_column_name] = [1683054498 + i for i in range(0, len(df) * 100, 100)]
+    df['user_id'] = user_id
+    yield DFPMessageMeta(df, user_id)
+
+
 def test_constructor(config: Config):
     from dfp.stages.dfp_rolling_window_stage import DFPRollingWindowStage
 
@@ -65,3 +76,27 @@ def test_get_user_cache_miss(config: Config):
 
     with stage._get_user_cache('test_user') as results2:
         results2 is results
+
+
+@pytest.mark.parametrize('use_on_data', [True, False])
+def test_build_window_no_new(
+        config: Config,
+        use_on_data: bool,
+        dfp_message_meta: "DFPMessageMeta"  # noqa: F821
+):
+    from dfp.messages.multi_dfp_message import MultiDFPMessage
+    from dfp.stages.dfp_rolling_window_stage import DFPRollingWindowStage
+
+    stage = DFPRollingWindowStage(config, min_history=5, min_increment=7, max_history=100, cache_dir='/test/path/cache')
+
+    mock_cache = mock.MagicMock()
+    mock_cache.append_dataframe.return_value = False
+    stage._user_cache_map[dfp_message_meta.user_id] = mock_cache
+
+    # on_data is a thin wrapper around _build_window, results should be the same
+    if use_on_data:
+        results = stage.on_data(dfp_message_meta)
+    else:
+        results = stage._build_window(dfp_message_meta)
+
+    assert results is None
