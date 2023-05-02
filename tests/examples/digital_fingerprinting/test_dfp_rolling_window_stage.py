e2e883b5528cc13be5111bdbb005253e81031db6
David Gardner <dagardner@nvidia.com>
2023-05-02 14:19:25 -0700

    Add test for build_window

--- tests/examples/digital_fingerprinting/test_dfp_rolling_window_stage.py
+++ tests/examples/digital_fingerprinting/test_dfp_rolling_window_stage.py
@@ -13,16 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 import os
-import typing
 from unittest import mock
 
+import pandas as pd
 import pytest
 
 from morpheus.config import Config
 from morpheus.pipeline.single_port_stage import SinglePortStage
-from utils import TEST_DIRS
 from utils.dataset_manager import DatasetManager
 
 
@@ -35,6 +33,22 @@ def dfp_message_meta(config: Config, dataset_pandas: DatasetManager):
     df[config.ae.timestamp_column_name] = [1683054498 + i for i in range(0, len(df) * 100, 100)]
     df['user_id'] = user_id
     yield DFPMessageMeta(df, user_id)
+
+
+def build_mock_user_cache(user_id: str = 'test_user',
+                          train_df: pd.DataFrame = None,
+                          count: int = 10,
+                          total_count: int = 20,
+                          last_train_count: int = 10) -> mock.MagicMock:
+    mock_cache = mock.MagicMock()
+    mock_cache.user_id = user_id
+    mock_cache.append_dataframe.return_value = True
+    mock_cache.get_train_df.return_value = train_df
+    mock_cache.count = count
+    mock_cache.total_count = total_count
+    mock_cache.last_train_count = last_train_count
+
+    return mock_cache
 
 
 def test_constructor(config: Config):
@@ -54,7 +68,7 @@ def test_get_user_cache_hit(config: Config):
 
     stage = DFPRollingWindowStage(config, min_history=5, min_increment=7, max_history=100, cache_dir='/test/path/cache')
 
-    mock_cache = mock.MagicMock()
+    mock_cache = build_mock_user_cache()
     stage._user_cache_map['test_user'] = mock_cache
 
     with stage._get_user_cache('test_user') as user_cache:
@@ -78,25 +92,117 @@ def test_get_user_cache_miss(config: Config):
         results2 is results
 
 
-@pytest.mark.parametrize('use_on_data', [True, False])
 def test_build_window_no_new(
         config: Config,
         use_on_data: bool,
         dfp_message_meta: "DFPMessageMeta"  # noqa: F821
 ):
+    from dfp.stages.dfp_rolling_window_stage import DFPRollingWindowStage
+
+    stage = DFPRollingWindowStage(config, min_history=5, min_increment=7, max_history=100, cache_dir='/test/path/cache')
+
+    mock_cache = build_mock_user_cache()
+    mock_cache.append_dataframe.return_value = False
+    stage._user_cache_map[dfp_message_meta.user_id] = mock_cache
+    stage._build_window(dfp_message_meta) is None
+
+
+def test_build_window_not_enough_data(
+        config: Config,
+        use_on_data: bool,
+        dfp_message_meta: "DFPMessageMeta"  # noqa: F821
+):
+    from dfp.stages.dfp_rolling_window_stage import DFPRollingWindowStage
+
+    stage = DFPRollingWindowStage(config, min_history=5, min_increment=7, max_history=100, cache_dir='/test/path/cache')
+
+    mock_cache = build_mock_user_cache(count=3)
+    stage._user_cache_map[dfp_message_meta.user_id] = mock_cache
+    stage._build_window(dfp_message_meta) is None
+
+
+def test_build_window_min_increment(
+        config: Config,
+        use_on_data: bool,
+        dfp_message_meta: "DFPMessageMeta"  # noqa: F821
+):
+    from dfp.stages.dfp_rolling_window_stage import DFPRollingWindowStage
+
+    stage = DFPRollingWindowStage(config, min_history=5, min_increment=7, max_history=100, cache_dir='/test/path/cache')
+
+    mock_cache = build_mock_user_cache(count=5, total_count=30, last_train_count=25)
+    stage._user_cache_map[dfp_message_meta.user_id] = mock_cache
+    stage._build_window(dfp_message_meta) is None
+
+
+@pytest.mark.parametrize('use_on_data', [True, False])
+def test_build_window_invalid(
+        config: Config,
+        use_on_data: bool,
+        dfp_message_meta: "DFPMessageMeta"  # noqa: F821
+):
+    from dfp.stages.dfp_rolling_window_stage import DFPRollingWindowStage
+
+    stage = DFPRollingWindowStage(config, min_history=5, min_increment=7, max_history=100, cache_dir='/test/path/cache')
+
+    train_df = dfp_message_meta.copy_dataframe()
+    # exact values not important so long as they don't match the actual hash
+    train_df['_row_hash'] = [-1 for _ in range(len(train_df))]
+
+    mock_cache = build_mock_user_cache(train_df=train_df)
+    stage._user_cache_map[dfp_message_meta.user_id] = mock_cache
+
+    with pytest.raises(RuntimeError):
+        stage._build_window(dfp_message_meta)
+
+
+def test_build_window_overlap(
+        config: Config,
+        use_on_data: bool,
+        dfp_message_meta: "DFPMessageMeta"  # noqa: F821
+):
+    from dfp.stages.dfp_rolling_window_stage import DFPRollingWindowStage
+
+    stage = DFPRollingWindowStage(config, min_history=5, min_increment=7, max_history=100, cache_dir='/test/path/cache')
+
+    # Create an overlap
+    train_df = dfp_message_meta.copy_dataframe()[-5:]
+    train_df['_row_hash'] = pd.util.hash_pandas_object(train_df, index=False)
+
+    mock_cache = build_mock_user_cache(train_df=train_df)
+    stage._user_cache_map[dfp_message_meta.user_id] = mock_cache
+
+    with pytest.raises(RuntimeError):
+        stage._build_window(dfp_message_meta)
+
+
+@pytest.mark.parametrize('use_on_data', [True, False])
+def test_build_window(
+        config: Config,
+        use_on_data: bool,
+        dfp_message_meta: "DFPMessageMeta",  # noqa: F821
+        dataset_pandas: DatasetManager):
     from dfp.messages.multi_dfp_message import MultiDFPMessage
     from dfp.stages.dfp_rolling_window_stage import DFPRollingWindowStage
 
     stage = DFPRollingWindowStage(config, min_history=5, min_increment=7, max_history=100, cache_dir='/test/path/cache')
 
-    mock_cache = mock.MagicMock()
-    mock_cache.append_dataframe.return_value = False
+    # Create an overlap
+    train_df = dfp_message_meta.copy_dataframe()
+    train_df['_row_hash'] = pd.util.hash_pandas_object(train_df, index=False)
+
+    mock_cache = build_mock_user_cache(train_df=train_df)
     stage._user_cache_map[dfp_message_meta.user_id] = mock_cache
 
     # on_data is a thin wrapper around _build_window, results should be the same
     if use_on_data:
-        results = stage.on_data(dfp_message_meta)
+        msg = stage.on_data(dfp_message_meta)
     else:
-        results = stage._build_window(dfp_message_meta)
+        msg = stage._build_window(dfp_message_meta)
 
-    assert results is None
+    assert isinstance(msg, MultiDFPMessage)
+    assert msg.meta.user_id == dfp_message_meta.user_id
+    assert msg.mess_offset == 0
+    assert msg.mess_count == len(dataset_pandas['filter_probs.csv'])
+    dataset_pandas.assert_df_equal(msg.get_meta(), train_df)
+    dataset_pandas.assert_df_equal(msg.meta.get_df(), train_df)
