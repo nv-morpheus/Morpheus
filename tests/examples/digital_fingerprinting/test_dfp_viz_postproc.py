c7e649457ac62b4a94fe657e945913a9d3a7c407
David Gardner <dagardner@nvidia.com>
2023-05-05 10:01:01 -0700

    Test for post_process method

--- tests/examples/digital_fingerprinting/test_dfp_viz_postproc.py
+++ tests/examples/digital_fingerprinting/test_dfp_viz_postproc.py
@@ -13,28 +13,60 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-from unittest import mock
-
-import numpy as np
+import pandas as pd
 import pytest
 
-from morpheus.common import TypeId
 from morpheus.config import Config
-from morpheus.messages.multi_ae_message import MultiAEMessage
+from morpheus.messages import MessageMeta
 from morpheus.pipeline.single_port_stage import SinglePortStage
-from morpheus.utils.logger import set_log_level
+from utils.dataset_manager import DatasetManager
+
+
+@pytest.fixture
+def dfp_multi_message(config, dfp_multi_message):
+    # Fill in some values for columns that the stage is looking for
+    with dfp_multi_message.meta.mutable_dataframe() as df:
+        step = (len(df) + 1) * 100
+        df["mean_abs_z"] = [i for i in range(0, len(df) * step, step)]
+        for (i, col) in enumerate(sorted(config.ae.feature_columns)):
+            step = i + 1 * 100
+            df[f"{col}_z_loss"] = [k for k in range(0, len(df) * step, step)]
+
+    yield dfp_multi_message
 
 
 def test_constructor(config: Config):
     from dfp.stages.dfp_viz_postproc import DFPVizPostprocStage
-    stage = DFPVizPostprocStage(config, period='M', output_dir='/test/dir', output_prefix='test_prefix')
+    stage = DFPVizPostprocStage(config, period='M', output_dir='/fake/test/dir', output_prefix='test_prefix')
 
     assert isinstance(stage, SinglePortStage)
     assert stage._user_column_name == config.ae.userid_column_name
     assert stage._timestamp_column == config.ae.timestamp_column_name
     assert stage._feature_columns == config.ae.feature_columns
     assert stage._period == 'M'
-    assert stage._output_dir == '/test/dir'
+    assert stage._output_dir == '/fake/test/dir'
     assert stage._output_prefix == 'test_prefix'
     assert stage._output_filenames == []
+
+
+def test_postprocess(
+        config: Config,
+        dfp_multi_message: "MultiDFPMessage",  # noqa: F821
+        dataset_pandas: DatasetManager):
+    from dfp.stages.dfp_viz_postproc import DFPVizPostprocStage
+
+    df = dfp_multi_message.meta.copy_dataframe()
+    expected_df = pd.DataFrame()
+    expected_df["user"] = df[config.ae.userid_column_name]
+    expected_df["time"] = df[config.ae.timestamp_column_name]
+    expected_df["period"] = pd.to_datetime(df[config.ae.timestamp_column_name]).dt.to_period('M')
+    expected_df["anomalyScore"] = df['mean_abs_z']
+    for col in config.ae.feature_columns:
+        expected_df[f"{col}_score"] = df[f"{col}_z_loss"]
+
+    # _postprocess doesn't write to disk, so the fake output_dir, shouldn't be an issue
+    stage = DFPVizPostprocStage(config, period='M', output_dir='/fake/test/dir', output_prefix='test_prefix')
+    results = stage._postprocess(dfp_multi_message)
+
+    assert isinstance(results, MessageMeta)
+    dataset_pandas.assert_compare_df(results.copy_dataframe(), expected_df)
