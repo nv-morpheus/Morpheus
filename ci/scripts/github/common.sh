4ec51e6d4724e509cbfc248b45ed4c6340a9cf3e
David Gardner <dagardner@nvidia.com>
2023-05-11 09:43:44 -0700

    Merge docs yaml

--- ci/scripts/github/common.sh
+++ ci/scripts/github/common.sh
@@ -62,6 +62,7 @@ export SCCACHE_IDLE_TIMEOUT=32768
 
 export CONDA_ENV_YML=${MORPHEUS_ROOT}/docker/conda/environments/cuda${CUDA_VER}_dev.yml
 export CONDA_EXAMPLES_YML=${MORPHEUS_ROOT}/docker/conda/environments/cuda${CUDA_VER}_examples.yml
+export CONDA_DOCS_YML=${MORPHEUS_ROOT}/docs/conda_docs.yml
 
 export CMAKE_BUILD_ALL_FEATURES="-DCMAKE_MESSAGE_CONTEXT_SHOW=ON -DMORPHEUS_CUDA_ARCHITECTURES=60;70;75;80 -DMORPHEUS_BUILD_BENCHMARKS=ON -DMORPHEUS_BUILD_EXAMPLES=ON -DMORPHEUS_BUILD_TESTS=ON -DMORPHEUS_USE_CONDA=ON -DMORPHEUS_PYTHON_INPLACE_BUILD=OFF -DMORPHEUS_PYTHON_BUILD_STUBS=ON -DMORPHEUS_USE_CCACHE=ON"
 
@@ -70,17 +71,26 @@ export FETCH_STATUS=0
 print_env_vars
 
 function update_conda_env() {
-    rapids-logger "Checking for updates to conda env"
-
     # Deactivate the environment first before updating
     conda deactivate
 
     ENV_YAML=${CONDA_ENV_YML}
-    if [[ "${MERGE_EXAMPLES_YAML}" == "1" ]]; then
-        # Merge the dev and examples envs, otherwise --prune will remove the examples packages
+    if [[ "${MERGE_EXAMPLES_YAML}" == "1" || "${MERGE_DOCS_YAML}" == "1" ]]; then
+        # Merge the dev, docs and examples envs, otherwise --prune will remove the examples packages
         ENV_YAML=${condatmpdir}/merged_env.yml
-        conda run -n morpheus --live-stream conda-merge ${CONDA_ENV_YML} ${CONDA_EXAMPLES_YML} > ${ENV_YAML}
+        YAMLS="${CONDA_ENV_YML}"
+        if [[ "${MERGE_EXAMPLES_YAML}" == "1" ]]; then
+            YAMLS="${YAMLS} ${CONDA_EXAMPLES_YML}"
+        fi
+        if [[ "${MERGE_DOCS_YAML}" == "1" ]]; then
+            YAMLS="${YAMLS} ${CONDA_DOCS_YML}"
+        fi
+
+        rapids-logger "Merging conda envs: ${YAMLS}"
+        conda run -n morpheus --live-stream conda-merge ${YAMLS} > ${ENV_YAML}
     fi
+
+    rapids-logger "Checking for updates to conda env"
 
     # Update the packages
     rapids-mamba-retry env update -n morpheus --prune -q --file ${ENV_YAML}
