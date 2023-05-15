6b718000f122cc3a42317dd8a2c74d5d97c6480e
David Gardner <dagardner@nvidia.com>
2023-05-15 11:34:58 -0700

    wip

--- morpheus/cli/run.py
+++ morpheus/cli/run.py
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Main entrypoint for the Morpheus CLI, when installed this module is executed as `morpheus`"""
 
 
 def run_cli():
-
+    """Main entrypoint for the CLI"""
     from morpheus.cli.commands import cli
 
     # the `cli` method expects a `ctx` instance which is provided by the `prepare_command` decordator, but pylint
