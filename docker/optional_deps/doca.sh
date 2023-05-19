4a7a320e65a15bed96096f2905a0ed73c93fcb6c
Christopher Harris <xixonia@gmail.com>
2023-05-19 23:00:46 +0000

    add -e to doca.sh install script

--- docker/optional_deps/doca.sh
+++ docker/optional_deps/doca.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 MORPHEUS_SUPPORT_DOCA=${MORPHEUS_SUPPORT_DOCA:-OFF}
 
 # Exit early if nothing to do
