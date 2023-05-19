6d678049f1f966be35bbfef63fc1ce9bc13e6646
Christopher Harris <xixonia@gmail.com>
2023-05-19 23:04:12 +0000

    remove references to Configure_gdrcopy.cmake

--- cmake/package_config/register_api.cmake
+++ cmake/package_config/register_api.cmake
@@ -20,7 +20,6 @@ include_guard(GLOBAL)
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
 
 include(${CMAKE_CURRENT_LIST_DIR}/bsd/Configure_bsd.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/gdrcopy/Configure_gdrcopy.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/md/Configure_md.cmake)
 
 list(POP_BACK CMAKE_MODULE_PATH)
