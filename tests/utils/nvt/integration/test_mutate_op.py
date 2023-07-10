3a44b8a05cb1397295e2073202734c0cd7d6e765
David Gardner <dagardner@nvidia.com>
2023-07-10 11:11:11 -0700

    Refactor setup into fixtures

--- tests/utils/nvt/integration/test_mutate_op.py
+++ tests/utils/nvt/integration/test_mutate_op.py
@@ -12,7 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import typing
+
 import pandas as pd
+import pytest
 from merlin.dag import ColumnSelector
 
 import cudf
@@ -21,25 +24,25 @@ from morpheus.utils.nvt import MutateOp
 from morpheus.utils.nvt.transforms import json_flatten
 
 
-def setUp():
-    json_data = [
+@pytest.fixture(name="json_data")
+def json_data_fixture():
+    yield [
         '{"key1": "value1", "key2": {"subkey1": "subvalue1", "subkey2": "subvalue2"}}',
         '{"key1": "value2", "key2": {"subkey1": "subvalue3", "subkey2": "subvalue4"}}',
         '{"key1": "value3", "key2": {"subkey1": "subvalue5", "subkey2": "subvalue6"}}'
     ]
 
-    expected_pdf = pd.DataFrame({
+
+@pytest.fixture(name="expected_pdf")
+def expected_pdf_fixture():
+    yield pd.DataFrame({
         'col1.key1': ['value1', 'value2', 'value3'],
         'col1.key2.subkey1': ['subvalue1', 'subvalue3', 'subvalue5'],
         'col1.key2.subkey2': ['subvalue2', 'subvalue4', 'subvalue6']
     })
 
-    return json_data, expected_pdf
 
-
-def test_integration_pandas():
-    json_data, expected_pdf = setUp()
-
+def test_integration_pandas(json_data: typing.List[str], expected_pdf: pd.DataFrame):
     pdf = pd.DataFrame({'col1': json_data})
     col_selector = ColumnSelector(['col1'])
 
@@ -50,9 +53,7 @@ def test_integration_pandas():
     assert result_pdf.equals(expected_pdf), "Integration test with pandas DataFrame failed"
 
 
-def test_integration_cudf():
-    json_data, expected_pdf = setUp()
-
+def test_integration_cudf(json_data: typing.List[str], expected_pdf: pd.DataFrame):
     cdf = cudf.DataFrame({'col1': json_data})
     col_selector = ColumnSelector(['col1'])
 
