5c8e81028b13a905bc0c37128210c2768b7ee4e0
David Gardner <dagardner@nvidia.com>
2023-07-10 10:06:46 -0700

    new test

--- tests/test_http_utils.py
+++ tests/test_http_utils.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from unittest import mock
+
 import pytest
 
 from morpheus.utils import http_utils
@@ -29,3 +31,28 @@ def test_prepare_url(url: str, expected: str):
 def test_prepare_url_error():
     with pytest.raises(ValueError):
         http_utils.prepare_url("")
+
+
+def make_mock_response(mock_request: mock.MagicMock,
+                       status_code: int = 200,
+                       content_type: str = http_utils.MimeTypes.TEXT.value,
+                       text: str = "test"):
+    mock_response = mock.MagicMock()
+    mock_response.status_code = status_code
+    mock_response.headers = {"Content-Type": content_type}
+    mock_response.text = text
+
+    mock_request.return_value = mock_response
+    return mock_response
+
+
+@mock.patch("requests.sessions.Session.request")
+@mock.patch("time.sleep")
+def test_max_retries(mock_sleep: mock.MagicMock, mock_request: mock.MagicMock):
+    make_mock_response(mock_request, status_code=500)
+
+    with pytest.raises(RuntimeError):
+        http_utils.request_with_retry({'method': 'GET', 'url': 'http://test.nvidia.com'}, max_retries=5, sleep_time=1)
+
+    assert mock_request.call_count == 5
+    assert mock_sleep.call_count == 4
