c3d2fc70a1bee4df36631a3cd59b3ed42597b9ba
Devin Robison <drobison00@users.noreply.github.com>
2023-07-11 12:37:26 -0600

    Integrate NVTabular into Morpheus Core and replace existing column_info based workflows. (#938)
    Closes #862 
    Closes #863 
    
    Integrates NVT into Morpheus core
    - Appropriate conda packages added
    - Existing process_dataframe calls in column_info.py are converted to nvt.Workflows
    - Backwards compatibility is maintained for anyone using DataFrameInputSchema objects
    
    Authors:
      - Devin Robison (https://github.com/drobison00)
      - Michael Demoret (https://github.com/mdemoret-nv)
      - David Gardner (https://github.com/dagardner-nv)
    
    Approvers:
      - Michael Demoret (https://github.com/mdemoret-nv)
    
    URL: https://github.com/nv-morpheus/Morpheus/pull/938

--- morpheus/utils/column_info.py
+++ morpheus/utils/column_info.py
@@ -22,16 +22,60 @@ import pandas as pd
 
 import cudf
 
-logger = logging.getLogger("morpheus.{}".format(__name__))
+logger = logging.getLogger(f"morpheus.{__name__}")
+
+DEFAULT_DATE = '1970-01-01T00:00:00.000000+00:00'
 
 
-def create_increment_col(df, column_name: str, groupby_column="username", timestamp_column="timestamp"):
+# Note(Devin): Proxying this for backwards compatibility. Had to move the primary definition to avoid circular imports.
+def process_dataframe(df_in: typing.Union[pd.DataFrame, cudf.DataFrame], input_schema) -> pd.DataFrame:
+    """
+    Processes a dataframe according to the given schema.
+
+    Parameters
+    ----------
+    df_in : pandas.DataFrame or cudf.DataFrame
+        The input dataframe to process.
+    input_schema : object
+        The schema used to process the dataframe.
+
+    Returns
+    -------
+    pandas.DataFrame
+        The processed dataframe.
+
+    """
+
+    from morpheus.utils import schema_transforms
+    return schema_transforms.process_dataframe(df_in, input_schema)
+
+
+def create_increment_col(df: pd.DataFrame,
+                         column_name: str,
+                         groupby_column: str = "username",
+                         timestamp_column: str = "timestamp") -> pd.Series:
     """
     Create a new integer column counting unique occurrences of values in `column_name` grouped per-day using the
     timestamp values in `timestamp_column` and then grouping by `groupby_column` returning incrementing values starting
     at `1`.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The input dataframe.
+    column_name : str
+        Name of the column in which unique occurrences are counted.
+    groupby_column : str, default "username"
+        The column to group by.
+    timestamp_column : str, default "timestamp"
+        The column containing timestamp values.
+
+    Returns
+    -------
+    pandas.Series
+        The new column with incrementing values.
+
     """
-    DEFAULT_DATE = '1970-01-01T00:00:00.000000+00:00'
 
     # Ensure we are pandas for this
     if (isinstance(df, cudf.DataFrame)):
@@ -51,28 +95,61 @@ def create_increment_col(df, column_name: str, groupby_column="username", timest
     return increment_col
 
 
-def column_listjoin(df, col_name: str) -> pd.Series:
+def column_listjoin(df: pd.DataFrame, col_name: str) -> pd.Series:
     """
     Returns the array series `df[col_name]` as flattened string series.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The dataframe from which to get the column.
+    col_name : str
+        The column to transform.
+
+    Returns
+    -------
+    pandas.Series
+        A series with the arrays in the column flattened to strings.
+
     """
+
     if col_name in df:
         return df[col_name].transform(lambda x: ",".join(x)).astype('string')
-    else:
-        return pd.Series(None, dtype='string')
+
+    return pd.Series(None, dtype='string')
 
 
 @dataclasses.dataclass
 class ColumnInfo:
     """Defines a single column and type-cast."""
     name: str
-    dtype: str  # The final type
+    dtype: str
+
+    def __setattr__(self, name: str, value: typing.Any) -> None:
+
+        # Convert the dtype to a string when its set to keep the type consistent.
+        if (name == "dtype"):
+            value = ColumnInfo.convert_pandas_dtype(value)
+
+        super().__setattr__(name, value)
+
+    @staticmethod
+    def convert_pandas_dtype(dtype: str | type) -> str:
+        """Return the pandas type string of column."""
+        if ((isinstance(dtype, str) and dtype.startswith("datetime"))
+                or (isinstance(dtype, type) and issubclass(dtype, datetime))):
+            return "datetime64[ns]"
+
+        if (isinstance(dtype, str)):
+            return dtype
+
+        return dtype.__name__
 
     def get_pandas_dtype(self) -> str:
-        """Return the pandas type string of column."""
-        if (issubclass(self.dtype, datetime)):
-            return "datetime64[ns]"
-        else:
-            return self.dtype
+        """Return the pandas type string for the currently set `dtype`."""
+
+        # The type is already converted. This is maintained for backwards compatibility.
+        return self.dtype
 
     def _process_column(self, df: pd.DataFrame) -> pd.Series:
         """
@@ -96,19 +173,73 @@ class ColumnInfo:
 
 @dataclasses.dataclass
 class CustomColumn(ColumnInfo):
-    """Subclass of `ColumnInfo`, defines a column to be computed by a user-defined function `process_column_fn`."""
+    """
+    Subclass of `ColumnInfo`, defines a column to be computed by a user-defined function `process_column_fn`.
+
+    Attributes
+    ----------
+    process_column_fn : Callable
+        A function that takes a DataFrame and returns a Series.
+
+    Methods
+    -------
+    _process_column(df: pandas.DataFrame) -> pandas.Series
+        Apply the `process_column_fn` to the DataFrame and return the result as a Series.
+
+    """
+
     process_column_fn: typing.Callable
 
     def _process_column(self, df: pd.DataFrame) -> pd.Series:
+        """
+        Apply the `process_column_fn` to the DataFrame and return the result as a Series.
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            The DataFrame to process.
+
+        Returns
+        -------
+        pandas.Series
+            The processed column.
+        """
         return self.process_column_fn(df)
 
 
 @dataclasses.dataclass
 class RenameColumn(ColumnInfo):
-    """Subclass of `ColumnInfo`, adds the ability to also perform a rename."""
+    """
+    Subclass of `ColumnInfo`, adds the ability to also perform a rename.
+
+    Attributes
+    ----------
+    input_name : str
+        The name of the input column.
+
+    Methods
+    -------
+    _process_column(df: pandas.DataFrame) -> pandas.Series
+        Rename the column and return it as a Series.
+
+    """
     input_name: str
 
     def _process_column(self, df: pd.DataFrame) -> pd.Series:
+        """
+        Rename the column and return it as a Series.
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            The DataFrame containing the column.
+
+        Returns
+        -------
+        pandas.Series
+            The renamed column.
+        """
+
         if (self.input_name not in df.columns):
             return pd.Series(None, index=df.index, dtype=self.get_pandas_dtype())
 
@@ -117,7 +248,29 @@ class RenameColumn(ColumnInfo):
 
 @dataclasses.dataclass
 class BoolColumn(RenameColumn):
-    """Subclass of `RenameColumn`, adds the ability to map a set custom values as boolean values."""
+    """
+    Subclass of `RenameColumn`, adds the ability to map a set custom values as boolean values.
+
+    Attributes
+    ----------
+    value_map : Dict[str, bool]
+        A mapping from input values to boolean values.
+    true_value : str
+        The value to map to True.
+    false_value : str
+        The value to map to False.
+    true_values : List[str]
+        A list of values to map to True.
+    false_values : List[str]
+        A list of values to map to False.
+
+    Methods
+    -------
+    _process_column(df: pandas.DataFrame) -> pandas.Series
+        Apply the mapping and return the result as a boolean Series.
+
+    """
+
     value_map: typing.Dict[str, bool] = dataclasses.field(init=False, default_factory=dict)
 
     true_value: dataclasses.InitVar[str] = None
@@ -144,6 +297,20 @@ class BoolColumn(RenameColumn):
             self.value_map.update({v: False for v in false_values})
 
     def _process_column(self, df: pd.DataFrame) -> pd.Series:
+        """
+        Apply the mapping and return the result as a boolean Series.
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            The DataFrame containing the column.
+
+        Returns
+        -------
+        pandas.Series
+            The processed column as a boolean Series.
+        """
+
         return super()._process_column(df).map(self.value_map).astype(bool)
 
 
@@ -152,18 +319,66 @@ class DateTimeColumn(RenameColumn):
     """
     Subclass of `RenameColumn`, specific to casting UTC localized datetime values. When incoming values contain a
     time-zone offset string the values are converted to UTC, while values without a time-zone are assumed to be UTC.
+
+    Methods
+    -------
+    _process_column(df: pandas.DataFrame) -> pandas.Series
+        Convert the values in the column to datetime and return the result as a Series.
+
     """
 
     def _process_column(self, df: pd.DataFrame) -> pd.Series:
+        """
+        Convert the values in the column to datetime and return the result as a Series.
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            The DataFrame containing the column.
+
+        Returns
+        -------
+        pandas.Series
+            The processed column as a datetime Series.
+        """
+
         return pd.to_datetime(super()._process_column(df), infer_datetime_format=True, utc=True)
 
 
 @dataclasses.dataclass
 class StringJoinColumn(RenameColumn):
-    """Subclass of `RenameColumn`, converts incoming `list` values to string by joining by `sep`."""
+    """
+    Subclass of `RenameColumn`, converts incoming `list` values to string by joining by `sep`.
+
+    Attributes
+    ----------
+    sep : str
+        The separator to use when joining the values.
+
+    Methods
+    -------
+    _process_column(df: pandas.DataFrame) -> pandas.Series
+        Join the values in the column and return the result as a Series.
+
+    """
+
     sep: str
 
     def _process_column(self, df: pd.DataFrame) -> pd.Series:
+        """
+        Join the values in the column and return the result as a Series.
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            The DataFrame containing the column.
+
+        Returns
+        -------
+        pandas.Series
+            The processed column as a string Series.
+        """
+
         return super()._process_column(df).str.join(sep=self.sep)
 
 
@@ -171,11 +386,38 @@ class StringJoinColumn(RenameColumn):
 class StringCatColumn(ColumnInfo):
     """
     Subclass of `ColumnInfo`, concatenates values from multiple columns into a new string column separated by `sep`.
+
+    Attributes
+    ----------
+    input_columns : List[str]
+        The columns to concatenate.
+    sep : str
+        The separator to use when joining the values.
+
+    Methods
+    -------
+    _process_column(df: pandas.DataFrame) -> pandas.Series
+        Concatenate the values from the input columns and return the result as a Series.
+
     """
     input_columns: typing.List[str]
     sep: str
 
     def _process_column(self, df: pd.DataFrame) -> pd.Series:
+        """
+        Concatenate the values from the input columns and return the result as a Series.
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            The DataFrame containing the columns.
+
+        Returns
+        -------
+        pandas.Series
+            The processed column as a string Series.
+        """
+
         first_col = df[self.input_columns[0]]
 
         return first_col.str.cat(others=df[self.input_columns[1:]], sep=self.sep)
@@ -186,11 +428,39 @@ class IncrementColumn(DateTimeColumn):
     """
     Subclass of `DateTimeColumn`, counts the unique occurrences of a value in `groupby_column` over a specific time
     window `period` based on dates in the `input_name` field.
+
+    Attributes
+    ----------
+    groupby_column : str
+        The column to group by.
+    period : str
+        The period to use when grouping.
+
+    Methods
+    -------
+    _process_column(df: pandas.DataFrame) -> pandas.Series
+        Count the unique occurrences and return the result as a Series.
+
     """
+
     groupby_column: str
     period: str = "D"
 
     def _process_column(self, df: pd.DataFrame) -> pd.Series:
+        """
+        Count the unique occurrences and return the result as a Series.
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            The DataFrame containing the columns.
+
+        Returns
+        -------
+        pandas.Series
+            The processed column as an integer Series.
+        """
+
         period = super()._process_column(df).dt.to_period(self.period)
 
         # Create the `groupby_column`, per-period log count
@@ -199,7 +469,26 @@ class IncrementColumn(DateTimeColumn):
 
 @dataclasses.dataclass
 class DataFrameInputSchema:
-    """Defines the schema specifying the columns to be included in the output `DataFrame`."""
+    """
+    Defines the schema specifying the columns to be included in the output `DataFrame`.
+
+    Attributes
+    ----------
+    json_columns : List[str]
+        The columns to include in the output DataFrame.
+    column_info : List[ColumnInfo]
+        Information about the columns.
+    preserve_columns : List[str]
+        The columns to preserve.
+    row_filter : Callable[[pandas.DataFrame], pandas.DataFrame]
+        A function to filter the rows of the DataFrame.
+
+    Methods
+    -------
+    __post_init__()
+        Compile the `preserve_columns` into a regular expression.
+
+    """
 
     json_columns: typing.List[str] = dataclasses.field(default_factory=list)
     column_info: typing.List[ColumnInfo] = dataclasses.field(default_factory=list)
@@ -207,7 +496,9 @@ class DataFrameInputSchema:
     row_filter: typing.Callable[[pd.DataFrame], pd.DataFrame] = None
 
     def __post_init__(self):
-
+        """
+        Compile the `preserve_columns` into a regular expression.
+        """
         input_preserve_columns = self.preserve_columns
 
         # Ensure preserve_columns is a list
@@ -216,107 +507,8 @@ class DataFrameInputSchema:
 
         # Compile the regex
         if (input_preserve_columns is not None and len(input_preserve_columns) > 0):
-            input_preserve_columns = re.compile("({})".format("|".join(input_preserve_columns)))
+            input_preserve_columns = re.compile(f"({'|'.join(input_preserve_columns)})")
         else:
             input_preserve_columns = None
 
         self.preserve_columns = input_preserve_columns
-
-
-def _process_columns(df_in, input_schema: DataFrameInputSchema):
-    # TODO(MDD): See what causes this to have such a perf impact over using df_in
-    output_df = pd.DataFrame()
-
-    convert_to_cudf = False
-
-    if (isinstance(df_in, cudf.DataFrame)):
-        df_in = df_in.to_pandas()
-        convert_to_cudf = True
-
-    # Iterate over the column info
-    for ci in input_schema.column_info:
-        try:
-            output_df[ci.name] = ci._process_column(df_in)
-        except Exception:
-            logger.exception("Failed to process column '%s'. Dataframe: \n%s", ci.name, df_in, exc_info=True)
-            raise
-
-    if (input_schema.preserve_columns is not None):
-        # Get the list of remaining columns not already added
-        df_in_columns = set(df_in.columns) - set(output_df.columns)
-
-        # Finally, keep any columns that match the preserve filters
-        match_columns = [y for y in df_in_columns if input_schema.preserve_columns.match(y)]
-
-        output_df[match_columns] = df_in[match_columns]
-
-    if (convert_to_cudf):
-        return cudf.from_pandas(output_df)
-
-    return output_df
-
-
-def _normalize_dataframe(df_in: pd.DataFrame, input_schema: DataFrameInputSchema):
-    if (input_schema.json_columns is None or len(input_schema.json_columns) == 0):
-        return df_in
-
-    convert_to_cudf = False
-
-    # Check if we are cudf
-    if (isinstance(df_in, cudf.DataFrame)):
-        df_in = df_in.to_pandas()
-        convert_to_cudf = True
-
-    json_normalized = []
-    remaining_columns = list(df_in.columns)
-
-    for j_column in input_schema.json_columns:
-
-        if (j_column not in remaining_columns):
-            continue
-
-        normalized = pd.json_normalize(df_in[j_column])
-
-        # Prefix the columns
-        normalized.rename(columns={n: f"{j_column}.{n}" for n in normalized.columns}, inplace=True)
-
-        # Reset the index otherwise there is a conflict
-        normalized.reset_index(drop=True, inplace=True)
-
-        json_normalized.append(normalized)
-
-        # Remove from the list of remaining columns
-        remaining_columns.remove(j_column)
-
-    # Also need to reset the original index
-    df_in.reset_index(drop=True, inplace=True)
-
-    df_normalized = pd.concat([df_in[remaining_columns]] + json_normalized, axis=1)
-
-    if (convert_to_cudf):
-        return cudf.from_pandas(df_normalized)
-
-    return df_normalized
-
-
-def _filter_rows(df_in: pd.DataFrame, input_schema: DataFrameInputSchema):
-    if (input_schema.row_filter is None):
-        return df_in
-
-    return input_schema.row_filter(df_in)
-
-
-def process_dataframe(df_in: pd.DataFrame, input_schema: DataFrameInputSchema) -> pd.DataFrame:
-    """
-    Applies colmn transformations as defined by `input_schema`
-    """
-    # Step 1 is to normalize any columns
-    df_processed = _normalize_dataframe(df_in, input_schema)
-
-    # Step 2 is to process columns
-    df_processed = _process_columns(df_processed, input_schema)
-
-    # Step 3 is to run the row filter if needed
-    df_processed = _filter_rows(df_processed, input_schema)
-
-    return df_processed
