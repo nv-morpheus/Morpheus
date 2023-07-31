6eb3a834d1c07adb724e131dbb056faf39226b50
Michael Demoret <mdemoret@nvidia.com>
2023-07-31 17:44:50 -0600

    Committing the stub files

---
+++ morpheus/_lib/modules/__init__.pyi
@@ -0,0 +1,17 @@
+"""
+        -----------------------
+        .. currentmodule:: morpheus.modules
+        .. autosummary::
+           :toctree: _generate
+
+        """
+from __future__ import annotations
+import morpheus._lib.modules
+import typing
+
+__all__ = [
+    
+]
+
+
+__version__ = '23.11.0'
