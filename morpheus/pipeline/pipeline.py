02a60f4f939dfd8ccf18f2ae613c094393f5af8e
David Gardner <dagardner@nvidia.com>
2023-08-01 16:30:44 -0700

    Revert "Don't hold a reference to the pipeline outside of the build method, explicitly release reference to the executor after join and stop"
    This reverts commit 7de3daa1eb26e29d6382e88374dd28fbc2d135fd.

--- morpheus/pipeline/pipeline.py
+++ morpheus/pipeline/pipeline.py
@@ -86,6 +86,7 @@ class Pipeline():
         self._is_started = False
 
         self._mrc_executor: mrc.Executor = None
+        self._mrc_pipeline: mrc.Pipeline = None
 
     @property
     def is_built(self) -> bool:
@@ -233,7 +234,7 @@ class Pipeline():
 
         self._mrc_executor = mrc.Executor(self._exec_options)
 
-        mrc_pipeline = mrc.Pipeline()
+        self._mrc_pipeline = mrc.Pipeline()
 
         def inner_build(builder: mrc.Builder, segment_id: str):
             logger.info(f"====Building Segment: {segment_id}====")
@@ -279,9 +280,9 @@ class Pipeline():
             segment_egress_ports = self._segments[segment_id]["egress_ports"]
             segment_inner_build = partial(inner_build, segment_id=segment_id)
 
-            mrc_pipeline.make_segment(segment_id, [port_info["port_pair"] for port_info in segment_ingress_ports],
-                                      [port_info["port_pair"] for port_info in segment_egress_ports],
-                                      segment_inner_build)
+            self._mrc_pipeline.make_segment(segment_id, [port_info["port_pair"] for port_info in segment_ingress_ports],
+                                            [port_info["port_pair"] for port_info in segment_egress_ports],
+                                            segment_inner_build)
 
         logger.info("====Building Pipeline Complete!====")
         self._is_build_complete = True
@@ -289,7 +290,7 @@ class Pipeline():
         # Finally call _on_start
         self._on_start()
 
-        self._mrc_executor.register_pipeline(mrc_pipeline)
+        self._mrc_executor.register_pipeline(self._mrc_pipeline)
 
         self._is_built = True
 
@@ -316,7 +317,6 @@ class Pipeline():
         self._mrc_executor.stop()
 
         logger.info("====Pipeline Stopped====")
-        self._on_stop()
 
     async def join(self):
         """
@@ -345,11 +345,6 @@ class Pipeline():
             # Now call join on all stages
             for s in list(self._stages):
                 await s.join()
-
-            self._on_stop()
-
-    def _on_stop(self):
-        self._mrc_executor = None
 
     async def _build_and_start(self):
 
