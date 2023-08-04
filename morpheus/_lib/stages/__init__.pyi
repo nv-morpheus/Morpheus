480e6bb997f83edfe0d65752732db285301ca6dc
David Gardner <dagardner@nvidia.com>
2023-08-04 16:41:45 -0700

    pyi

--- morpheus/_lib/stages/__init__.pyi
+++ morpheus/_lib/stages/__init__.pyi
@@ -25,6 +25,7 @@ __all__ = [
     "PreallocateMultiMessageStage",
     "PreprocessFILStage",
     "PreprocessNLPStage",
+    "RestSourceStage",
     "SerializeStage",
     "WriteToFileStage"
 ]
@@ -65,6 +66,9 @@ class PreprocessFILStage(mrc.core.segment.SegmentObject):
     pass
 class PreprocessNLPStage(mrc.core.segment.SegmentObject):
     def __init__(self, builder: mrc.core.segment.Builder, name: str, vocab_hash_file: str, sequence_length: int, truncation: bool, do_lower_case: bool, add_special_token: bool, stride: int, column: str) -> None: ...
+    pass
+class RestSourceStage(mrc.core.segment.SegmentObject):
+    def __init__(self, builder: mrc.core.segment.Builder, name: str, bind_address: str = '127.0.0.1', port: int = 8080, endpoint: str = '/message', method: str = 'POST', accept_status: int = 201, sleep_time: float = 0.10000000149011612, queue_timeout: int = 5, max_queue_size: int = 1024, num_server_threads: int = 1, max_payload_size: int = 10485760, request_timeout: int = 30, lines: bool = False, stop_after: int = 0) -> None: ...
     pass
 class SerializeStage(mrc.core.segment.SegmentObject):
     def __init__(self, builder: mrc.core.segment.Builder, name: str, include: typing.List[str], exclude: typing.List[str], fixed_columns: bool = True) -> None: ...
