e66dfb87757836e41437c129270c5b8d95431228
Tad ZeMicheal <thzemicheal@gmail.com>
2023-08-25 23:06:37 +0000

    Fix linting

--- tests/_utils/__init__.py
+++ tests/_utils/__init__.py
@@ -189,29 +189,12 @@ def make_mock_response(mock_request_session: mock.MagicMock,
     mock_request_session.request.return_value = mock_response
     return mock_response
 
-def make_url(port: int, endpoint: str) -> str:
-    if not endpoint.startswith("/"):
-        endpoint = "/" + endpoint
 
-    return f"http://127.0.0.1:{port}{endpoint}"
-
-
-def make_mock_response(mock_request_session: mock.MagicMock,
-                       status_code: int = 200,
-                       content_type: str = "text/plain",
-                       text: str = "test") -> mock.MagicMock:
-    """
-    Given a mocked `requests.Session` object, returns a mocked `requests.Response` object with the given status code
-    """
-    mock_response = mock.MagicMock()
-    mock_response.status_code = status_code
-    mock_response.headers = {"Content-Type": content_type}
-    mock_response.text = text
-
-    mock_request_session.return_value = mock_request_session
-    mock_request_session.request.return_value = mock_response
-    return mock_response
-
+def remove_module(mod_to_remove: str):
+    mod_prefix = f"{mod_to_remove}."
+    for mod_name in list(sys.modules.keys()):
+        if mod_name == mod_to_remove or mod_name.startswith(mod_prefix):
+            del sys.modules[mod_name]
 
 
 def mk_async_infer(inf_results: np.ndarray) -> typing.Callable:
