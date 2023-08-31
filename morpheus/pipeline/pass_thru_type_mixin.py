f5149c32617ed60c1c8c64811ae197ccb65a79f9
David Gardner <dagardner@nvidia.com>
2023-08-31 09:26:35 -0700

    Add an inferred mixin for stages like monitor & trigger

--- morpheus/pipeline/pass_thru_type_mixin.py
+++ morpheus/pipeline/pass_thru_type_mixin.py
@@ -11,37 +11,64 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Mixin used by stages which are emitting newly constructed DataFrame or MessageMeta instances into the segment."""
+"""Mixins for stages which receicve and emit the same type."""
 
 import types
 import typing
 from abc import ABC
 
-ERROR_MSG = "PassThruTypeMixin is intended to only be used by stages which accept only a single type."
+import typing_utils
+
+from morpheus.utils.type_utils import greatest_ancestor
+from morpheus.utils.type_utils import is_union_type
 
 
-class PassThruTypeMixin(ABC):
+class ExplicitPassThruTypeMixin(ABC):
     """
     Mixin intented to be added to stages which accept only a single type, and emit the exact same type.
     This is not intended to be used by stages which declare `typing.Any` as their accepted type.
     """
 
+    ERROR_MSG = "ExplicitPassThruTypeMixin is intended to only be used by stages which accept only a single type."
+
     def output_type(self, parent_output_types: list[type]) -> type:
         accepted_types = self.accepted_types()
         if len(accepted_types) != 1:
-            raise RuntimeError(ERROR_MSG)
+            raise RuntimeError(self.ERROR_MSG)
 
         if len(set(parent_output_types)) != 1:
-            raise RuntimeError(ERROR_MSG)
+            raise RuntimeError(self.ERROR_MSG)
 
         accepted_type = accepted_types[0]
 
-        if (accepted_type is typing.Any or issubclass(accepted_type, types.UnionType)):
-            raise RuntimeError(ERROR_MSG)
+        # using `typing_utils.issubtype` since `issubclass` does not work with `types.UnionType`
+        if (accepted_type is typing.Any or typing_utils.issubtype(accepted_type, types.UnionType)):
+            raise RuntimeError(self.ERROR_MSG)
 
         # Using != rather than `is not` since `int | float == typing.Union[int, float]` is True but
         # `int | float is typing.Union[int, float]` is False.
         if (accepted_type != parent_output_types[0]):
-            raise RuntimeError(ERROR_MSG)
+            raise RuntimeError(self.ERROR_MSG)
 
         return accepted_type
+
+
+class InferredPassThruTypeMixin(ABC):
+    """
+    Mixin intented to be added to stages which can accept multiple types or even `typing.Any`, who's output type type
+    is inferred from the output types of the parent stages.
+    """
+
+    def output_type(self, parent_output_types: list[type]) -> type:
+
+        flattened_types = []
+        for parent_type in parent_output_types:
+            # TODO: Move this to pipeline.py
+            assert parent_type is not typing.Any, "typing.Any is not an acceptable output type"
+
+            if is_union_type(parent_type):
+                flattened_types.extend(typing.get_args(parent_type))
+            else:
+                flattened_types.append(parent_type)
+
+        return greatest_ancestor(*flattened_types)
