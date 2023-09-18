7a9e2f9b3b83898ae6e09b8e794eb7b72bc0eeca
Christopher Harris <xixonia@gmail.com>
2023-09-18 15:53:33 +0000

    use logger

--- examples/multiprocessing/run.py
+++ examples/multiprocessing/run.py
@@ -13,12 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
 import ctypes
 import multiprocessing as mp
 import os
 import threading as mt
 import time
-from multiprocessing.connection import Connection
 
 import cupy
 import mrc
@@ -35,7 +35,9 @@ from morpheus.stages.input.in_memory_source_stage import InMemorySourceStage
 from morpheus.stages.output.in_memory_sink_stage import InMemorySinkStage
 from morpheus.stages.postprocess.serialize_stage import SerializeStage
 from morpheus.stages.preprocess.deserialize_stage import DeserializeStage
+from morpheus.utils.logger import configure_logging
 
+logger = logging.getLogger(f"morpheus.{__name__}")
 
 class MyMultiprocessingStage(SinglePortStage):
 
@@ -57,7 +59,9 @@ class MyMultiprocessingStage(SinglePortStage):
 
     @staticmethod
     def child_receive(queue_recv: mp.Queue, queue_send: mp.Queue, cancellation_token: mp.Value):
-        print("===== Started child receive =====", os.getppid(), os.getpid())
+
+        # using print instead of logging because logging is not multiprocessing safe.
+        print(f"===== Started child receive ===== {os.getppid()} {os.getpid()}")
         while not cancellation_token.value:
             if queue_recv.qsize() == 0:
                 print("child: waiting...")
@@ -67,6 +71,7 @@ class MyMultiprocessingStage(SinglePortStage):
             event = queue_recv.get()
 
             if event["type"] == "on_next":
+                print("child: on_next")
                 #  we can do our fancy processing here.
                 message: MultiMessage = event["value"]
                 message.set_meta("b", cupy.arange(message.mess_count))
@@ -74,21 +79,26 @@ class MyMultiprocessingStage(SinglePortStage):
                 continue
 
             if event["type"] == "on_error":
+                print("child: on_error")
                 queue_send.put(event)
                 break
 
             if event["type"] == "on_completed":
+                print("child: on_completed")
                 queue_send.put(event)
                 break
 
         print("child: closing...")
 
+        # this on_completed may be unnecessary/unexpected if we've already forwarded an on_completed event.
+        queue_send.put({"type": "on_completed"})
+
     @staticmethod
     def parent_receive(queue_recv: mp.Queue, sub: mrc.Subscriber, cancellation_token: mp.Value):
-        print("===== Started parent receive =====", os.getpid())
-        while not cancellation_token.value:
+        logger.debug(f"===== Started parent receive ===== {os.getpid()}")
+        while not cancellation_token.value and sub.is_subscribed():
             if queue_recv.qsize() == 0:
-                print("parent: waiting...")
+                logger.debug("parent: waiting...")
                 time.sleep(1)
                 continue
 
@@ -106,35 +116,43 @@ class MyMultiprocessingStage(SinglePortStage):
                 sub.on_completed()
                 break
 
-        print("parent: closing...")
+        logger.debug("parent: closing...")
+
+        sub.on_completed()
 
     def generate(self, obs: mrc.Observable, sub: mrc.Subscriber):
         pre_queue = self.mp_context.Queue()
         post_queue = self.mp_context.Queue()
-        my_process = self.mp_context.Process(target=MyMultiprocessingStage.child_receive,
+        self._my_process = self.mp_context.Process(target=MyMultiprocessingStage.child_receive,
                                              args=(pre_queue, post_queue, self.cancellation_token))
-        my_thread = mt.Thread(target=MyMultiprocessingStage.parent_receive,
+        self._my_thread = mt.Thread(target=MyMultiprocessingStage.parent_receive,
                               args=(post_queue, sub, self.cancellation_token))
 
         def on_next(message: MultiMessage):
-            print("obs on next", os.getpid())
+            logger.debug(f"obs on next {os.getpid()}")
             pre_queue.put({"type": "on_next", "value": message})
 
         def on_error(error: BaseException):
-            print("obs on error", os.getpid())
+            logger.debug(f"obs on error {os.getpid()}")
             pre_queue.put({"type": "on_error", "value": error})
 
         def on_completed():
-            print("obs on completed", os.getpid())
+            logger.debug(f"obs on completed {os.getpid()}")
             pre_queue.put({"type": "on_completed"})
 
-        my_process.start()
-        my_thread.start()
+        logger.debug("starting child process and parent thread")
+
+        self._my_process.start()
+        self._my_thread.start()
 
         obs.subscribe(Observer.make_observer(on_next, on_error, on_completed))
 
-        my_thread.join()
-        my_process.join()
+        logger.debug("joining child process and parent thread")
+
+        self._my_process.join()
+        self._my_thread.join()
+
+        logger.debug(f"done: sub.is_subscribed() == {sub.is_subscribed()}")
 
     def _build_single(self, builder: mrc.Builder, input: StreamPair):
         [input_node, input_type] = input
@@ -143,11 +161,17 @@ class MyMultiprocessingStage(SinglePortStage):
         return my_node, input_type
 
     def stop(self):
-        super().stop()
+        logger.debug("stopping...")
         self.cancellation_token.value = True
+        self._my_thread.join()
+        self._my_process.join()
+        super().stop()
+        logger.debug("stopped")
 
 
 def run_pipeline():
+
+    configure_logging(log_level=logging.DEBUG)
 
     config = Config()
 
@@ -161,13 +185,16 @@ def run_pipeline():
     pipeline.add_stage(SerializeStage(config))
     sink = pipeline.add_stage(InMemorySinkStage(config))
 
+    logger.debug("before run")
+
     pipeline.run()
+
+    logger.debug("after run")
 
     messages = sink.get_messages()
 
-    if len(messages) > 0:
-        print(messages[0].copy_dataframe())
-
+    for message in messages:
+        logger.debug(message.copy_dataframe())
 
 if __name__ == "__main__":
     run_pipeline()
