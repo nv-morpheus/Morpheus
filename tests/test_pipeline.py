e1bcb26235db7435ab9cb5c2723209eae224a403
David Gardner <dagardner@nvidia.com>
2023-09-19 09:10:26 -0700

    fix calling awaitable coroutine [no ci]

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -38,7 +38,7 @@ class SourceTestStage(InMemorySourceStage):
     def __init__(self,
                  config,
                  dataframes: typing.List[DataFrameType],
-                 destructor_cb: typing.Callable[[], None],
+                 destructor_cb: typing.Callable[[], None] = None,
                  repeat: int = 1):
         super().__init__(config, dataframes, repeat)
         self._destructor_cb = destructor_cb
@@ -48,32 +48,52 @@ class SourceTestStage(InMemorySourceStage):
         return "test-source"
 
     def __del__(self):
-        self._destructor_cb()
+        if self._destructor_cb is not None:
+            self._destructor_cb()
 
 
 class SinkTestStage(InMemorySinkStage):
 
-    def __init__(self, config, destructor_cb: typing.Callable[[], None]):
+    def __init__(self,
+                 config,
+                 on_start_cb: typing.Callable[[], None] = None,
+                 start_async_cb: typing.Callable[[], None] = None,
+                 destructor_cb: typing.Callable[[], None] = None):
         super().__init__(config)
+        self._on_start_cb = on_start_cb
+        self._start_async_cb = start_async_cb
         self._destructor_cb = destructor_cb
 
     @property
     def name(self) -> str:
         return "test-sink"
 
+    def on_start(self):
+        if self._on_start_cb is not None:
+            self._on_start_cb()
+
+    async def start_async(self):
+        await super().start_async()
+        if self._start_async_cb is not None:
+            self._start_async_cb()
+
     def __del__(self):
-        self._destructor_cb()
+        if self._destructor_cb is not None:
+            self._destructor_cb()
 
 
-def _run_pipeline(config: Config, filter_probs_df: DataFrameType, update_state_dict: typing.Callable[[str], None]):
+def _run_pipeline(filter_probs_df: DataFrameType,
+                  source_callbacks: dict[str, typing.Callable[[], None]],
+                  sink_callbacks: dict[str, typing.Callable[[], None]]):
+    config = Config()
     pipe = LinearPipeline(config)
-    pipe.set_source(SourceTestStage(config, [filter_probs_df], destructor_cb=lambda: update_state_dict("source")))
-    pipe.add_stage(SinkTestStage(config, destructor_cb=lambda: update_state_dict("sink")))
+    pipe.set_source(SourceTestStage(config, [filter_probs_df], **source_callbacks))
+    pipe.add_stage(SinkTestStage(config, **sink_callbacks))
     pipe.run()
 
 
 @pytest.mark.use_cudf
-def test_destructors_called(config: Config, filter_probs_df: DataFrameType):
+def test_destructors_called(filter_probs_df: DataFrameType):
     """
     Test to ensure that the destructors of stages are called (issue #1114).
     """
@@ -83,11 +103,36 @@ def test_destructors_called(config: Config, filter_probs_df: DataFrameType):
         nonlocal state_dict
         state_dict[key] = True
 
-    _run_pipeline(config, filter_probs_df, update_state_dict)
+    source_callbacks = {'destructor_cb': lambda: update_state_dict("source")}
+    sink_callbacks = {'destructor_cb': lambda: update_state_dict("sink")}
+
+    _run_pipeline(filter_probs_df, source_callbacks, sink_callbacks)
 
     gc.collect()
     assert state_dict["source"]
     assert state_dict["sink"]
+
+
+@pytest.mark.use_cudf
+def test_startup_cb_called(filter_probs_df: DataFrameType):
+    """
+    Test to ensure that the destructors of stages are called (issue #1114).
+    """
+    state_dict = {"on_start": False, "start_async": False}
+
+    def update_state_dict(key: str):
+        nonlocal state_dict
+        state_dict[key] = True
+
+    sink_callbacks = {
+        'on_start_cb': lambda: update_state_dict("on_start"),
+        'start_async_cb': lambda: update_state_dict("start_async")
+    }
+
+    _run_pipeline(filter_probs_df, source_callbacks={}, sink_callbacks=sink_callbacks)
+
+    assert state_dict["on_start"]
+    assert state_dict["start_async"]
 
 
 @pytest.mark.use_cudf
