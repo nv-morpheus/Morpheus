14ce11420befc661d8f1be2b35cc3884c3784a30
David Gardner <dagardner@nvidia.com>
2023-10-19 10:32:22 -0700

    Fix import order

--- tests/pipeline/conftest.py
+++ tests/pipeline/conftest.py
@@ -22,8 +22,8 @@ from _utils.stages.multi_port_pass_thru import MultiPortPassThruStage
 from _utils.stages.split_stage import SplitStage
 from morpheus.config import Config
 from morpheus.pipeline import Pipeline
-from morpheus.pipeline.stage_base import StageBase
 from morpheus.pipeline.source_stage import SourceStage
+from morpheus.pipeline.stage_base import StageBase
 from morpheus.stages.input.in_memory_source_stage import InMemorySourceStage
 from morpheus.stages.preprocess.deserialize_stage import DeserializeStage
 
