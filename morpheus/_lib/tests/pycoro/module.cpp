486abdee757afdf7e3d5369f1a3559762fba7a0b
Christopher Harris <xixonia@gmail.com>
2023-10-20 16:59:20 +0000

    fix styles

--- morpheus/_lib/tests/pycoro/module.cpp
+++ morpheus/_lib/tests/pycoro/module.cpp
@@ -1,3 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "pycoro/pycoro.hpp"
 
 #include <mrc/coroutines/task.hpp>
@@ -5,6 +22,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/pytypes.h>
 #include <pymrc/types.hpp>
+
 #include <stdexcept>
 
 namespace morpheus::tests::pycoro {
@@ -25,7 +43,8 @@ mrc::coroutines::Task<mrc::pymrc::PyHolder> call_fib_async(mrc::pymrc::PyHolder 
 
 mrc::coroutines::Task<mrc::pymrc::PyHolder> raise_at_depth_async(mrc::pymrc::PyHolder fn, int depth)
 {
-    if (depth <= 0) {
+    if (depth <= 0)
+    {
         throw std::runtime_error("depth reached zero in c++");
     }
 
--- tests/pycoro/test_pycoro.py
+++ tests/pycoro/test_pycoro.py
@@ -1,6 +1,26 @@
-from morpheus._lib.tests.pycoro import call_fib_async, raise_at_depth_async, call_async
-import pytest
+# SPDX-FileCopyrightText: Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import asyncio
+
+import pytest
+
+from morpheus._lib.tests.pycoro import call_async
+from morpheus._lib.tests.pycoro import call_fib_async
+from morpheus._lib.tests.pycoro import raise_at_depth_async
+
 
 @pytest.mark.asyncio
 async def test_python_cpp_async_interleave():
@@ -8,27 +28,28 @@ async def test_python_cpp_async_interleave():
     def fib(n):
         if n < 0:
             raise ValueError()
-            
+
         if n < 2:
             return 1
-        
-        return fib(n-1) + fib(n-2)
+
+        return fib(n - 1) + fib(n - 2)
 
     async def fib_async(n):
         if n < 0:
             raise ValueError()
-            
+
         if n < 2:
             return 1
-        
+
         task_a = call_fib_async(fib_async, n, 1)
         task_b = call_fib_async(fib_async, n, 2)
-        
+
         [a, b] = await asyncio.gather(task_a, task_b)
-        
+
         return a + b
-    
+
     assert fib(15) == await fib_async(15)
+
 
 @pytest.mark.asyncio
 async def test_python_cpp_async_exception():
@@ -36,7 +57,7 @@ async def test_python_cpp_async_exception():
     async def py_raise_at_depth_async(n: int):
         if n <= 0:
             raise RuntimeError("depth reached zero in python")
-        
+
         await raise_at_depth_async(py_raise_at_depth_async, n - 1)
 
     depth = 100
@@ -48,6 +69,7 @@ async def test_python_cpp_async_exception():
     with pytest.raises(RuntimeError) as ex:
         await raise_at_depth_async(py_raise_at_depth_async, depth)
     assert "c++" in str(ex.value)
+
 
 @pytest.mark.asyncio
 async def test_can_cancel_coroutine_from_python():
