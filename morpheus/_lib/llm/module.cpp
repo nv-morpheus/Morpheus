351eebad4ea4ae85b8253e317c448d3d9559be25
Christopher Harris <xixonia@gmail.com>
2023-10-25 18:45:30 +0000

    remove pycoro in favor of mrc coro

--- morpheus/_lib/llm/module.cpp
+++ morpheus/_lib/llm/module.cpp
@@ -21,7 +21,6 @@
 #include "./include/py_llm_task_handler.hpp"
 #include "py_llm_engine_stage.hpp"
 #include "py_llm_lambda_node.hpp"
-#include "pycoro/pycoro.hpp"  // IWYU pragma: keep
 
 #include "morpheus/llm/input_map.hpp"
 #include "morpheus/llm/llm_context.hpp"
@@ -47,6 +46,7 @@
 #include <pybind11/pybind11.h>    // for arg, init, class_, module_, str_attr_accessor, PYBIND11_MODULE, pybind11
 #include <pybind11/pytypes.h>     // for dict, sequence
 #include <pybind11/stl.h>         // IWYU pragma: keep
+#include <pymrc/coro.hpp>         // IWYU pragma: keep
 #include <pymrc/utils.hpp>        // for pymrc::import
 
 #include <memory>
@@ -73,8 +73,8 @@ PYBIND11_MODULE(llm, _module)
     // Load the cudf helpers
     CudfHelper::load();
 
-    // Import the pycoro module
-    mrc::pymrc::import(_module, "morpheus._lib.pycoro");
+    // Import the mrc coro module
+    mrc::pymrc::import(_module, "mrc.core.coro");
 
     // Import the messages module
     mrc::pymrc::import(_module, "morpheus._lib.messages");
