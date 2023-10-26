96e88eb136778257b087efd9648c9c77c453bee9
Eli Fajardo <efajardo@nvidia.com>
2023-10-26 17:26:44 -0400

    clang-format fixes

--- morpheus/_lib/tests/llm/test_llm_context.cpp
+++ morpheus/_lib/tests/llm/test_llm_context.cpp
@@ -26,10 +26,9 @@
 #include "morpheus/types.hpp"
 
 #include <gtest/gtest.h>
-#include <nlohmann/json.hpp>
-
 #include <mrc/channel/forward.hpp>
 #include <mrc/coroutines/sync_wait.hpp>
+#include <nlohmann/json.hpp>
 
 #include <coroutine>
 #include <memory>
@@ -50,7 +49,7 @@ TEST_F(TestLLMContext, InitializationTest)
     task_dict = {
         {"task_type", "dictionary"},
         {"model_name", "test"},
-    };    
+    };
 
     llm::LLMContext ctx_2{llm::LLMTask{"template", task_dict}, nullptr};
     ASSERT_EQ(ctx_2.task().get("task_type"), "dictionary");
@@ -59,17 +58,7 @@ TEST_F(TestLLMContext, InitializationTest)
     // llm::LLMContext context_2{llm::LLMTask{}, nullptr};
 
     nlohmann::json msg_config;
-    msg_config["tasks"] = {
-        {
-            {"type", "llm_engine"},
-            {"properties",
-                {
-                    {"type", "template"},
-                    {"properties", task_dict}
-                }
-            }
-        }
-    };
+    msg_config["tasks"] = {{{"type", "llm_engine"}, {"properties", {{"type", "template"}, {"properties", task_dict}}}}};
 
     auto msg = std::make_shared<ControlMessage>(msg_config);
 
@@ -82,7 +71,7 @@ TEST_F(TestLLMContext, InitializationTest)
     ASSERT_EQ(ctx_4.task().get("model_name"), "test");
 
     auto parent_ctx = std::make_shared<llm::LLMContext>(llm::LLMTask{"template", task_dict}, msg);
-    auto inputs = llm::input_mappings_t{{"/ext1", "input1"}};
+    auto inputs     = llm::input_mappings_t{{"/ext1", "input1"}};
     llm::LLMContext ctx_5{parent_ctx, "child", inputs};
     ASSERT_EQ(ctx_5.input_map()[0].external_name, "/ext1");
     ASSERT_EQ(ctx_5.input_map()[0].internal_name, "input1");
@@ -93,14 +82,11 @@ TEST_F(TestLLMContext, InitializationTest)
     ASSERT_EQ(ctx_5.full_name(), "/child");
 }
 
-TEST_F(TestLLMContext, OutputTest) {
-
+TEST_F(TestLLMContext, OutputTest)
+{
     llm::LLMContext ctx_1{llm::LLMTask{}, nullptr};
     nlohmann::json outputs;
-    outputs["input1"] = {
-        {"key1", "val1"},
-        {"key2", "val2"}
-    };
+    outputs["input1"] = {{"key1", "val1"}, {"key2", "val2"}};
     ctx_1.set_output(outputs);
 
     ASSERT_EQ(ctx_1.view_outputs()["input1"]["key1"], "val1");
@@ -112,22 +98,18 @@ TEST_F(TestLLMContext, OutputTest) {
     ASSERT_EQ(ctx_2.view_outputs()["input1"]["key2"], "val2");
 }
 
-TEST_F(TestLLMContext, PushPopTest) {
-
+TEST_F(TestLLMContext, PushPopTest)
+{
     auto parent_context = std::make_shared<llm::LLMContext>(llm::LLMTask{}, nullptr);
-    auto inputs = llm::input_mappings_t{{"/ext1", "input1"}};
+    auto inputs         = llm::input_mappings_t{{"/ext1", "input1"}};
     llm::LLMContext child_context{parent_context, "child", inputs};
-
 
     ASSERT_EQ(child_context.name(), "child");
     ASSERT_EQ(child_context.input_map()[0].external_name, "/ext1");
     ASSERT_EQ(child_context.input_map()[0].internal_name, "input1");
 
     nlohmann::json outputs;
-    outputs["input1"] = {
-        {"key1", "val1"},
-        {"key2", "val2"}
-    };
+    outputs["input1"] = {{"key1", "val1"}, {"key2", "val2"}};
     child_context.set_output(outputs);
     ASSERT_EQ(child_context.view_outputs()["input1"]["key1"], "val1");
     ASSERT_EQ(child_context.view_outputs()["input1"]["key2"], "val2");
@@ -135,4 +117,6 @@ TEST_F(TestLLMContext, PushPopTest) {
     child_context.pop();
     ASSERT_EQ(parent_context->view_outputs()["child"]["input1"]["key1"], "val1");
     ASSERT_EQ(parent_context->view_outputs()["child"]["input1"]["key2"], "val2");
+
+    std::cerr << parent_context->view_outputs();
 }
