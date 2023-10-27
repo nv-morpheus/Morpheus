de2fa38dac60aef4a9a36463631bc3b789cf19a7
David Gardner <dagardner@nvidia.com>
2023-10-27 14:39:03 -0700

    Lint fixes

--- examples/llm/agents/kafka_pipeline.py
+++ examples/llm/agents/kafka_pipeline.py
@@ -12,40 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging
-import os
 import time
 
-import pymilvus
 from langchain.agents import AgentType
 from langchain.agents import initialize_agent
 from langchain.agents import load_tools
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms.openai import OpenAI
 from langchain.llms.openai import OpenAIChat
-from requests_cache import SQLiteCache
-
-import cudf
 
 from morpheus.config import Config
 from morpheus.config import PipelineModes
 from morpheus.llm import LLMEngine
 from morpheus.llm.nodes.extracter_node import ExtracterNode
 from morpheus.llm.nodes.langchain_agent_node import LangChainAgentNode
-from morpheus.llm.nodes.prompt_template_node import PromptTemplateNode
-from morpheus.llm.nodes.rag_node import RAGNode
-from morpheus.llm.services.nemo_llm_service import NeMoLLMService
 from morpheus.llm.task_handlers.simple_task_handler import SimpleTaskHandler
 from morpheus.messages import ControlMessage
 from morpheus.pipeline.linear_pipeline import LinearPipeline
-from morpheus.service.milvus_vector_db_service import MilvusVectorDBService
-from morpheus.stages.general.monitor_stage import MonitorStage
-from morpheus.stages.input.in_memory_source_stage import InMemorySourceStage
 from morpheus.stages.input.kafka_source_stage import KafkaSourceStage
 from morpheus.stages.llm.llm_engine_stage import LLMEngineStage
 from morpheus.stages.output.in_memory_sink_stage import InMemorySinkStage
-from morpheus.stages.output.write_to_kafka_stage import WriteToKafkaStage
 from morpheus.stages.preprocess.deserialize_stage import DeserializeStage
-from morpheus.utils.vector_db_service_utils import VectorDBServiceFactory
 
 logger = logging.getLogger(__name__)
 
@@ -76,13 +61,7 @@ def _build_engine(model_name: str):
     return engine
 
 
-def pipeline(
-    num_threads,
-    pipeline_batch_size,
-    model_max_batch_size,
-    model_name,
-    repeat_count,
-):
+def pipeline(num_threads, pipeline_batch_size, model_max_batch_size, model_name):
     config = Config()
     config.mode = PipelineModes.OTHER
 
@@ -116,6 +95,6 @@ def pipeline(
 
     pipe.run()
 
-    logger.info("Pipeline complete.")
+    logger.info("Pipeline complete. Received %s responses", len(sink.get_messages()))
 
     return start_time
