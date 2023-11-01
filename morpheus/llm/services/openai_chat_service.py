2f5adb1f9ac2a2aca59037f5dbca5bee6a04897e
David Gardner <dagardner@nvidia.com>
2023-11-01 16:42:29 -0700

    Update to match new api

--- morpheus/llm/services/openai_chat_service.py
+++ morpheus/llm/services/openai_chat_service.py
@@ -54,13 +54,20 @@ class OpenAIChatClient(LLMClient):
     model_name : str
         The name of the model to interact with.
 
+    set_assistant: bool, optional default=False
+        When `True`, a second input field named `assistant` will be used to proide additional context to the model.
+
     model_kwargs : dict[str, typing.Any]
         Additional keyword arguments to pass to the model when generating text.
     """
 
-    def __init__(self, model_name: str, **model_kwargs: dict[str, typing.Any]) -> None:
+    def __init__(self, model_name: str, set_assistant: bool = False, **model_kwargs: dict[str, typing.Any]) -> None:
         super().__init__()
         _verify_deps()
+
+        self._set_assistant = set_assistant
+        self._prompt_key = "prompt"
+        self._assistant_key = "assistant"
 
         # Preserve original configuration.
         model_kwargs = copy.deepcopy(model_kwargs)
@@ -69,97 +76,103 @@ class OpenAIChatClient(LLMClient):
 
         self._model = OpenAIChat(model_name=model_name, **model_kwargs)
 
+    def get_input_names(self) -> list[str]:
+        input_names = [self._prompt_key]
+        if self._set_assistant:
+            input_names.append(self._assistant_key)
+
+        return input_names
+
     def _create_messages(self, prompt: str, assistant: str = None) -> list["BaseMessage"]:
         messages = [
             SystemMessage(content="You are a helpful assistant."),
             HumanMessage(content=prompt),
         ]
 
-        if (assistant is not None):
+        if (self._set_assistant):
             messages.append(AIMessage(content=assistant))
 
         return messages
 
-    def generate(self, prompt: str, assistant: str = None) -> str:
-        """
-        Issue a request to generate a response based on a given prompt.
-
-        Parameters
-        ----------
-        prompt : str
-            The prompt to generate a response for.
-        
-        assistant : str, optional
-            Additional context for the prompt
-        """
-        messages = self._create_messages(prompt, assistant=assistant)
+    def _generate(self, prompt: str, assistant: str = None) -> str:
+        messages = self._create_messages(prompt, assistant)
 
         output = self._model.predict_messages(messages=messages)
 
         return output.content
 
-    async def generate_async(self, prompt: str, assistant: str = None) -> str:
+    def generate(self, input_dict: dict[str, str]) -> str:
         """
-        Issue an asynchronous request to generate a response based on a given prompt.
+        Issue a request to generate a response based on a given prompt.
 
         Parameters
         ----------
-        prompt : str
-            The prompt to generate a response for.
-
-        assistant : str, optional
-            Additional context for the prompt
+        input_dict : dict
+            Input containing prompt data.
         """
-        messages = self._create_messages(prompt, assistant=assistant)
+        return self._generate(input_dict[self._prompt_key], input_dict.get(self._assistant_key))
+
+    async def _generate_async(self, prompt: str, assistant: str = None) -> str:
+        messages = self._create_messages(prompt, assistant)
 
         output = await self._model.apredict_messages(messages=messages)
 
         return output.content
 
-    def generate_batch(self, prompts: list[str], assistants: list[str] = None) -> list[str]:
+    async def generate_async(self, input_dict: dict[str, str]) -> str:
+        """
+        Issue an asynchronous request to generate a response based on a given prompt.
+
+        Parameters
+        ----------
+        input_dict : dict
+            Input containing prompt data.
+        """
+        return await self._generate_async(input_dict[self._prompt_key], input_dict.get(self._assistant_key))
+
+    def generate_batch(self, inputs: dict[str, list[str]]) -> list[str]:
         """
         Issue a request to generate a list of responses based on a list of prompts.
 
         Parameters
         ----------
-        prompts : list[str]
-            The prompts to generate responses for.
-
-        assistants : list[str], optional
-            Additional contexts for the prompts. If not None, must be the same length as `prompts`.
+        inputs : dict
+            Inputs containing prompt data.
         """
-        if assistants is not None:
+        prompts = inputs[self._prompt_key]
+        assistants = None
+        if (self._set_assistant):
+            assistants = inputs[self._assistant_key]
             if len(prompts) != len(assistants):
                 raise ValueError("The number of prompts and assistants must be equal.")
 
         results = []
         for (i, prompt) in enumerate(prompts):
             assistant = assistants[i] if assistants is not None else None
-            results.append(self.generate(prompt, assistant))
+            results.append(self._generate(prompt, assistant))
 
         return results
 
-    async def generate_batch_async(self, prompts: list[str], assistants: list[str] = None) -> list[str]:
+    async def generate_batch_async(self, inputs: dict[str, list[str]]) -> list[str]:
         """
         Issue an asynchronous request to generate a list of responses based on a list of prompts.
 
         Parameters
         ----------
-        prompts : list[str]
-            The prompts to generate responses for.
-
-        assistants : list[str], optional
-            Additional contexts for the prompts. If not None, must be the same length as `prompts`.
+        inputs : dict
+            Inputs containing prompt data.
         """
-
-        if assistants is not None:
+        prompts = inputs[self._prompt_key]
+        assistants = None
+        if (self._set_assistant):
+            assistants = inputs[self._assistant_key]
             if len(prompts) != len(assistants):
                 raise ValueError("The number of prompts and assistants must be equal.")
 
         coros = []
         for (i, prompt) in enumerate(prompts):
             assistant = assistants[i] if assistants is not None else None
-            coros.append(self.generate(prompt, assistant))
+            coros.append(self._generate_async(prompt, assistant))
 
         return await asyncio.gather(*coros)
 
@@ -173,7 +186,10 @@ class OpenAIChatService(LLMService):
         super().__init__()
         _verify_deps()
 
-    def get_client(self, model_name: str, **model_kwargs: dict[str, typing.Any]) -> OpenAIChatClient:
+    def get_client(self,
+                   model_name: str,
+                   set_assistant: bool = False,
+                   **model_kwargs: dict[str, typing.Any]) -> OpenAIChatClient:
         """
         Returns a client for interacting with a specific model. This method is the preferred way to create a client.
 
@@ -186,4 +202,4 @@ class OpenAIChatService(LLMService):
             Additional keyword arguments to pass to the model when generating text.
         """
 
-        return OpenAIChatClient(model_name, **model_kwargs)
+        return OpenAIChatClient(model_name=model_name, set_assistant=set_assistant, **model_kwargs)
