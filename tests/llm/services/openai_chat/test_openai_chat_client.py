a845b1da01369766882912bf95c1ca8dc4026f4d
David Gardner <dagardner@nvidia.com>
2023-11-02 10:28:03 -0700

    Tests for async methods

--- tests/llm/services/openai_chat/test_openai_chat_client.py
+++ tests/llm/services/openai_chat/test_openai_chat_client.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import asyncio
 from unittest import mock
 
 import pytest
@@ -27,6 +28,7 @@ def test_constructor(mock_chat_completion: mock.MagicMock):
     mock_chat_completion.assert_not_called()
 
 
+@pytest.mark.parametrize("use_async", [True, False])
 @pytest.mark.parametrize(
     "input_dict, set_assistant, expected_messages",
     [({
@@ -50,17 +52,28 @@ def test_constructor(mock_chat_completion: mock.MagicMock):
       }])])
 @pytest.mark.parametrize("temperature", [0, 1, 2])
 def test_generate(mock_chat_completion: mock.MagicMock,
+                  use_async: bool,
                   input_dict: dict[str, str],
                   set_assistant: bool,
                   expected_messages: list[dict],
                   temperature: int):
     client = OpenAIChatClient(model_name="test_model", set_assistant=set_assistant, temperature=temperature)
-    assert client.generate(input_dict) == "test_output"
-    mock_chat_completion.create.assert_called_once_with(model="test_model",
-                                                        messages=expected_messages,
-                                                        temperature=temperature)
+    if use_async:
+        results = asyncio.run(client.generate_async(input_dict))
+        mock_chat_completion.acreate.assert_called_once_with(model="test_model",
+                                                             messages=expected_messages,
+                                                             temperature=temperature)
+
+    else:
+        results = client.generate(input_dict)
+        mock_chat_completion.create.assert_called_once_with(model="test_model",
+                                                            messages=expected_messages,
+                                                            temperature=temperature)
+
+    assert results == "test_output"
 
 
+@pytest.mark.parametrize("use_async", [True, False])
 @pytest.mark.parametrize(
     "inputs, set_assistant, expected_messages",
     [({
@@ -96,13 +109,23 @@ def test_generate(mock_chat_completion: mock.MagicMock,
       }]])])
 @pytest.mark.parametrize("temperature", [0, 1, 2])
 def test_generate_batch(mock_chat_completion: mock.MagicMock,
+                        use_async: bool,
                         inputs: dict[str, list[str]],
                         set_assistant: bool,
                         expected_messages: list[list[dict]],
                         temperature: int):
     client = OpenAIChatClient(model_name="test_model", set_assistant=set_assistant, temperature=temperature)
-    assert client.generate_batch(inputs) == ["test_output" for _ in range(len(inputs["prompt"]))]
 
-    mock_chat_completion.create.assert_has_calls(
-        [mock.call(model="test_model", messages=messages, temperature=temperature) for messages in expected_messages],
-        any_order=False)
+    expected_results = ["test_output" for _ in range(len(inputs["prompt"]))]
+    expected_calls = [
+        mock.call(model="test_model", messages=messages, temperature=temperature) for messages in expected_messages
+    ]
+
+    if use_async:
+        results = asyncio.run(client.generate_batch_async(inputs))
+        mock_chat_completion.acreate.assert_has_calls(expected_calls, any_order=False)
+    else:
+        results = client.generate_batch(inputs)
+        mock_chat_completion.create.assert_has_calls(expected_calls, any_order=False)
+
+    assert results == expected_results
