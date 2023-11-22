48fda28d399243b34a080c9f7b30096d7e0be521
David Gardner <dagardner@nvidia.com>
2023-11-22 13:38:01 -0800

    Specify pip only dependencies

--- setup.py
+++ setup.py
@@ -52,6 +52,9 @@ setup(
     packages=find_packages(include=["morpheus*"], exclude=['tests']),
     install_requires=[
         # Only list the packages which cannot be installed via conda here.
+        "databricks-connect",
+        "pymilvus==2.3.2",
+        "milvus==2.3.2"
     ],
     license="Apache",
     python_requires='>=3.10, <4',
