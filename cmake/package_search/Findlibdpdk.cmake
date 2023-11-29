ea188a12af95c0916b459b448c3665a986905ed7
Michael Demoret <42954918+mdemoret-nv@users.noreply.github.com>
2023-11-29 15:32:20 -0700

    Fixing the conda-build with DOCA enabled and upgrading to CMake 3.25 (#1386)
    This PR fixes the DOCA modules with building with `MORPHEUS_SUPPORT_DOCA=ON`. In addition, the following changes needed to be made:
    
    - Upgrade CMake from 3.24 -> 3.25
       - Necessary to cleanly set `CMAKE_FIND_ROOT_PATH_MODE_INCLUDE` (and similar variables) using the `block()` function
       - Other option is to manually save off these specific variables and restore them all later which is cumbersome and error prone.
    - Adds dependency to `rdma-core>=48`
       - This is necessary since DOCA requires the MLX_1.19 symbols which arent in `rdma-core=28.9`
    
    Authors:
      - Michael Demoret (https://github.com/mdemoret-nv)
      - Pete MacKinnon (https://github.com/pdmack)
    
    Approvers:
      - Christopher Harris (https://github.com/cwharris)
    
    URL: https://github.com/nv-morpheus/Morpheus/pull/1386

--- cmake/package_search/Findlibdpdk.cmake
+++ cmake/package_search/Findlibdpdk.cmake
@@ -12,89 +12,84 @@
 # the License.
 # =============================================================================
 
-# Often this is installed in /opt/mellanox/libdpdk. If so, run `set(libdpdk_ROOT "/opt/mellanox/libdpdk")` before calling this
+# Often this is installed in /opt/mellanox/dpdk. If so, run `set(libdpdk_ROOT "/opt/mellanox/dpdk")` before calling this
 # file
 
-# List of required args. Start with INCLUDE_DIR since the first one is displayed
-
-list(APPEND libdpdk_REQUIRED_VARS libdpdk_INCLUDE_DIR)
-
 # Find BSD
-find_package(bsd QUIET)
-list(APPEND doca_REQUIRED_VARS bsd_FOUND)
+find_package(bsd)
 
-# CMAKE_LIBRARY_ARCHITECTURE needs to be set for this to work correctly. Save the value so we can restore
-set(_saved_CMAKE_LIBRARY_ARCHITECTURE ${CMAKE_LIBRARY_ARCHITECTURE})
-
-if(NOT DEFINED CMAKE_LIBRARY_ARCHITECTURE)
-  set(CMAKE_LIBRARY_ARCHITECTURE x86_64-linux-gnu)
-endif()
-
-# Find the include path
-find_path(
-  libdpdk_INCLUDE_DIR rte_eal.h
-  PATH_SUFFIXES dpdk
-)
-mark_as_advanced(libdpdk_INCLUDE_DIR)
-
-# Find all of the libraries
-list(APPEND libdpdk_REQUIRED_LIBS
-  rte_eal
+# Create a scope to allow variables to be set locally without altering the parent scope
+block(SCOPE_FOR VARIABLES
+  PROPAGATE
+    libdpdk_FOUND
+    libdpdk_VERSION
 )
 
-foreach(library_name IN LISTS libdpdk_REQUIRED_LIBS)
-  find_library(
-    ${library_name}_LIBRARY
-    NAMES ${library_name}
+  # List of required args. Start with INCLUDE_DIR since the first one is displayed
+  list(APPEND libdpdk_REQUIRED_VARS bsd_FOUND libdpdk_INCLUDE_DIR)
+
+  # CMAKE_LIBRARY_ARCHITECTURE needs to be set for this to work correctly. Will be restored at the end of the block
+  if(NOT DEFINED CMAKE_LIBRARY_ARCHITECTURE)
+    set(CMAKE_LIBRARY_ARCHITECTURE x86_64-linux-gnu)
+  endif()
+
+  # This library will always be installed on the host. Allow that to be searched here (Should fix this up in the future)
+  set("CMAKE_FIND_ROOT_PATH_MODE_INCLUDE" BOTH)
+  set("CMAKE_FIND_ROOT_PATH_MODE_LIBRARY" BOTH)
+
+  # Find the include path
+  find_path(
+    libdpdk_INCLUDE_DIR rte_eal.h
+    PATH_SUFFIXES dpdk
   )
-  mark_as_advanced(${library_name}_LIBRARY)
-  list(APPEND libdpdk_REQUIRED_VARS ${library_name}_LIBRARY)
-endforeach()
+  mark_as_advanced(libdpdk_INCLUDE_DIR)
 
-macro(parse_define_number define_name file_string output_variable)
-  string(REGEX MATCH "#define ${define_name} ([^\n]+)\n" _ "${file_string}")
-  set(${output_variable} "${CMAKE_MATCH_1}")
-endmacro()
-
-macro(parse_define_string define_name file_string output_variable)
-  string(REGEX MATCH "#define ${define_name} \"([^\n]+)\"\n" _ "${file_string}")
-  set(${output_variable} "${CMAKE_MATCH_1}")
-endmacro()
-
-# Restore CMAKE_LIBRARY_ARCHITECTURE now that we are done
-set(CMAKE_LIBRARY_ARCHITECTURE ${_saved_CMAKE_LIBRARY_ARCHITECTURE})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(libdpdk
-  FOUND_VAR libdpdk_FOUND
-  REQUIRED_VARS
-    ${libdpdk_REQUIRED_VARS}
-)
-
-if(libdpdk_FOUND)
-
-  list(APPEND libdpdk_child_targets)
+  # Find all of the libraries
+  list(APPEND libdpdk_REQUIRED_LIBS
+    rte_eal
+  )
 
   foreach(library_name IN LISTS libdpdk_REQUIRED_LIBS)
-    if(NOT TARGET libdpdk::${library_name})
-      add_library(${library_name} SHARED IMPORTED GLOBAL)
-      add_library(libdpdk::${library_name} ALIAS ${library_name})
-      set_target_properties(${library_name} PROPERTIES
-        IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-        IMPORTED_LOCATION "${${library_name}_LIBRARY}"
-        INTERFACE_INCLUDE_DIRECTORIES "${libdpdk_INCLUDE_DIR};${libdpdk_INCLUDE_DIR}/../x86_64-linux-gnu/dpdk"
-        INTERFACE_LINK_LIBRARIES "bsd::bsd"
-      )
-
-      # Add to the list of dependent targets
-      list(APPEND libdpdk_child_targets libdpdk::${library_name})
-    endif()
+    find_library(
+      ${library_name}_LIBRARY
+      NAMES ${library_name}
+    )
+    mark_as_advanced(${library_name}_LIBRARY)
+    list(APPEND libdpdk_REQUIRED_VARS ${library_name}_LIBRARY)
   endforeach()
 
-  if(NOT TARGET libdpdk::libdpdk)
-    add_library(libdpdk::libdpdk INTERFACE IMPORTED)
-    set_target_properties(libdpdk::libdpdk PROPERTIES
-      INTERFACE_LINK_LIBRARIES "${libdpdk_child_targets}"
-    )
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(libdpdk
+    FOUND_VAR libdpdk_FOUND
+    REQUIRED_VARS
+      ${libdpdk_REQUIRED_VARS}
+  )
+
+  if(libdpdk_FOUND)
+
+    list(APPEND libdpdk_child_targets)
+
+    foreach(library_name IN LISTS libdpdk_REQUIRED_LIBS)
+      if(NOT TARGET libdpdk::${library_name})
+        add_library(${library_name} SHARED IMPORTED GLOBAL)
+        add_library(libdpdk::${library_name} ALIAS ${library_name})
+        set_target_properties(${library_name} PROPERTIES
+          IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+          IMPORTED_LOCATION "${${library_name}_LIBRARY}"
+          INTERFACE_INCLUDE_DIRECTORIES "${libdpdk_INCLUDE_DIR};${libdpdk_INCLUDE_DIR}/../x86_64-linux-gnu/dpdk"
+          INTERFACE_LINK_LIBRARIES "bsd::bsd"
+        )
+
+        # Add to the list of dependent targets
+        list(APPEND libdpdk_child_targets libdpdk::${library_name})
+      endif()
+    endforeach()
+
+    if(NOT TARGET libdpdk::libdpdk)
+      add_library(libdpdk::libdpdk INTERFACE IMPORTED)
+      set_target_properties(libdpdk::libdpdk PROPERTIES
+        INTERFACE_LINK_LIBRARIES "${libdpdk_child_targets}"
+      )
+    endif()
   endif()
-endif()
+endblock()
