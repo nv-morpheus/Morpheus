746ad6c7bf2fcb98ab0783fe858b5cd4e51c6989
David Gardner <dagardner@nvidia.com>
2023-12-14 15:06:44 -0800

    Add support for dictionary types by configuring the click type as a tuple with multiple=True

--- morpheus/cli/register_stage.py
+++ morpheus/cli/register_stage.py
@@ -211,6 +211,11 @@ def set_options_param_type(options_kwargs: dict, annotation, doc_type: str):
         else:
             options_kwargs["type"] = annotation
 
+    elif (issubtype(annotation, dict)):
+        options_kwargs["multiple"] = True
+        options_kwargs["type"] = click.Tuple([str, str])
+        options_kwargs["callback"] = lambda ctx, param, value: dict(value)
+
     else:
         options_kwargs["type"] = annotation
 
