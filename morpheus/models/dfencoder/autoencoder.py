fix loss calculation to ensure model convergence

morpheus/models/dfencoder/autoencoder.py
@@ -698,10 +698,18 @@ class AutoEncoder(torch.nn.Module):
 
     def do_backward(self, mse, bce, cce):
         # running `backward()` seperately on mse/bce/cce is equivalent to summing them up and run `backward()` once
-        loss_fn = mse + bce
+        loss = 0
+
+        if len(self.numeric_fts) > 0:
+            loss += mse
+
+        if len(self.binary_fts) > 0:
+            loss += bce
+
         for ls in cce:
-            loss_fn += ls
-        loss_fn.backward()
+            loss += ls
+
+        loss.backward()
 
     def compute_baseline_performance(self, in_, out_):
         """