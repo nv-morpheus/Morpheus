c440f240e8c1f795d0fdee53e255dc296bb7d998
David Gardner <dagardner@nvidia.com>
2024-01-29 08:29:08 -0800

    Add test for get_column_names

--- tests/messages/test_message_meta.py
+++ tests/messages/test_message_meta.py
@@ -27,6 +27,7 @@ from _utils.dataset_manager import DatasetManager
 from morpheus._lib.messages import MessageMeta as MessageMetaCpp
 from morpheus.config import Config
 from morpheus.messages.message_meta import MessageMeta
+from morpheus.utils.type_aliases import DataFrameType
 
 
 @pytest.fixture(name="index_type", scope="function", params=["normal", "skip", "dup", "down", "updown"])
@@ -84,20 +85,20 @@ def fixture_is_sliceable(index_type: typing.Literal['normal', 'skip', 'dup', 'do
     return index_type not in ("dup", "updown")
 
 
-def test_count(df: cudf.DataFrame):
+def test_count(df: DataFrameType):
 
     meta = MessageMeta(df)
 
     assert meta.count == len(df)
 
 
-def test_has_sliceable_index(df: cudf.DataFrame, is_sliceable: bool):
+def test_has_sliceable_index(df: DataFrameType, is_sliceable: bool):
 
     meta = MessageMeta(df)
     assert meta.has_sliceable_index() == is_sliceable
 
 
-def test_ensure_sliceable_index(df: cudf.DataFrame, is_sliceable: bool):
+def test_ensure_sliceable_index(df: DataFrameType, is_sliceable: bool):
 
     meta = MessageMeta(df)
 
@@ -107,7 +108,7 @@ def test_ensure_sliceable_index(df: cudf.DataFrame, is_sliceable: bool):
     assert old_index_name == (None if is_sliceable else "_index_")
 
 
-def test_mutable_dataframe(df: cudf.DataFrame):
+def test_mutable_dataframe(df: DataFrameType):
 
     meta = MessageMeta(df)
 
@@ -117,7 +118,7 @@ def test_mutable_dataframe(df: cudf.DataFrame):
     assert meta.copy_dataframe()['v2'].iloc[3] == 47
 
 
-def test_using_ctx_outside_with_block(df: cudf.DataFrame):
+def test_using_ctx_outside_with_block(df: DataFrameType):
 
     meta = MessageMeta(df)
 
@@ -133,7 +134,7 @@ def test_using_ctx_outside_with_block(df: cudf.DataFrame):
     pytest.raises(AttributeError, operator.setitem, ctx, 'col', 5)
 
 
-def test_copy_dataframe(df: cudf.DataFrame):
+def test_copy_dataframe(df: DataFrameType):
 
     meta = MessageMeta(df)
 
@@ -208,3 +209,13 @@ def test_cast_cpp_to_python(dataset: DatasetManager):
     assert not isinstance(py_meta, MessageMetaCpp)
 
     DatasetManager.assert_compare_df(py_meta.copy_dataframe(), df)
+
+
+def test_get_column_names(df: DataFrameType):
+    """
+    Test that we can get the column names from a MessageMeta
+    """
+    expected_columns = sorted(df.columns.to_list())
+    meta = MessageMeta(df)
+
+    assert sorted(meta.get_column_names()) == expected_columns
