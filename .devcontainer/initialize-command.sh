9d8f97a2fa03b93f368d9a56f5d1a65096554aca
Christopher Harris <xixonia@gmail.com>
2024-04-23 01:01:13 +0000

    devcontainer updates

--- .devcontainer/initialize-command.sh
+++ .devcontainer/initialize-command.sh
@@ -18,10 +18,3 @@ set -e
 
 # create a docker network for morpheus
 docker network inspect morpheus >/dev/null 2>&1 || docker network create morpheus
-
-# create the parent conda folder so it's found when mounting
-mkdir -p .cache/conda/envs
-mkdir -p ../.conda/pkgs
-
-# create a config directory if it does not exist so it's found when mounting
-mkdir -p ../.config
