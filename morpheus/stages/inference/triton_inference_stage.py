60cf6edbd216fc79ba948f0c1b34136cf671c7eb
David Gardner <dagardner@nvidia.com>
2024-04-30 17:10:44 -0700

    Only force python for shared mem

--- morpheus/stages/inference/triton_inference_stage.py
+++ morpheus/stages/inference/triton_inference_stage.py
@@ -756,10 +756,8 @@ class TritonInferenceStage(InferenceStage):
 
     def supports_cpp_node(self) -> bool:
         # Get the value from the worker class
-        # The C++ impl of this stage does not support the force_convert_inputs and use_shared_memory options
-        # Work-around for issue #1673
-        return (TritonInferenceWorker.supports_cpp_node() and not self._force_convert_inputs
-                and not self._use_shared_memory)
+        # The C++ impl of this stage does not support the use_shared_memory options
+        return (TritonInferenceWorker.supports_cpp_node() and not self._use_shared_memory)
 
     def _get_inference_worker(self, inf_queue: ProducerConsumerQueue) -> TritonInferenceWorker:
         """
