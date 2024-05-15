1e8518b031c4e4be24996f4d64f245a5f6739468
eago <elena.ago@gmail.com>
2024-05-15 16:15:21 +0200

    DOCA stage split: source + convert (#1617)
    Split original DOCA Source Stage in two stages:
    - Source stage to receive and forward packets with the new RawPacketMessage
    - Convert stage to convert from RawPacketMessage to MessageMeta format
    
    ## By Submitting this PR I confirm:
    - I am familiar with the [Contributing Guidelines](https://github.com/nv-morpheus/Morpheus/blob/main/docs/source/developer_guide/contributing.md).
    - When the PR is ready for review, new or existing tests cover these changes.
    - When the PR is ready for review, the documentation is up to date with these changes.
    
    Authors:
      - eago (https://github.com/e-ago)
      - https://github.com/eagonv
      - Michael Demoret (https://github.com/mdemoret-nv)
    
    Approvers:
      - Michael Demoret (https://github.com/mdemoret-nv)
    
    URL: https://github.com/nv-morpheus/Morpheus/pull/1617

--- morpheus/_lib/doca/include/morpheus/doca/doca_stages.hpp
+++ morpheus/_lib/doca/include/morpheus/doca/doca_stages.hpp
@@ -20,6 +20,7 @@
 #include "morpheus/doca/common.hpp"
 #include "morpheus/export.h"
 #include "morpheus/messages/meta.hpp"
+#include "morpheus/messages/raw_packet.hpp"
 
 #include <mrc/segment/builder.hpp>
 #include <pymrc/node.hpp>
@@ -41,16 +42,17 @@ struct DocaSemaphore;
  *
  * Tested only on ConnectX 6-Dx with a single GPU on the same NUMA node running firmware 24.35.2000
  */
-class MORPHEUS_EXPORT DocaSourceStage : public mrc::pymrc::PythonSource<std::shared_ptr<MessageMeta>>
+class MORPHEUS_EXPORT DocaSourceStage : public mrc::pymrc::PythonSource<std::shared_ptr<RawPacketMessage>>
 {
   public:
-    using base_t = mrc::pymrc::PythonSource<std::shared_ptr<MessageMeta>>;
+    using base_t = mrc::pymrc::PythonSource<std::shared_ptr<RawPacketMessage>>;
     using typename base_t::source_type_t;
     using typename base_t::subscriber_fn_t;
 
     DocaSourceStage(std::string const& nic_pci_address,
                     std::string const& gpu_pci_address,
                     std::string const& traffic_type);
+    ~DocaSourceStage() override;
 
   private:
     subscriber_fn_t build();
@@ -60,7 +62,6 @@ class MORPHEUS_EXPORT DocaSourceStage : public mrc::pymrc::PythonSource<std::sha
     std::vector<std::shared_ptr<morpheus::doca::DocaSemaphore>> m_semaphore;
     std::shared_ptr<morpheus::doca::DocaRxPipe> m_rxpipe;
     enum doca_traffic_type m_traffic_type;
-    rmm::cuda_stream rstream;
 };
 
 /****** DocaSourceStageInterfaceProxy***********************/
@@ -77,6 +78,53 @@ struct MORPHEUS_EXPORT DocaSourceStageInterfaceProxy
                                                                        std::string const& nic_pci_address,
                                                                        std::string const& gpu_pci_address,
                                                                        std::string const& traffic_type);
+};
+
+/**
+ * @brief Transform DOCA GPUNetIO raw packets into Dataframe for other Morpheus stages.
+ *
+ * Tested only on ConnectX 6-Dx with a single GPU on the same NUMA node running firmware 24.35.2000
+ */
+class MORPHEUS_EXPORT DocaConvertStage
+  : public mrc::pymrc::PythonNode<std::shared_ptr<RawPacketMessage>, std::shared_ptr<MessageMeta>>
+{
+  public:
+    using base_t = mrc::pymrc::PythonNode<std::shared_ptr<RawPacketMessage>, std::shared_ptr<MessageMeta>>;
+    // Input = Receive = Sink = RawPacketMessage
+    using typename base_t::sink_type_t;
+    // Output = Send = Source = MessageMeta
+    using typename base_t::source_type_t;
+    using typename base_t::subscribe_fn_t;
+
+    DocaConvertStage();
+    ~DocaConvertStage() override;
+
+  private:
+    /**
+     * Called every time a message is passed to this stage
+     */
+    source_type_t on_data(sink_type_t x);
+    source_type_t on_raw_packet_message(sink_type_t x);
+
+    cudaStream_t m_stream;
+    rmm::cuda_stream_view m_stream_cpp;
+    uint32_t* m_fixed_pld_size_list;
+    uint32_t* m_fixed_pld_size_list_cpu;
+    uint32_t* m_fixed_hdr_size_list;
+    uint32_t* m_fixed_hdr_size_list_cpu;
+};
+
+/****** DocaConvertStageInterfaceProxy***********************/
+/**
+ * @brief Interface proxy, used to insulate python bindings.
+ */
+struct MORPHEUS_EXPORT DocaConvertStageInterfaceProxy
+{
+    /**
+     * @brief Create and initialize a DocaConvertStage, and return the result.
+     */
+    static std::shared_ptr<mrc::segment::Object<DocaConvertStage>> init(mrc::segment::Builder& builder,
+                                                                        std::string const& name);
 };
 
 }  // namespace morpheus
