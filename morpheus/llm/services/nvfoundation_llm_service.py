6718d0d36e0bb0acf18a3feddf720d19e9d80769
David Gardner <dagardner@nvidia.com>
2024-06-24 15:29:49 -0700

    Fix incorrect yaml, langchain-nvidia-ai-endpoints does not exist in the dev yaml

--- morpheus/llm/services/nvfoundation_llm_service.py
+++ morpheus/llm/services/nvfoundation_llm_service.py
@@ -26,7 +26,7 @@ IMPORT_ERROR_MESSAGE = (
     "The `langchain-nvidia-ai-endpoints` package was not found. Install it and other additional dependencies by "
     "running the following command:"
     "`conda env update --solver=libmamba -n morpheus "
-    "--file morpheus/conda/environments/dev_cuda-121_arch-x86_64.yaml --prune`")
+    "--file conda/environments/examples_cuda-121_arch-x86_64.yaml`")
 
 try:
     from langchain_core.prompt_values import StringPromptValue
