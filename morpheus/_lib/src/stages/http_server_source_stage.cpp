af4aae3d52ec7e8e7a1eae15fd82ded6faa7703d
David Gardner <dagardner@nvidia.com>
2024-08-07 11:55:29 -0700

    Remove out of date comment [no ci]

--- morpheus/_lib/src/stages/http_server_source_stage.cpp
+++ morpheus/_lib/src/stages/http_server_source_stage.cpp
@@ -49,7 +49,6 @@ void make_output_message(std::shared_ptr<MessageMeta>& incoming_message,
                          morpheus::utilities::json_t&& http_fields,
                          std::shared_ptr<ControlMessage>& out_message)
 {
-    // TODO fill in metadata
     utilities::json_t cm_config = {{"metadata", {{"http_fields", http_fields}}}};
     auto cm_msg                 = std::make_shared<ControlMessage>(incoming_message, cm_config);
     if (task)
