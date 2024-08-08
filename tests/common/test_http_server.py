072fc200148456f668451139988921231aca743b
David Gardner <dagardner@nvidia.com>
2024-08-08 10:25:27 -0700

    Specify ids for test params to make it clearer which parameter is being tested [no ci]

--- tests/common/test_http_server.py
+++ tests/common/test_http_server.py
@@ -40,9 +40,9 @@ def make_parse_fn(status: HTTPStatus = HTTPStatus.OK,
 @pytest.mark.parametrize("endpoints", [("/t1", "/t2", "/t3"), ("test/", "123/", "a1d/"), ("/a", "/a/b", "/a/b/c/d")])
 @pytest.mark.parametrize("port", [8088, 9090])
 @pytest.mark.parametrize("method", ["GET", "POST", "PUT"])
-@pytest.mark.parametrize("include_headers", [True, False])
-@pytest.mark.parametrize("use_callback", [True, False])
-@pytest.mark.parametrize("use_context_mgr", [True, False])
+@pytest.mark.parametrize("include_headers", [True, False], ids=["with_headers", "without_headers"])
+@pytest.mark.parametrize("use_callback", [True, False], ids=["with_callback", "without_callback"])
+@pytest.mark.parametrize("use_context_mgr", [True, False], ids=["with_context_mgr", "without_context_mgr"])
 @pytest.mark.parametrize("num_threads", [1, 2, min(8, os.cpu_count())])
 @pytest.mark.parametrize("status,content_type,content",
                          [(HTTPStatus.OK, MimeTypes.TEXT.value, "OK"),
