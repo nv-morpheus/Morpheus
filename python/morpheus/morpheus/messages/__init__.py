f647a7d3503e3adc2909b13cc0f1e239b2874b60
Yuchen Zhang <134643420+yczhang-nv@users.noreply.github.com>
2024-09-11 10:14:21 -0700

    fix CI

--- python/morpheus/morpheus/messages/__init__.py
+++ python/morpheus/morpheus/messages/__init__.py
@@ -31,7 +31,6 @@ from morpheus.messages.memory.response_memory import ResponseMemoryAE
 from morpheus.messages.memory.response_memory import ResponseMemoryProbs
 from morpheus.messages.message_base import MessageBase
 from morpheus.messages.message_meta import MessageMeta
-from morpheus.messages.multi_message import MultiMessage
 from morpheus.messages.message_meta import UserMessageMeta
 
 __all__ = [
