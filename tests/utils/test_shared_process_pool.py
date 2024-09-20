506c0cf1960a7b4ce4d30610b0c3252e7ace46bf
Yuchen Zhang <134643420+yczhang-nv@users.noreply.github.com>
2024-09-19 17:35:40 -0700

    fix test

--- tests/utils/test_shared_process_pool.py
+++ tests/utils/test_shared_process_pool.py
@@ -20,7 +20,7 @@ from decimal import Decimal
 from fractions import Fraction
 
 import pytest
-
+import multiprocessing as mp
 from morpheus.utils.shared_process_pool import PoolStatus
 from morpheus.utils.shared_process_pool import SharedProcessPool
 
@@ -49,6 +49,10 @@ def _add_task(x, y):
 def _time_consuming_task(sleep_time):
     time.sleep(sleep_time)
     return sleep_time
+
+
+def _blocked_until_signaled_task(q: mp.Queue):
+    return q.get()
 
 
 def _function_raises_exception():
@@ -247,16 +251,27 @@ def test_task_completion_with_early_stop(shared_process_pool):
     pool.set_usage("test_stage_2", 0.3)
     pool.set_usage("test_stage_3", 0.5)
 
+    manager = mp.Manager()
+    queue = manager.Queue()
+
     tasks = []
 
-    task_num = 5
-    sleep_time = 10
+    task_num = 10
+
     for _ in range(task_num):
-        tasks.append(pool.submit_task("test_stage_1", _time_consuming_task, sleep_time))
-        tasks.append(pool.submit_task("test_stage_2", _time_consuming_task, sleep_time))
-        tasks.append(pool.submit_task("test_stage_3", _time_consuming_task, sleep_time))
+        tasks.append(pool.submit_task("test_stage_1", _blocked_until_signaled_task, queue))
+        tasks.append(pool.submit_task("test_stage_2", _blocked_until_signaled_task, queue))
+        tasks.append(pool.submit_task("test_stage_3", _blocked_until_signaled_task, queue))
 
     pool.stop()
+
+    # No tasks have been completed since they have not been signaled yet
+    for task in tasks:
+        assert not task.done()
+
+    for i in range(len(tasks)):
+        queue.put(i)
+
     pool.join()
 
     # all tasks should be completed before the pool is shutdown
