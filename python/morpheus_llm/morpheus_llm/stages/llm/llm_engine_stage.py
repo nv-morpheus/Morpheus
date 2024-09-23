203c56dfa4f5ed74ffe23cfd01427ae6a625e48a
David Gardner <dagardner@nvidia.com>
2024-09-23 12:01:48 -0700

    WIP - untested

--- python/morpheus_llm/morpheus_llm/stages/llm/llm_engine_stage.py
+++ python/morpheus_llm/morpheus_llm/stages/llm/llm_engine_stage.py
@@ -68,28 +68,66 @@ class LLMEngineStage(PassThruTypeMixin, GpuAndCpuMixin, SinglePortStage):
         """Indicates whether this stage supports a C++ node."""
         return True
 
-    def _cast_control_message(self, message: ControlMessage, *, cpp_messages_lib: types.ModuleType) -> ControlMessage:
+    def _store_payload(self, message: ControlMessage) -> ControlMessage:
+        """
+        Store the MessageMeta in the ControlMessage's metadata.
+
+        In CPU-only allows the ControlMessage to hold an instance of a Python MessageMeta containing a pandas DataFrame.
+        """
+        message.set_metadata("llm_message_meta", message.payload())
+        return message
+
+    def _cast_to_cpp_control_message(self, message: ControlMessage, *,
+                                     cpp_messages_lib: types.ModuleType) -> ControlMessage:
         """
         LLMEngineStage does not contain a Python implementation, however it is capable of running in cpu-only mode.
-        This method is needed to cast the Python ControlMessage to a C++ ControlMessage.
+        This method is needed to create an instance of a C++ ControlMessage.
 
         This is different than casting from the Python bindings for the C++ ControlMessage to a C++ ControlMessage.
         """
-        return cpp_messages_lib.ControlMessage(message, no_cast=True)
+        cm = cpp_messages_lib.ControlMessage()
+        metadata = message.get_metadata()
+        for (key, value) in metadata.items():
+            cm.set_metadata(key, value)
+
+        return cm
+
+    def _restore_payload(self, message: ControlMessage) -> ControlMessage:
+        """
+        Pop llm_message_meta from the metadata and set it as the payload.
+
+        In CPU-only mode this has the effect of converting the C++ ControlMessage back to a Python ControlMessage.
+        """
+        metadata = message.get_metadata()
+        message_meta = metadata.pop("llm_message_meta")
+
+        out_message = ControlMessage()
+        out_message.payload(message_meta)
+        for (key, value) in metadata.items():
+            out_message.set_metadata(key, value)
+
+        return out_message
 
     def _build_single(self, builder: mrc.Builder, input_node: mrc.SegmentObject) -> mrc.SegmentObject:
         import morpheus_llm._lib.llm as _llm
+
+        store_payload_node = builder.make_node(f"{self.unique_name}-store-payload", ops.map(self._store_payload))
+        builder.make_edge(input_node, store_payload_node)
+
         node = _llm.LLMEngineStage(builder, self.unique_name, self._engine)
         node.launch_options.pe_count = 1
 
         if self._config.execution_mode == ExecutionMode.CPU:
             import morpheus._lib.messages as _messages
-            cast_fn = functools.partial(self._cast_control_message, cpp_messages_lib=_messages)
-            pre_node = builder.make_node(f"{self.unique_name}-pre-cast", ops.map(cast_fn))
-            builder.make_edge(input_node, pre_node)
+            cast_to_cpp_fn = functools.partial(self._cast_to_cpp_control_message, cpp_messages_lib=_messages)
+            cast_to_cpp_node = builder.make_node(f"{self.unique_name}-pre-msg-cast", ops.map(cast_to_cpp_fn))
+            builder.make_edge(store_payload_node, cast_to_cpp_node)
+            builder.make_edge(cast_to_cpp_node, node)
 
-            input_node = pre_node
+        else:
+            builder.make_edge(store_payload_node, node)
 
-        builder.make_edge(input_node, node)
+        restore_payload_node = builder.make_node(f"{self.unique_name}-restore-payload", ops.map(self._restore_payload))
+        builder.make_edge(node, restore_payload_node)
 
-        return node
+        return restore_payload_node
