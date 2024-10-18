8d96aa1bc6c283a9c11a329ca66369f632124cd9
Anuradha Karuppiah <anuradhak@nvidia.com>
2024-10-18 22:01:21 +0000

    Generate the requirements files from dependencies.yaml
    Signed-off-by: Anuradha Karuppiah <anuradhak@nvidia.com>

--- ci/conda/recipes/morpheus-libs/morpheus_dfp_test.sh
+++ ci/conda/recipes/morpheus-libs/morpheus_dfp_test.sh
@@ -13,11 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# morpheus tests are dependent on some pypi packages
-rapids-dependency-file-generator \
-  --output requirements \
-  --file-key morpheus_dfp --matrix "" | tee "dfp_requirements.txt"
-
-pip install -r dfp_requirements.txt
+python3 <<EOF
+import pkgutil
+import subprocess
+data = pkgutil.get_data("morpheus_dfp", "requirements_morpheus_dfp.txt")
+requirements = data.decode("utf-8")
+subprocess.call(f"pip install {requirements}".split())
+EOF
 
 pytest tests/morpheus_dfp
