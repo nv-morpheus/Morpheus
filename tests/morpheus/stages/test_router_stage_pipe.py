55b0a1df37f56b6b1abd9f9233ba45a8deb4641f
Michael Demoret <mdemoret@nvidia.com>
2024-10-23 15:27:50 -0400

    Adding backpressure test

--- tests/morpheus/stages/test_router_stage_pipe.py
+++ tests/morpheus/stages/test_router_stage_pipe.py
@@ -13,15 +13,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import threading
+
+import pytest
+
+from morpheus.config import ExecutionMode
 from morpheus.messages import ControlMessage
+from morpheus.messages import MessageMeta
 from morpheus.pipeline import Pipeline
+from morpheus.pipeline.stage_decorator import stage
 from morpheus.stages.general.router_stage import RouterStage
+from morpheus.stages.input.in_memory_data_generation_stage import InMemoryDataGenStage
 from morpheus.stages.input.in_memory_source_stage import InMemorySourceStage
 from morpheus.stages.output.in_memory_sink_stage import InMemorySinkStage
 from morpheus.stages.preprocess.deserialize_stage import DeserializeStage
 
 
-def test_router_stage_pipe(config, filter_probs_df):
+@pytest.mark.parametrize("is_runnable", [True, False])
+def test_router_stage_pipe(config, filter_probs_df, is_runnable: bool):
 
     keys = ["odd", "even"]
 
@@ -35,7 +44,7 @@ def test_router_stage_pipe(config, filter_probs_df):
     pipe = Pipeline(config)
     source = pipe.add_stage(InMemorySourceStage(config, dataframes=[filter_probs_df], repeat=5))
     deserialize = pipe.add_stage(DeserializeStage(config))
-    router_stage = pipe.add_stage(RouterStage(config, keys=keys, key_fn=determine_route_fn))
+    router_stage = pipe.add_stage(RouterStage(config, keys=keys, key_fn=determine_route_fn, is_runnable=is_runnable))
     sink1 = pipe.add_stage(InMemorySinkStage(config))
     sink2 = pipe.add_stage(InMemorySinkStage(config))
 
@@ -49,3 +58,69 @@ def test_router_stage_pipe(config, filter_probs_df):
 
     assert len(sink1.get_messages()) == 2, "Expected 2 messages in sink1"
     assert len(sink2.get_messages()) == 3, "Expected 3 messages in sink2"
+
+
+def test_router_stage_backpressure_pipe(config, filter_probs_df):
+
+    # This test simulates a slow single consumer by blocking the second output port of the router stage The router stage
+    # will buffer the messages and block the source stage from sending more data When run as a component, less threads
+    # will be used but this system will eventually block. With a runnable, this should be able to run to completion
+
+    # Set the edge buffer size to trigger blocking
+    config.edge_buffer_size = 4
+
+    keys = ["odd", "even"]
+
+    count = 0
+
+    release_event = threading.Event()
+
+    def source_fn():
+
+        for i in range(20):
+            cm = ControlMessage()
+            cm.set_metadata("index", i)
+            cm.payload(MessageMeta(filter_probs_df))
+            yield cm
+
+        # Release the event to allow the pipeline to continue
+        release_event.set()
+
+        # Send more data
+        for i in range(20, 30):
+            cm = ControlMessage()
+            cm.set_metadata("index", i)
+            cm.payload(MessageMeta(filter_probs_df))
+            yield cm
+
+    def determine_route_fn(x: ControlMessage):
+        nonlocal count
+        count += 1
+        return keys[count % len(keys)]
+
+    pipe = Pipeline(config)
+
+    source = pipe.add_stage(InMemoryDataGenStage(config, data_source=source_fn, output_data_type=ControlMessage))
+    router_stage = pipe.add_stage(RouterStage(config, keys=keys, key_fn=determine_route_fn, is_runnable=True))
+    sink1 = pipe.add_stage(InMemorySinkStage(config))
+    sink2 = pipe.add_stage(InMemorySinkStage(config))
+
+    @stage(execution_modes=[ExecutionMode.CPU, ExecutionMode.GPU])
+    def blocking_stage(data: ControlMessage) -> ControlMessage:
+
+        release_event.wait()
+
+        return data
+
+    blocking = pipe.add_stage(blocking_stage(config))
+
+    # Connect the stages
+    pipe.add_edge(source, router_stage)
+    pipe.add_edge(router_stage.output_ports[0], sink1)
+    pipe.add_edge(router_stage.output_ports[1], blocking)
+    pipe.add_edge(blocking, sink2)
+
+    pipe.run()
+
+    assert len(sink1.get_messages()) == 15, "Expected 15 messages in sink1"
+    assert len(sink2.get_messages()) == 15, "Expected 15 messages in sink2"
