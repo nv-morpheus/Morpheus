e351e0737d6ac756e72b2facaaa7bd74e2229996
David Gardner <96306125+dagardner-nv@users.noreply.github.com>
2024-10-24 09:01:50 -0700

    Ensure timestamps are copied in `LLMEngineStage` (#1975)
    Closes #1973
    
    ## By Submitting this PR I confirm:
    - I am familiar with the [Contributing Guidelines](https://github.com/nv-morpheus/Morpheus/blob/main/docs/source/developer_guide/contributing.md).
    - When the PR is ready for review, new or existing tests cover these changes.
    - When the PR is ready for review, the documentation is up to date with these changes.
    
    Authors:
      - David Gardner (https://github.com/dagardner-nv)
    
    Approvers:
      - Michael Demoret (https://github.com/mdemoret-nv)
    
    URL: https://github.com/nv-morpheus/Morpheus/pull/1975

--- tests/morpheus_llm/stages/test_llm_engine_stage_pipe.py
+++ tests/morpheus_llm/stages/test_llm_engine_stage_pipe.py
@@ -15,14 +15,20 @@
 # limitations under the License.
 
 import os
+from datetime import datetime
+
+import pytest
 
 from _utils import TEST_DIRS
 from _utils import assert_results
 from _utils.dataset_manager import DatasetManager
 from morpheus.config import Config
+from morpheus.messages import ControlMessage
 from morpheus.pipeline.linear_pipeline import LinearPipeline
+from morpheus.pipeline.stage_decorator import stage
 from morpheus.stages.input.in_memory_source_stage import InMemorySourceStage
 from morpheus.stages.output.compare_dataframe_stage import CompareDataFrameStage
+from morpheus.stages.output.in_memory_sink_stage import InMemorySinkStage
 from morpheus.stages.preprocess.deserialize_stage import DeserializeStage
 from morpheus_llm.llm import LLMEngine
 from morpheus_llm.llm.nodes.extracter_node import ExtracterNode
@@ -37,9 +43,10 @@ def _build_engine() -> LLMEngine:
     return engine
 
 
-def test_pipeline(config: Config, dataset_cudf: DatasetManager):
+@pytest.mark.gpu_and_cpu_mode
+def test_pipeline(config: Config, dataset: DatasetManager):
     test_data = os.path.join(TEST_DIRS.validation_data_dir, 'root-cause-validation-data-input.jsonlines')
-    input_df = dataset_cudf[test_data]
+    input_df = dataset[test_data]
     expected_df = input_df.copy(deep=True)
     expected_df["response"] = expected_df['log']
 
@@ -53,3 +60,34 @@ def test_pipeline(config: Config, dataset_cudf: DatasetManager):
     pipe.run()
 
     assert_results(sink.get_results())
+
+
+@pytest.mark.gpu_and_cpu_mode
+def test_error_1973(config: Config, dataset: DatasetManager):
+    expected_timestamps: dict[str, datetime] = {}
+
+    @stage(execution_modes=(config.execution_mode, ))
+    def log_timestamp(msg: ControlMessage, *, timestamp_name: str) -> ControlMessage:
+        ts = datetime.now()
+        msg.set_timestamp(key=timestamp_name, timestamp=ts)
+        expected_timestamps[timestamp_name] = ts
+        return msg
+
+    task_payload = {"task_type": "llm_engine", "task_dict": {"input_keys": ['v1']}}
+    pipe = LinearPipeline(config)
+    pipe.set_source(InMemorySourceStage(config, dataframes=[dataset["filter_probs.csv"]]))
+    pipe.add_stage(DeserializeStage(config, task_type="llm_engine", task_payload=task_payload))
+    pipe.add_stage(log_timestamp(config, timestamp_name="pre_llm"))
+    pipe.add_stage(LLMEngineStage(config, engine=_build_engine()))
+    pipe.add_stage(log_timestamp(config, timestamp_name="post_llm"))
+    sink = pipe.add_stage(InMemorySinkStage(config))
+
+    pipe.run()
+
+    messages = sink.get_messages()
+    assert len(messages) == 1
+
+    msg = messages[0]
+    for (timestamp_name, expected_timestamp) in expected_timestamps.items():
+        actual_timestamp = msg.get_timestamp(timestamp_name, fail_if_nonexist=True)
+        assert actual_timestamp == expected_timestamp
