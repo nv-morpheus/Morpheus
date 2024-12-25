d9ef7eaf0d4b733f77743baa8e946fef45ec5be1
anindyam1969 <amukherjee@kinetica.com>
2024-12-25 14:23:20 +0530

    WIP - Implementation of Kinetica vector DB service. Added sample usage.

--- python/morpheus_llm/morpheus_llm/modules/output/write_to_vector_db.py
+++ python/morpheus_llm/morpheus_llm/modules/output/write_to_vector_db.py
@@ -145,9 +145,13 @@ def _write_to_vector_db(builder: mrc.Builder):
                 try:
                     if accum_stats.data:
                         df_pkg = get_df_pkg_from_obj(accum_stats.data[0])
+                        print("1 ......")
                         merged_df = df_pkg.concat(accum_stats.data)
+                        print("2 ......")
                         service.insert_dataframe(name=key, df=merged_df)
+                        print("3 ......")
                         final_df_references.append(accum_stats.data)
+                        print("4 ......")
                 except Exception as e:
                     logger.error("Unable to upload dataframe entries to vector database: %s", e)
         finally:
