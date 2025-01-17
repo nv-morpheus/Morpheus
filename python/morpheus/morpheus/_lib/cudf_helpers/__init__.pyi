f03448e2e92c90a588148d186c4d1f63b2ea37ed
David Gardner <dagardner@nvidia.com>
2025-01-17 15:22:13 -0800

    Update stubs

---
+++ python/morpheus/morpheus/_lib/cudf_helpers/__init__.pyi
@@ -0,0 +1,27 @@
+from __future__ import annotations
+import morpheus._lib.cudf_helpers
+import typing
+from cudf.core.column.column import ColumnBase
+from cudf.core.buffer.exposure_tracked_buffer import ExposureTrackedBuffer
+from cudf.core.buffer.spillable_buffer import SpillableBuffer
+from cudf.core.dtypes import StructDtype
+import cudf
+import itertools
+import pylibcudf
+import rmm
+
+__all__ = [
+    "ColumnBase",
+    "ExposureTrackedBuffer",
+    "SpillableBuffer",
+    "StructDtype",
+    "as_buffer",
+    "cudf",
+    "itertools",
+    "plc",
+    "rmm"
+]
+
+
+__pyx_capi__: dict # value = {'make_table_from_table_with_metadata': <capsule object "PyObject *(cudf::io::table_with_metadata, int)">, 'make_table_from_table_info_data': <capsule object "PyObject *(morpheus::TableInfoData, PyObject *)">, 'make_table_info_data_from_table': <capsule object "morpheus::TableInfoData (PyObject *)">, 'data_from_table_view_indexed': <capsule object "PyObject *(cudf::table_view, PyObject *, PyObject *, PyObject *, PyObject *)">}
+__test__ = {}
