804fa86629c568b3eeee3dc364bc140e348bf74e
Will Killian <wkillian@nvidia.com>
2025-03-05 13:34:09 -0500

    Revert back to mambabuild
    Signed-off-by: Will Killian <wkillian@nvidia.com>

--- ci/conda/recipes/run_conda_build.sh
+++ ci/conda/recipes/run_conda_build.sh
@@ -38,7 +38,7 @@ export x="\033[0m"
 export CONDA_ALWAYS_YES=true
 
 # Change this to switch between build/mambabuild/debug
-export CONDA_COMMAND=${CONDA_COMMAND:-"build"}
+export CONDA_COMMAND=${CONDA_COMMAND:-"mambabuild"}
 
 # Get the path to the morpheus git folder
 export MORPHEUS_ROOT=${MORPHEUS_ROOT:-$(git rev-parse --show-toplevel)}
