5ad89c782f656ab6ff5d5e6bee24c754cba0be1b
David Gardner <dagardner@nvidia.com>
2025-03-05 17:21:07 -0800

    importlib.resources.path now returns a context manager
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- ci/conda/recipes/morpheus-libs/morpheus_core_test.sh
+++ ci/conda/recipes/morpheus-libs/morpheus_core_test.sh
@@ -22,8 +22,8 @@ fi
 python3 <<EOF
 import importlib.resources
 import subprocess
-requirements_file = importlib.resources.path("morpheus", "requirements_morpheus_core_arch-$(arch).txt")
-subprocess.call(f"pip install -r {requirements_file}".split())
+with importlib.resources.path("morpheus", "requirements_morpheus_core_arch-$(arch).txt") as requirements_file:
+    subprocess.call(f"pip install -r {requirements_file}".split())
 EOF
 
 pytest tests/morpheus
