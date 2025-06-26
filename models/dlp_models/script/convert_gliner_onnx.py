902c1f0db34b4b270bc909a8bb616747143a1d68
David Gardner <dagardner@nvidia.com>
2025-06-26 11:44:44 -0700

    Remove scripts from LFS
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- models/dlp_models/script/convert_gliner_onnx.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:adc620b87aac8cc9aee1c97cb232cfc43fb5decff23c23469b82906891da7afe
-size 10880
--- models/dlp_models/script/setup_onnx_model.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:25f53792bb82e29a09d695ad8266707c6fd650f014889e308c4876859d02c8ed
-size 1587
