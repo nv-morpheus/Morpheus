674adaa2b3d740172efaef952e30928926e21c8c
David Gardner <dagardner@nvidia.com>
2025-07-08 09:05:23 -0700

    Add round-trip test for write_df_to_file/read_file_to_df and include parquet support
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- tests/morpheus/io/test_serializers.py
+++ tests/morpheus/io/test_serializers.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pathlib
 import typing
 from io import BytesIO
 
@@ -22,12 +23,14 @@ import pytest
 
 from _utils.dataset_manager import DatasetManager
 from morpheus.common import FileTypes
+from morpheus.io.deserializers import read_file_to_df
 from morpheus.io.serializers import df_to_csv
 from morpheus.io.serializers import df_to_json
 from morpheus.io.serializers import df_to_parquet
 from morpheus.io.serializers import df_to_stream_csv
 from morpheus.io.serializers import df_to_stream_json
 from morpheus.io.serializers import df_to_stream_parquet
+from morpheus.io.serializers import write_df_to_file
 
 
 @pytest.mark.parametrize(
@@ -76,5 +79,32 @@ def test_serializers(dataset: DatasetManager,
         deserialize_fn = pd.read_parquet
 
     result_df = deserialize_fn(results, **deserialize_kwargs)
+
+    dataset.assert_compare_df(df, result_df)
+
+
+@pytest.mark.parametrize(
+    "extension,file_type,serialize_kwargs",
+    [("csv", FileTypes.CSV, {
+        'include_header': True, 'include_index_col': False
+    }), ("csv", FileTypes.Auto, {
+        'include_header': True, 'include_index_col': False
+    }), ("json", FileTypes.JSON, {}), ("json", FileTypes.Auto, {}), ("jsonlines", FileTypes.JSON, {}),
+     ("jsonlines", FileTypes.Auto, {}), ("parquet", FileTypes.PARQUET, {
+         'include_index_col': False
+     }), ("parquet", FileTypes.Auto, {
+         'include_index_col': False
+     })],
+    ids=['csv', 'csv_auto', 'json', 'json_auto', 'jsonlines', 'jsonlines_auto', 'parquet', 'parquet_auto'])
+def test_write_df_to_file(dataset: DatasetManager,
+                          tmp_path: pathlib.Path,
+                          extension: str,
+                          file_type: FileTypes,
+                          serialize_kwargs: dict[str, typing.Any]):
+    df = dataset['filter_probs.csv']
+    out_file = str(tmp_path / f"test.{extension}")
+
+    write_df_to_file(df=df, file_name=out_file, file_type=file_type, **serialize_kwargs)
+    result_df = read_file_to_df(out_file, file_type=file_type, filter_nulls=False)
 
     dataset.assert_compare_df(df, result_df)
