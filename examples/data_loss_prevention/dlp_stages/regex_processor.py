9320685f52c35b0f1e5d6f801beedfa60baba5a4
David Gardner <dagardner@nvidia.com>
2025-07-11 09:57:42 -0700

    Remove unused imports
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- examples/data_loss_prevention/dlp_stages/regex_processor.py
+++ examples/data_loss_prevention/dlp_stages/regex_processor.py
@@ -13,9 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 import logging
-import pathlib
 
 import mrc
 from mrc.core import operators as ops
