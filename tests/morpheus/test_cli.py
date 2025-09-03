e0bb8aadd005cb323e5e350c3382933744f993e9
David Gardner <dagardner@nvidia.com>
2025-09-03 11:02:03 -0700

    Remove old 24.06 work-around
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- tests/morpheus/test_cli.py
+++ tests/morpheus/test_cli.py
@@ -118,7 +118,7 @@ def mlflow_uri(tmp_path):
 
     yield uri
 
-    num_runs = len(fluent._active_run_stack.get())
+    num_runs = len(fluent._active_run_stack)
     for _ in range(num_runs):
         mlflow.end_run()
 
