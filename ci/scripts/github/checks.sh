1e2932e7d36563fe856160d4cac1f5fafa879bba
David Gardner <dagardner@nvidia.com>
2026-01-20 11:59:30 -0800

    Fix pip install command in ci script
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- ci/scripts/github/checks.sh
+++ ci/scripts/github/checks.sh
@@ -49,9 +49,9 @@ cmake --build ${BUILD_DIR} --parallel ${PARALLEL_LEVEL}
 log_sccache_stats
 
 rapids-logger "Installing Morpheus"
-pip install ./python/morpheus
-pip install ./python/morpheus_llm
-pip install ./python/morpheus_dfp
+pip install --no-build-isolation ./python/morpheus
+pip install --no-build-isolation ./python/morpheus_llm
+pip install --no-build-isolation ./python/morpheus_dfp
 
 rapids-logger "Checking copyright headers"
 python ${MORPHEUS_ROOT}/ci/scripts/copyright.py --verify-apache-v2 --git-diff-commits ${CHANGE_TARGET} ${GIT_COMMIT}
