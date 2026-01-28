4100f909eb019f8b9575b489ae1c59de59bc3b3b
David Gardner <dagardner@nvidia.com>
2026-01-28 10:00:46 -0800

    Revert "WIP"
    This reverts commit 6ee62c1167e16e0ad81ff09f07c7fdd67a33e5f9.
    
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- ci/scripts/github/docs.sh
+++ ci/scripts/github/docs.sh
@@ -45,11 +45,10 @@ rapids-logger "Configuring for docs"
 cmake ${CMAKE_BUILD_ALL_FEATURES} -DMORPHEUS_PYTHON_INPLACE_BUILD=ON -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} -DMORPHEUS_PYTHON_BUILD_STUBS=OFF -DMORPHEUS_BUILD_DOCS=ON .
 
 rapids-logger "Installing Morpheus"
-cmake --build ${BUILD_DIR} --parallel ${PARALLEL_LEVEL}
+cmake --build ${BUILD_DIR} --parallel ${PARALLEL_LEVEL} --target install
 
 rapids-logger "Building docs"
-cmake --build ${BUILD_DIR} --parallel ${PARALLEL_LEVEL} --target morpheus_docs
-# morpheus_docs_linkcheck
+cmake --build ${BUILD_DIR} --parallel ${PARALLEL_LEVEL} --target morpheus_docs morpheus_docs_linkcheck
 
 rapids-logger "Archiving the docs"
 tar cfj "${WORKSPACE_TMP}/docs.tar.bz" ${BUILD_DIR}/docs/html
